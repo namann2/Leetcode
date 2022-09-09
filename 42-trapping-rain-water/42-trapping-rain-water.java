8df61ececc86a47ea992e058a16481ebf21dcfec
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-09-09 23:39:17 +0530

    Time: 1 ms (99.77%), Space: 47.7 MB (84.13%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,42 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        /*
-            Topic Segregation :
-            We need to find the max water that can be stored. Hence,
-            possible solutions : 
-            Brute Force
-            Greedy
-            DP
-            Binary Search
-            
-            Brute Force would be O(n^2) ~ 10^8 so, it won't work
-            Binary Search/DP is ruled out 
-            Greedy should work !
-            
-            Generally, greedy problems implements sorting or use of heaps or a tricky mind-boggling solution
-        */
         int n = height.length;
-        int[]L = new int[n];
-        int[]R = new int[n];
-        
-        /* Find max building on the left and on the right for a particular building 
-            as that much water could be stored on the current building
-        */
-        
-        L[0] = height[0];
-        for(int i=1;i<n;i++) {
-            L[i] = Math.max(height[i], L[i-1]);
-        }
-        
-        R[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--) {
-            R[i] = Math.max(height[i], R[i+1]);
-        }
-        
+        int leftMax = height[0], rightMax = height[n-1];
         int maxWater = 0;
-        for(int i=0;i<n;i++) 
-            maxWater += (Math.min(L[i], R[i]) - height[i]);
         
+        int left = 0, right = n-1;
+        while(left < right) {
+            if(leftMax <= rightMax) {
+                maxWater += (leftMax - height[left]);
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+            } else {
+                maxWater += (rightMax - height[right]);
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+            }
+        }
         return maxWater;
     }
 }
