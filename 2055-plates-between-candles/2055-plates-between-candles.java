6eca7a29f679c80158481a7763d17c469f9df8f7
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-09-11 13:28:57 +0530

    Time: 149 ms (15.86%), Space: 136.8 MB (76.45%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] platesBetweenCandles(String s, int[][] Q) {
+        int n = s.length();
+        int[]ps = new int[n+1];
+        TreeSet<Integer> plates = new TreeSet<>();
+        ps[0] = s.charAt(0) == '*' ? 1 : 0;
+        if(s.charAt(0) == '|') plates.add(0);
+        for(int i=1;i<n;i++) {
+            char ch = s.charAt(i);
+            if(ch == '*') ps[i] = ps[i-1] + 1;
+            else {
+                ps[i] = ps[i-1];
+                plates.add(i);
+            }
+        }
+        
+        int[] ans = new int[Q.length];
+        int idx = 0;
+        for(int[]q : Q) {
+            Integer higher = plates.ceiling(q[0]);
+            Integer lower = plates.floor(q[1]);
+            if(lower == null || higher == null) {
+                idx++;
+                continue;
+            }
+            int val = ps[lower] - ps[higher];
+            ans[idx++] = val >= 0 ? val : 0;
+        } 
+        return ans;
+    }
+}
