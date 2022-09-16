c8bc3dc46d3ca4e86418e0393b99c092d5582c65
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-09-16 23:14:26 +0530

    Time: 17 ms (26.97%), Space: 48.8 MB (55.72%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -5,21 +5,26 @@ class Solution {
         // since subsequence, order does not matter
         Arrays.sort(A);
         int n = A.length;
-        int[]ps = new int[n];
-        ps[0] = A[0];
         for(int i=1;i<n;i++)
-            ps[i] += ps[i-1] + A[i];
-        
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int i=0;i<n;i++) {
-            map.put(ps[i], i);
-        }
+            A[i] += A[i-1];
         
         for(int i=0;i<m;i++) {
-            Integer idx = map.floorKey(q[i]);
-            if(idx != null) 
-                answer[i] = map.get(idx)+1;
+            int idx = binarySearch(A, q[i]);
+            if(idx != -1) 
+                answer[i] = idx+1;
         }
         return answer;
+    }
+    private int binarySearch(int[]A, int sum) {
+        int n = A.length;
+        int start = 0, end = n-1, ans = -1;
+        while(start <= end) {
+            int m = start + (end - start)/2;
+            if(A[m] <= sum) {
+                ans = m;
+                start = m + 1;
+            } else end = m - 1;
+        }
+        return ans;
     }
 }
