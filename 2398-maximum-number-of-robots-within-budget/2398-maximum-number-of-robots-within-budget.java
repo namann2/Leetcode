268ef91d59fa29734a93ab8a931cf55bd125620d
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-10-02 14:18:36 +0530

    Time: 56 ms (77.17%), Space: 114.8 MB (75.07%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+        int n = chargeTimes.length;
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        int begin = 0, end = 0, maxi = 0;
+        long sum = 0;
+        while(end < n) {
+            sum += runningCosts[end];
+            while(!q.isEmpty() && chargeTimes[q.peekLast()] < chargeTimes[end]) {
+                q.pollLast();
+            }
+            q.addLast(end);
+            long cb = sum * (end - begin + 1) + chargeTimes[q.peekFirst()];
+            if(cb > budget) {
+                if(!q.isEmpty() && q.peekFirst() == begin) q.pollFirst();
+                sum -= runningCosts[begin++];
+            }
+            if(cb <= budget) maxi = Math.max(maxi, end-begin+1);
+            end++;
+        }
+        return maxi;
+    }
+}
