4a799221c2cc5a40394626c6e6413cfc28983851
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-10-14 21:29:44 +0530

    Update 253-meeting-rooms-ii.java

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,3 +1,4 @@
+// Approach 1 : Sweep Line Algorithm
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         int n = intervals.length, max = 0;
@@ -19,5 +20,55 @@ class Solution {
             ans = Math.max(ps[i], ans);
         }
         return ans;
+    }
+}
+
+//  Approach 2 : Focus on meeting that start early and ends early to minimize the number of meeting rooms required
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (i1, i2)->{
+            return i1[0] - i2[0];
+        });
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> {
+            return i1[1] - i2[1];
+        });
+        
+        pq.add(intervals[0]);
+        int n = intervals.length;
+        
+        for(int i=1;i<n;i++) {
+            int[] goingOn = pq.poll();
+            if(intervals[i][0] >= goingOn[1]) goingOn[1] = intervals[i][1];
+            else pq.add(intervals[i]);
+            pq.add(goingOn);
+        }
+        return pq.size();
+    }
+}
+
+// Approach 3 : Another way of performing line sweep
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+        int[]start = new int[n], end = new int[n];
+        int p=0;
+        for(int[] interval : intervals) {
+            start[p] = interval[0];
+            end[p++] = interval[1];
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
+        
+        int meetingRooms = 1, 
+        i = 0;
+        for(int j=1;j<n;j++) {
+            if(start[j] >= end[i]) i++;
+            else meetingRooms ++;
+        }
+        return meetingRooms;
     }
 }
