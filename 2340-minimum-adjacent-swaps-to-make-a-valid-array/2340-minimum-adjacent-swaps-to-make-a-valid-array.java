778f399513ef83224cdb695369977e4d2d7ef4b3
Naman Gupta <59756482+namann2@users.noreply.github.com>
2022-12-30 23:39:36 +0530

    Time: 9 ms (38.81%), Space: 55.9 MB (75.34%) - LeetHub

---
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        if(nums == null || nums.length == 1)
+            return 0;
+        
+        int n = nums.length;
+        int smallest = Integer.MAX_VALUE, largest = Integer.MIN_VALUE;
+        int si = 0, li = 0;
+        
+        for(int i=0;i<n;i++) {
+            if(smallest > nums[i]) {
+                si = i;
+                smallest = nums[i];
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--) {
+            if(largest < nums[i]) {
+                li = i;
+                largest = nums[i];
+            }
+        }
+        
+        System.out.println(si +" :: "+li);
+        int x = si + n - li - 1;
+        return si > li ? x - 1 : x;
+    }
+}
+
+/*
+    Options : 
+        greedy
+        bfs
+        dynamic programming
+    
+    greedy : 
+    Greedily select elements to move such that we add minimum swaps
+    
+*/
