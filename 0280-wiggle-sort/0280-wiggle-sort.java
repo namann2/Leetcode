517ddab5f19be3a3a33dde45dcb1db2858764777
Naman Gupta <59756482+namann2@users.noreply.github.com>
2023-02-14 22:36:10 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (53.45%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.java
+++ 0280-wiggle-sort/0280-wiggle-sort.java
@@ -1,5 +1,9 @@
 class Solution {
     public void wiggleSort(int[] nums) {
+        // naive_sorting(nums);
+        greedy(nums);
+    }
+    private void naive_sorting(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         for(int i=1;i<n-1;i+=2) {
@@ -11,5 +15,14 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = temp;
         
+    }
+    private void greedy(int[] nums) {
+        // observation : even index elements should be smaller then next elements and 
+        // odd index elements should be greater than next elements
+        int n = nums.length;
+        for(int i=0;i<n-1;i++) {
+            if((i%2 == 0 && nums[i] > nums[i+1]) || (i%2 == 1 && nums[i] < nums[i+1]))
+                swap(nums, i, i+1);
+        }
     }
 }
