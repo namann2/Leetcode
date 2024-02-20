525e53e399aa17bbc0a512575acd3e2ff55836e2
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-02-20 22:39:58 +0530

    Time: 0 ms (100.00%), Space: 45 MB (30.20%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -1,9 +1,31 @@
+enum BOUND {
+    LOWER, UPPER;
+}
 class Solution {
+    // https://www.youtube.com/watch?v=6BoymP9R9MM
     public int findSpecialInteger(int[] A) {
-        int n = A.length, req = n / 4;
-        for(int i=0;i<n;i++) {
-            if(A[i] == A[i+req]) return A[i];
+        int n = A.length;
+        int[] B = {n/4, n/2, 3*n/4};
+        for(int b : B) {
+            int start = bs(A, A[b], 0, b, BOUND.LOWER);
+            int end = bs(A, A[b], b, n-1, BOUND.UPPER);
+            if(end-start+1 > n/4)
+                return A[b];
         }
         return -1;
     }
+    private int bs(int[] A, int target, int start, int end, BOUND bound) {
+        int ans = -1;
+        while(start <= end) {
+            int mid = (start + end) >> 1;
+            if(A[mid] == target) {
+                ans = mid;
+                if(bound == BOUND.LOWER) end = mid - 1;
+                else start = mid + 1;
+            } else if(A[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+        return ans;
+    }
+    
 }
