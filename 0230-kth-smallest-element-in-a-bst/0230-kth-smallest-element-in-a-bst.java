1f63a99bbd8fe4b754d319abe613deb34aea3b4d
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-02-24 00:39:53 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (38.00%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        // TC : O(N), SC : O(H) 
+        // We can use morris traversal to save extra space
+        // Stack<TreeNode> stack = new Stack<>();
+        // TreeNode curr = root;
+        // while(curr != null || !stack.isEmpty()) {
+        //     while(curr != null) {
+        //         stack.push(curr);
+        //         curr = curr.left;
+        //     }
+        //     curr = stack.pop();
+        //     if(--k == 0) return curr.val;
+        //     curr = curr.right;
+        // }
+        // return -1;
+        
+        // Morris Inorder Traversal : TC : O(3N), SC : O(1)
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left != null) {
+                // update the thread from leaf to curr
+                TreeNode ptr = curr.left;
+                while(ptr.right != null && ptr.right != curr) 
+                    ptr = ptr.right;
+                // two conditions for the ptr
+                if(ptr.right == null) {
+                    ptr.right = curr;
+                    curr = curr.left;
+                } else { // ptr.right points to the curr
+                    ptr.right = null;
+                    if(--k == 0) return curr.val;
+                    curr = curr.right;
+                }
+                
+            } else {
+                if(--k == 0) return curr.val;
+                curr = curr.right;
+            }
+        }
+        return -1;
+    }
+}
