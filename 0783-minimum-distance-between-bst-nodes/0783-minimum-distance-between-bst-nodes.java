b8ef8ec831c6eced92bb3d2976d195b104b29d47
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-02-23 23:40:08 +0530

    Time: 1 ms (21.25%), Space: 40.7 MB (95.48%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -1,25 +1,18 @@
 class Solution {
     public int minDiffInBST(TreeNode root) {
-        /*
-            Minimum diff between any two node in a bst can be only 
-            found from the adjacent nodes. Doing inorder traversal, we
-            will have the sorted format of all numbers, we can then find the 
-            difference between them to find the min.
-        */
-        if(root == null) return 0;
-        List<Integer> all = new ArrayList<>();
-        inorder(root, all);
-        // we now have a sorted sequence of data
-        int ans = Integer.MAX_VALUE, size = all.size();
-        for(int i=1;i<size;i++) {
-            ans = Math.min(ans, all.get(i) - all.get(i-1));
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root, prev = null;
+        int ans = Integer.MAX_VALUE;
+        while(curr != null || !stack.isEmpty()) {
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if(prev != null) ans = Math.min(ans, curr.val - prev.val);
+            prev = curr;
+            curr = curr.right;
         }
         return ans;
-    }
-    private void inorder(TreeNode root, List<Integer> all) {
-        if(root == null) return;
-        inorder(root.left, all);
-        all.add(root.val);
-        inorder(root.right, all);
     }
 }
