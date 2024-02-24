b2f73e97bb899de8c172580946626aa181f53d8a
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-02-24 16:22:43 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (97.36%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    int ans = 0;
+    public int maxPathSum(TreeNode root) {
+        if(root == null) return 0;
+        ans = Integer.MIN_VALUE;
+        bottomup(root);
+        return ans;
+    }
+    private int bottomup(TreeNode root) {
+        if(root == null) return 0;
+        
+        int left = Math.max(0, bottomup(root.left));
+        int right = Math.max(0, bottomup(root.right));
+        
+        ans = Math.max(ans, root.val); // only root
+        ans = Math.max(ans, root.val + left + right); // consider this subtree
+        ans = Math.max(ans, root.val + Math.max(left, right)); // consider the root and either of the left/right subtree
+        
+        // return max sum of current subtree to parent
+        return root.val + Math.max(left, right);
+    }
+}
