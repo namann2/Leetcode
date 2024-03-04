a9f9629c3ffc8fb0813ac238c59fe3ff17405919
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-04 22:11:00 +0530

    Time: 3 ms (37.26%), Space: 42.4 MB (92.99%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,8 +1,11 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
+        // greedy approach
         int n = tokens.length;
         if(tokens == null || n == 0) return 0;
+        
         Arrays.sort(tokens);
+        
         if(power < tokens[0]) return 0;
         
         int score = 0;
@@ -12,7 +15,9 @@ class Solution {
             if(power >= tokens[left]) {
                 power -= tokens[left++];
                 score++;
-            } else if(score >= 1 && left != right) {
+            }  // only increase power or i.e. decrease score only if we have the chance to increase 
+            // score afterwards. Thus, left < right or left != right
+            else if(score >= 1 && left < right) {
                 power += tokens[right--];
                 score--;
             } else break;
