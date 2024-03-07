71abe15ddfad8e7a24819baa2074f4342b03d84c
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-08 02:43:23 +0530

    Time: 4 ms (92.84%), Space: 117.4 MB (89.80%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,16 +1,18 @@
 class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int n = grid.length, m = grid[0].length;
-        for(int i=0;i<n;i++) {
+        for(int i=1;i<n;i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i=1;i<m;i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+        for(int i=1;i<n;i++) {
             for(int j=1;j<m;j++) {
-                grid[i][j] += grid[i][j-1];
+                grid[i][j] += grid[i][j-1] + grid[i-1][j] - grid[i-1][j-1] ;
             }
         }
-        for(int j=0;j<m;j++) { // col
-            for(int i=1;i<n;i++) { // row
-                grid[i][j] += grid[i-1][j];
-            }
-        }
+        
         int ans = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
