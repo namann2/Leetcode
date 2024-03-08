d81b677779deca9d080acfb69d34773b3dab4101
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-08 15:02:47 +0530

    Time: 4 ms (68.17%), Space: 59.4 MB (85.67%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,30 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length, k = 2;
-        int[][][]dp = new int[n+1][2][k+1];
-        for(int[][]mat : dp)
-            for(int[] row : mat)
-                Arrays.fill(row, -1);
-        // 0 = can not Buy
-        // 1 = can Buy
-        return maxProfit(prices, 0, 1, n, 2, dp);
-    }
-    private int maxProfit(int[] prices, int index, int canBuy, int n, int k, int[][][]dp) {
-        // base case
-        if(index == n || k == 0) 
-            return 0;
-        if(dp[index][canBuy][k] != -1)
-            return dp[index][canBuy][k];
-        
-        // main logic
-        if(canBuy == 1) {
-            int buy = -prices[index] + maxProfit(prices, index+1, 0, n, k, dp);
-            int notBuy = maxProfit(prices, index+1, 1, n, k, dp);
-            return dp[index][canBuy][k] = Math.max(buy, notBuy);
-        } else {
-            int sell = prices[index] + maxProfit(prices, index+ 1, 1, n, k-1, dp);
-            int notSell = maxProfit(prices, index+1, 0, n, k, dp);
-            return dp[index][canBuy][k] = Math.max(sell, notSell);
+        int min = Integer.MIN_VALUE;
+        int A = min, B = min, C = min, D = min;
+        for(int price : prices) {
+            A = Math.max(A, - price); // buy or not buy
+            B = Math.max(B, A + price); // sell or not sell, B is the resultant profit after a transaction
+            C = Math.max(C, B - price); // buy or not buy second time
+            D = Math.max(D, C + price);
         }
+        return D;
     }
 }
