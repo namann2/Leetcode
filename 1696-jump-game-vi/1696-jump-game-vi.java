a01a5334518e21d0fab0a5d6fb9ad60cb08e4c63
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-11 23:00:33 +0530

    Time: 53 ms (21.64%), Space: 58.5 MB (43.27%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int maxScore = nums[n-1];
+        // TC : O(n * klogk)
+        // SC : O(k)
+        dp[n-1] = nums[n-1];
+        // we want k max values from i so, [i+1....i+k]
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return b[1] - a[1]; 
+        });
+        
+        pq.offer(new int[]{n-1, dp[n-1]});
+        
+        for(int i = n - 2; i >= 0; i--) {
+            while(pq.peek()[0] - i > k) pq.poll();
+            dp[i] = nums[i] + pq.peek()[1];
+            pq.offer(new int[]{i, dp[i]});
+        }
+        return dp[0];
+    }
+}
