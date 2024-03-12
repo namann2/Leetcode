34f95e4fdd0229ed1fcb54d11fab0008f33dc81f
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-13 03:25:33 +0530

    Time: 26 ms (77.82%), Space: 65.4 MB (19.20%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,47 +1,36 @@
 class Solution {
-    HashSet<int[]> visited = new HashSet<>();
-    Boolean[][]dp;
     public boolean canCross(int[] stones) {
         int n = stones.length;
         
         if(stones[1] != 1) return false;
+        
         Map<Integer, Integer> map = new HashMap<>();
         
+        HashSet<int[]> visited = new HashSet<>();
+        Boolean[][]dp = new Boolean[n+1][n+1];
         
-        dp = new Boolean[n+1][n+1];
         for(int i = 1; i < n ; i++) 
             map.put(stones[i], i);
         
         visited.add(new int[]{1, 1}); // index, jump
         
-        return canCross(stones, 1, 1, n, map);
+        return canCross(stones, 1, 1, n, map, visited, dp);
     }
-    private boolean canCross(int[] stones, int index, int k, int n, Map<Integer, Integer> map) {
+    private boolean canCross(int[] stones, int index, int k, int n, Map<Integer, Integer> map, HashSet<int[]> visited, Boolean[][]dp) {
         // base case
         if(index >= n-1)
             return true;
         
         if(dp[index][k] != null)
             return dp[index][k];
+        
         // main logic
-        // k - 1 jump
-        boolean op1 = false, op2 = false, op3 = false;
+        for(int jump = k - 1; jump <= k + 1; jump ++) {
+            if(jump > 0 && map.containsKey(stones[index] + jump) && visited.add(new int[]{stones[index] + jump, jump}))
+                if(canCross(stones, map.get(stones[index] + jump), jump, n, map, visited, dp))
+                    return dp[index][k] = true;
+        }
         
-        if(k - 1 > 0 && map.containsKey(stones[index] + k - 1) && visited.add(new int[]{stones[index] + k - 1, k - 1}))
-            op1 = canCross(stones, map.get(stones[index] + k - 1), k - 1, n, map);
-        
-        if(op1) return true;
-        // k jump
-        if(map.containsKey(stones[index] + k) && visited.add(new int[]{stones[index] + k, k}))
-            op2 = canCross(stones, map.get(stones[index] + k), k , n, map);
-        
-        if(op2) return true;
-        // k+1 jump
-        if(map.containsKey(stones[index] + k + 1) && visited.add(new int[]{stones[index] + k + 1, k + 1}))
-            op3 = canCross(stones, map.get(stones[index] + k + 1), k + 1, n, map);
-        
-        if(op3) return true;
-        
-        return dp[index][k] = op1 || op2 || op3;
+        return dp[index][k] = false;
     }
 }
