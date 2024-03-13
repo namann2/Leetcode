b4959c915872abac51dec38ae932efeadeac5862
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-13 19:45:03 +0530

    Time: 9 ms (54.37%), Space: 103.4 MB (96.57%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,12 +1,13 @@
 class Solution {
-    public long mostPoints(int[][] questions) {
-        int n = questions.length;
+    public long mostPoints(int[][] A) {
+        int n = A.length;
         Long[] dp = new Long[n+1];
         // return mostPoints(questions, 0, n, dp);
         
         dp[n] = 0l;
         for(int index = n - 1; index >= 0; index --) {
-            dp[index] = Math.max(questions[index][0] + (index + questions[index][1] + 1 < n ? dp[index + questions[index][1] + 1] : 0),
+            dp[index] = Math.max(
+                A[index][0] + (index + A[index][1] + 1 < n ? dp[index + A[index][1] + 1] : 0),
                                 dp[index+1]);
         }
         return dp[0];
