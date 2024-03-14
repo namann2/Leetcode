6d97a4363307608683bbffda059a69ddc4915d31
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-14 22:18:40 +0530

    Time: 22 ms (42.75%), Space: 52.7 MB (7.22%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,13 +1,11 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         // subarray sum equals k -> prefix sum
-        // doubt full in sliding window approach
         Map<Integer, Integer> map = new HashMap<>();
         int cnt = 0, sum = 0, n = nums.length;
+        map.put(0, 1);
         for(int i = 0; i < n; i++) {
             sum += nums[i];
-            // possible that current sum equals goal
-            if(sum == goal) cnt++;
             if(map.containsKey(sum - goal)) 
                 cnt += map.get(sum - goal);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
