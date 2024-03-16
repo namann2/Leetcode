5cb7254d3fb7ba102626a784bab24ad002da603b
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-17 02:01:13 +0530

    Update 252-meeting-rooms.java

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,3 +1,4 @@
+// Sorting : TC : O(nlogn), SC : O(1)
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, (i1, i2) -> {
@@ -9,6 +10,26 @@ class Solution {
         int n = intervals.length;
         for(int i=1;i<n;i++) {
             if(intervals[i][0] < intervals[i-1][1]) return false;
+        }
+        return true;
+    }
+}
+
+// Line Sweep : TC : O(n), SC : O(n)
+// if there is an overlap in the meeting times, then all meetings can not be attended
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int[]line_sweep = new int[(int)1e6+1];
+        for(int[] interval : intervals) {
+            line_sweep[interval[0]] += 1;
+            line_sweep[interval[1]] -= 1;
+        }
+        
+        if(line_sweep[0] > 1) return false;
+        
+        for(int i = 1; i < line_sweep.length; i++) {
+            line_sweep[i] += line_sweep[i-1];
+            if(line_sweep[i] > 1) return false;
         }
         return true;
     }
