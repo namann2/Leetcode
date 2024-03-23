98de7a09e4fdb45cbf94bf11f33b14afac529544
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-24 01:55:26 +0530

    Time: 7 ms (30.90%), Space: 41.6 MB (68.91%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -4,21 +4,20 @@ class Solution {
         int n = s1.length(), m = s2.length(), K = s3.length();
         if(n + m != K)
             return false;
-        boolean[][][]dp = new boolean[n+1][m+1][K+1];
+        boolean[][]dp = new boolean[n+1][m+1];
         
-        dp[n][m][K] = true;
+        dp[n][m] = true;
         for(int i = n; i >= 0; i--) {
             for(int j = m; j >= 0; j--) {
-                for(int k = K-1; k >= 0; k--) {
-                    if(i < n && s1.charAt(i) == s3.charAt(k)) {
-                        dp[i][j][k] |= dp[i+1][j][k+1];
-                    }
-                    if(j < m && s2.charAt(j) == s3.charAt(k)) {
-                        dp[i][j][k] |= dp[i][j+1][k+1];
-                    }
+                int k = i + j;
+                if(i < n && s1.charAt(i) == s3.charAt(k)) {
+                    dp[i][j] |= dp[i+1][j];
+                }
+                if(j < m && s2.charAt(j) == s3.charAt(k)) {
+                    dp[i][j] |= dp[i][j+1];
                 }
             }
         }
-        return dp[0][0][0];   
+        return dp[0][0];   
     }
 }
