4fe094db6a98e0df2818594b4340d560dbbe7199
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-03-26 22:12:57 +0530

    Time: 9 ms (74.52%), Space: 44.9 MB (67.57%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,21 +1,21 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
+        // sorting with end, since we want to increase the 
+        // length of longest increasing sequence which
+        // depends upon the end number. [s,e] the smallest the 
+        // end of a pair in the starting, more pairs can be 
+        // considered.
         Arrays.sort(pairs, (p1, p2) -> {
-            if(p1[0] == p2[0]) return p1[1] - p2[1];
-            return p1[0] - p2[0];
+            return p1[1] - p2[1];
         });
         
-        int length = 1, n = pairs.length;
-        int[]dp = new int[n];
-        dp[0] = 1;
-        for(int i=1;i<n;i++) {
-            dp[i] = 1;
-            for(int j=0;j<i;j++) {
-                if(pairs[j][1] < pairs[i][0] && dp[j]+1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                }
+        int length = 0, n = pairs.length;
+        int prev = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            if(pairs[i][0] > prev) {
+                length++;
+                prev = pairs[i][1];
             }
-            length = Math.max(length, dp[i]);
         }
         return length;
     }
