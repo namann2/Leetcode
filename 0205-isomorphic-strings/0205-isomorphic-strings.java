56a17146f97178dcc028eb2a2edcdc0b9983a707
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-02 22:30:53 +0530

    Time: 4 ms (98.36%), Space: 42.5 MB (26.81%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,14 +3,15 @@ class Solution {
         int n = s.length(), m = t.length();
         if(n != m) return false;
         
-        Map<Character, Character> map = new HashMap<>();
-        Set<Character> used = new HashSet<>();
+        int[] index1 = new int[256];
+        int[] index2 = new int[256];
+        
+        
+        // bde - cce
         for(int i = 0; i < n ; i++) {
             char s_ch = s.charAt(i), t_ch = t.charAt(i);
-            if(map.containsKey(s_ch) && map.get(s_ch) != t_ch) return false;
-            if(used.contains(t_ch) && !map.containsKey(s_ch)) return false;
-            used.add(t_ch);
-            map.put(s_ch, t_ch);
+            if(index1[s_ch] != index2[t_ch]) return false;
+            index1[s_ch] = index2[t_ch] = i+1;
         }
         return true;
     }
