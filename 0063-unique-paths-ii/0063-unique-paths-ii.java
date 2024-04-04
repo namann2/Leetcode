b49c741ee7555fc006cc345f6b53ab118eaff759
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-05 00:55:41 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (74.71%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
+        int[][]dp = new int[n][m];
+        
+        // first row
+        dp[0][0] = 1;
+        
+        for(int j=0;j<m;j++) {
+            if(obstacleGrid[0][j] == 1) break;
+            dp[0][j] += j-1 >= 0 ? dp[0][j-1] : 0;
+        }
+        
+        // first column
+        for(int i=0;i<n;i++) {
+            if(obstacleGrid[i][0] == 1) break;
+            dp[i][0] += i-1 >= 0 ? dp[i-1][0] : 0;
+        }
+        
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<m;j++) {
+                if(obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+}
