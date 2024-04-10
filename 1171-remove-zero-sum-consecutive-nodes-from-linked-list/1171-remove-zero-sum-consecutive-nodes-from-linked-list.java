4a01c4fd8f71a2cd6b0d627b29f0eb356ff72487
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-10 14:10:53 +0530

    Time: 3 ms (64.80%), Space: 43.8 MB (69.82%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -1,37 +1,29 @@
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        // TC : in the worst case, we will traverse every node twice : O(2*n) : O(n)
-        // SC : O(n)
-        
-        // this is same as subarray sum equals 0
-        if(head == null)
-            return null;
         Map<Integer, ListNode> map = new HashMap<>();
-        // update a dummy head pointer because head also might get deleted
-        ListNode dh = new ListNode(-1);
-        map.put(0, dh);
-        dh.next = head;
-        // maintain a sum variable to calculate prefix sum
+        ListNode dH = new ListNode(-1);
+        map.put(0, dH);
+        dH.next = head;
         int sum = 0;
+        
         while(head != null) {
             sum += head.val;
             boolean contains = map.containsKey(sum);
-            // if sum already found previously
+            
             if(contains) {
-                ListNode prev = map.get(sum);
-                ListNode runner = prev;
-                // todo : remove values of sum that contributed [prev, head)
-                int temp_sum = sum;
-                runner = runner.next;
-                while(runner != head) {
-                    temp_sum += runner.val;
-                    map.remove(temp_sum);
-                    runner = runner.next;
-                }
+                ListNode prev = map.get(sum), temp = prev.next;
                 prev.next = head.next;
-            } else map.put(sum, head);
+                int temp_sum = sum;
+                while(temp != head) {
+                    temp_sum += temp.val;
+                    map.remove(temp_sum);
+                    temp = temp.next;
+                }
+            } else {
+                map.put(sum, head);
+            }
             head = head.next;
         }
-        return dh.next;
+        return dH.next;
     }
 }
