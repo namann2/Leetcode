5252f8ecdcc3f823731a6940050229a4f51f85fa
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-10 12:18:49 +0530

    Time: 6 ms (51.43%), Space: 68.4 MB (36.98%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,20 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    ListNode left;
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true;
-        left = head;
-        return isPalindromeHelper(head);
-    }
-    private boolean isPalindromeHelper(ListNode right) {
-        // base case
-        if(right == null) return true;
         
-        boolean isPalin = isPalindromeHelper(right.next);
+        // In linkedlist questions, it is better to ask for -> whether we can update the links of the LL
         
-        if(!isPalin) return false;
+        // Iterative Approach : 
+        // 1. Find middle
+        // 2. detach and reverse second half
+        // 3. check both the halfs
         
-        if(left.val != right.val) return false;
-        left = left.next;
+        ListNode middle = getMiddleOfLinkedList(head);
+        ListNode nextHalfHead = middle.next;
+        middle.next = null;
+        nextHalfHead = reverse(nextHalfHead);
+        
+        while(head != null && nextHalfHead != null) { 
+            if(head.val != nextHalfHead.val) return false;
+            head = head.next;
+            nextHalfHead = nextHalfHead.next;
+        }
         return true;
+    }
+    private ListNode reverse(ListNode head) {
+        // there are two techniques to solve this. -> iterative and other is recursive
+        ListNode curr = head, prev = null, next = null;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    private ListNode getMiddleOfLinkedList(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
