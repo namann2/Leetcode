6ed1d925e091f7519ba14ec244b2c0ad4b747ce2
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-12 23:16:03 +0530

    Time: 445 ms (27.76%), Space: 53.3 MB (9.34%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
-            int j = i + 1, k = n - 1;
-            while(j < k) {
-                int curr_sum = nums[i] + nums[j] + nums[k];
-                if(curr_sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (j < k && nums[j] == nums[j + 1]) j++; // Skip duplicates for j
-                    while (j < k && nums[k] == nums[k - 1]) k--; // Skip duplicates for k
-                    j++;
-                    k--;
-                } else if(curr_sum > 0) {
-                    k--;
-                } else {
-                    j++;
+        Set<List<Integer>> ans = new HashSet<>();
+        Set<Integer> starts = new HashSet<>();
+        // required sum with i as starting point of the triplet, 
+        // to keep a check that the triplet we are formulating
+        // starts with index >= i
+        Map<Integer,Integer> requiredSumWithI = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            if(starts.add(nums[i])) {
+                for(int j = i + 1; j < n; j++) {
+                    int third = -(nums[i] + nums[j]);
+                    if(requiredSumWithI.containsKey(third) && requiredSumWithI.get(third) >= i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
+                        Collections.sort(triplet);
+                        ans.add(triplet);
+                    }
+                    requiredSumWithI.put(nums[j], i);
                 }
             }
         }
-        return ans;
+        return new ArrayList<>(ans);
     }
 }
