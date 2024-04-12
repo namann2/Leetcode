d32fa793a0ec9b9a21957d5275d1597651a0296b
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-12 23:17:09 +0530

    Time: 452 ms (27.53%), Space: 53.6 MB (7.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,14 +8,19 @@ class Solution {
         // starts with index >= i
         Map<Integer,Integer> requiredSumWithI = new HashMap<>();
         for(int i = 0; i < n; i++) {
+            // check if unique start point
             if(starts.add(nums[i])) {
                 for(int j = i + 1; j < n; j++) {
+                    // the required number
                     int third = -(nums[i] + nums[j]);
                     if(requiredSumWithI.containsKey(third) && requiredSumWithI.get(third) >= i) {
                         List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
+                        // sorting a triplet 
                         Collections.sort(triplet);
                         ans.add(triplet);
                     }
+                    // update the index of the number to check 
+                    // if this can contribute for a triplet ahead
                     requiredSumWithI.put(nums[j], i);
                 }
             }
