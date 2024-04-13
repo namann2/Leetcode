b460f16522d7f106dbf1ebfe914863b611c0f459
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-14 02:40:13 +0530

    Time: 2 ms (90.17%), Space: 48.5 MB (15.93%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,59 +1,41 @@
 class Solution {
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null || head.next.next == null)
-            return;
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode middle = slow;
+        ListNode middleNext = middle.next;
+        middle.next = null;
         
-        Stack<ListNode> stack = new Stack<>();
-        int size = 0;
-        ListNode ptr = head;
-        while(ptr != null) {
-            size++;
-            stack.push(ptr);
-            ptr = ptr.next;
+        middleNext = reverse(middleNext, middleNext);
+        merge(head, middleNext);
+    }
+    private ListNode reverse(ListNode head, ListNode node) {
+        if(node == null || node.next == null) {
+            head = node;
+            return head;
         }
         
-        // insert stack.top in between two nodes of original list
-        ListNode L = head;
-        for(int i=0;i<size/2;i++) {
-            ListNode next = L.next;
-            ListNode curr = stack.pop();
-            L.next = curr;
-            curr.next = next;
-            L = next;
+        ListNode get = reverse(head, node.next);
+        node.next.next = node;
+        node.next = null;
+        return get;
+    }
+    private void merge(ListNode a, ListNode b) {
+        while(b != null) {
+            ListNode temp = a.next;
+            a.next = b;
+            a = temp;
+            
+            temp = b.next;
+            b.next = a;
+            b = temp;
         }
-        L.next = null;
+        if(b != null) {
+            while(a.next != null) a = a.next;
+            a.next = b;
+        }
     }
 }
-/*
-class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null) return;
-        reorderList(head, head.next);
-    }
-    
-    public ListNode reorderList(ListNode root, ListNode curr) {
-        if (curr == null) return root;
-
-        // keep on passing the intial root to the end
-        root = reorderList(root, curr.next);
-
-        if (root == null) return null;
-        
-//          * we stop reconfiguring in 2 cases. 
-//          *   1. returned new root is same as head: Odd number of list 
-//                 items so we have come to the middle
-//          *   2. returned new root's next is same as head: Even number of list items
-         
-        ListNode temp = null;
-        if (root == curr || root.next == curr) {
-            curr.next = null;
-        } else {
-            // make returned root's next to be curr and return root's next as the new root
-            temp = root.next;
-            root.next = curr;
-            curr.next = temp;
-        }
-        return temp;
-    }
-}
-*/
