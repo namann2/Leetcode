4334803fc1555b9b437ea809060e719bbb1345c6
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-14 00:35:25 +0530

    Time: 2 ms (79.58%), Space: 44.6 MB (28.91%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        return mergeSort(lists, 0, lists.length - 1);
+    }
+    private ListNode mergeSort(ListNode[] lists, int leftStart, int rightEnd) {
+        if(leftStart == rightEnd) return lists[leftStart];
+        
+        int mid = leftStart + (rightEnd - leftStart) / 2;
+        
+        ListNode a = mergeSort(lists, leftStart, mid);
+        ListNode b = mergeSort(lists, mid+1, rightEnd);
+        
+        return mergeHalves(a, b);
+    }
+    private ListNode mergeHalves(ListNode a, ListNode b) {
+        if(a == null && b == null) return null;
+        if(a == null || b == null) return a == null ? b : a;
+        
+        ListNode result = null;
+        if(a.val <= b.val) {
+            result = a;
+            result.next = mergeHalves(a.next, b);
+        } else {
+            result = b;
+            result.next = mergeHalves(a, b.next);
+        }
+        return result;
+    }
+}
