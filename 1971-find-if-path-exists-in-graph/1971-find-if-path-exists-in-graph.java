c9786632d1923ee10c811ac5fef876a8444fc5e0
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-21 11:07:03 +0530

    Time: 68 ms (75.74%), Space: 139.9 MB (69.63%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,28 +1,66 @@
 class Solution {
-    // TC : O(V + E)
-    // SC : O(V + E)
+    // Let n be the number of nodes and m be the number of edges.
+    // TC : O(m * alpha(n)) ~ O(E * alpha(V))
+    // SC : O(V)
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            g.add(new ArrayList<>());
-        }
+        UnionFind uf = new UnionFind();
+        uf.makeSet(n);
         
         for(int[] edge : edges) {
-            g.get(edge[0]).add(edge[1]);
-            g.get(edge[1]).add(edge[0]);
+            uf.union(edge[0], edge[1]);
         }
         
-        return dfs(g, source, destination, new boolean[n]);
+        return uf.findSet(source) == uf.findSet(destination);
     }
-    private boolean dfs(ArrayList<ArrayList<Integer>> g, int src, int dest, boolean[] visited) {
-        if(src == dest)
-            return true;
-        visited[src] = true;
-        for(int v : g.get(src)) {
-            if(!visited[v])
-                if(dfs(g, v, dest, visited))
-                    return true;
+}
+
+class Node {
+    int data, rank;
+    Node parent;
+    Node(int data, int rank, Node parent) {
+        this.data = data;
+        this.rank = rank;
+        this.parent = parent;
+    }
+}
+
+class UnionFind {
+    Map<Integer, Node> map;
+    UnionFind() {
+        map = new HashMap<>();
+    }
+    
+    public void makeSet(int n) {
+        for(int i = 0; i < n; i++) {
+            Node node = new Node(i, i, null);
+            node.parent = node;
+            map.put(i, node);
+        }    
+    }
+    
+    public int findSet(int a) {
+        return findSet(map.get(a)).data;
+    }
+    
+    private Node findSet(Node node) {
+        if(node == null || node.parent == node) return node;
+        Node newNode = findSet(node.parent);
+        node.parent = newNode;
+        return newNode;
+    }
+    
+    public void union(int a, int b) {
+        Node pA = findSet(map.get(a));
+        Node pB = findSet(map.get(b));
+        
+        if(pA == pB) return;
+        
+        if(pA.rank >= pB.rank) {
+            pB.parent = pA;
+            pA.rank = pA.rank == pB.rank ? pA.rank + 1 : pA.rank;
+        } else {
+            pA.parent = pB;
+            pB.rank += 1;
         }
-        return false;
     }
 }
