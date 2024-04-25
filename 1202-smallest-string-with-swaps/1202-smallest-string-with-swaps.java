3804a303b0cf1c7aa17f7caee3b94d4b03cb2340
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-25 19:58:10 +0530

    Time: 73 ms (18.02%), Space: 99.6 MB (17.12%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -0,0 +1,74 @@
+class Solution {
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        int n = s.length();
+        UnionFind uf = new UnionFind();
+        uf.makeSet(n); // O(V)
+        
+        for(int i = 0; i < pairs.size(); i++) { // O(E)
+            uf.union(pairs.get(i).get(0), pairs.get(i).get(1)); // alpha(V)
+        }
+        
+        Map<Integer, PriorityQueue<Character>> component_char = new HashMap<>();
+        for(int i = 0; i < n ; i ++) {
+            int p = uf.findSet(i);
+            component_char.putIfAbsent(p, new PriorityQueue<>());
+            component_char.get(p).offer(s.charAt(i));
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < n ; i++) {
+            int p = uf.findSet(i);
+            answer.append(component_char.get(p).poll());
+        }
+        return answer.toString();
+    }
+}
+
+class Node {
+    int index, rank;
+    Node parent;
+}
+
+class UnionFind {
+    
+    private Map<Integer, Node> map ;
+    
+    UnionFind() {
+        this.map = new HashMap<>();
+    }
+    
+    public void makeSet(int n) {
+        for(int i = 0; i < n; i++) {
+            Node newNode = new Node();
+            newNode.index = i;
+            newNode.rank = 0;
+            newNode.parent = newNode;
+            map.put(i, newNode);
+        }
+    }
+    
+    public int findSet(int a) {
+        return findSet(map.get(a)).index;
+    }
+    
+    private Node findSet(Node node) {
+        if(node == null || node.parent == node) return node;
+        Node newNode = findSet(node.parent);
+        node.parent = newNode;
+        return newNode;
+    }
+    
+    public void union(int a, int b) {
+        Node pA = findSet(map.get(a));
+        Node pB = findSet(map.get(b));
+        
+        if(pA == pB) return;
+        if(pA.rank >= pB.rank) {
+            pB.parent = pA;
+            pA.rank = pA.rank == pB.rank ? pA.rank : pA.rank + 1;
+        } else {
+            pA.parent = pB;
+            pB.rank ++;
+        }
+    }
+}
