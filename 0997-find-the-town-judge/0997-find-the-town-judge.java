766637391f8689c08bf8458de07de278b1ff54be
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-29 00:52:51 +0530

    Time: 3 ms (62.87%), Space: 53.7 MB (57.34%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,15 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] indegree = new int[n];
-        int[] outdegree = new int[n];
+        int[] degree = new int[n];
+        
+        // The maximum indegree is N - 1.
         
         for(int[] t : trust) {
-            outdegree[t[0]-1]++;
-            indegree[t[1]-1]++;
+            degree[t[0]-1]--;
+            degree[t[1]-1]++;
         }
         
         for(int i = 0; i < n; i++) {
-            if(indegree[i] == n-1 && outdegree[i] == 0)
+            if(degree[i] == n-1)
                 return i+1;
         }
         return -1;
