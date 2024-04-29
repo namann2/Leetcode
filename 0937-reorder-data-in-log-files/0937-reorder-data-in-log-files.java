92a81cb503b03ab8a3220b3e007ffcb56c2bf080
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-04-29 21:43:38 +0530

    Time: 6 ms (49.71%), Space: 45.2 MB (16.67%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -1,3 +1,31 @@
+/*
+Let N be the number of logs in the list and M be the maximum length of a single log.
+
+Time Complexity: O(M⋅N⋅logN)
+
+First of all, the time complexity of the Arrays.sort() is 
+O(N⋅logN), as stated in the API specification, which is to say that the compare() function would be invoked 
+O(N⋅logN) times.
+
+For each invocation of the compare() function, it could take up to 
+O(M) time, since we compare the contents of the logs.
+
+Therefore, the overall time complexity of the algorithm is 
+O(M⋅N⋅logN).
+
+
+Space Complexity: 
+O(M⋅logN)
+
+For each invocation of the compare() function, we would need up to 
+O(M) space to hold the parsed logs.
+
+In addition, since the implementation of Arrays.sort() is based on quicksort algorithm whose space complexity is 
+O(logn), assuming that the space for each element is O(1)). Since each log could be of O(M) space, we would need O(M⋅logN) space to hold the intermediate values for sorting.
+
+In total, the overall space complexity of the algorithm is  O(M⋅logN).
+
+*/
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
         Arrays.sort(logs, new CustomComparator());
