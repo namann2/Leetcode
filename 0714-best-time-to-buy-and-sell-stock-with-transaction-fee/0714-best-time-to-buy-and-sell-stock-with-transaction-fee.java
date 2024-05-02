ef8fecb495b7504e309a5f1e37385bcb0ae84aa5
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-02 23:52:36 +0530

    Time: 19 ms (43.33%), Space: 54.8 MB (75.10%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -2,22 +2,37 @@ class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
         int[][]dp = new int[n+1][2];
-        int nextDayBuy = 0, nextDayNotBuy = 0;
-        for(int index = n - 1; index >= 0; index --) {
-            for(int canBuy = 0; canBuy < 2; canBuy++) {
-                if(canBuy == 1) {
-                    int buy = -prices[index] + nextDayNotBuy; //dp[index+1][0];
-                    int notBuy = nextDayBuy;
-                    int currDayBuy = Math.max(buy, notBuy);
-                    nextDayBuy = currDayBuy;
+        // return maxProfit(prices, 0, n, fee, 0, dp); // 0 - buy, 1 - sell
+        
+        for(int index = n-1; index >= 0; index--) {
+            for(int j = 0; j < 2; j++) {
+                if(j == 0) {
+                    dp[index][0] = Math.max(-prices[index] + dp[index + 1][1], dp[index + 1][0]);
                 } else {
-                    int sell = prices[index] - fee + nextDayBuy;
-                    int notSell = nextDayNotBuy;
-                    int currDayNotBuy = Math.max(sell, notSell);
-                    nextDayNotBuy = currDayNotBuy;
+                    dp[index][1] = Math.max(prices[index] - fee + dp[index + 1][0], dp[index + 1][1]);
                 }
             }
         }
-        return nextDayBuy; 
+        return dp[0][0];
+    }
+    
+    private int maxProfit(int[] prices, int index, int n, int fee, int buy, Integer[][] dp) {
+        // base case
+        if(index == n)
+            return 0;
+        
+        if(dp[index][buy] != null)
+            return dp[index][buy];
+        
+        // main logic
+        if(buy == 0) {
+            int x = -prices[index] + maxProfit(prices, index + 1, n, fee, 1, dp);
+            int y = maxProfit(prices, index + 1, n, fee, 0, dp);
+            return dp[index][buy] = Math.max(x, y);
+        } else {
+            int x = prices[index] - fee + maxProfit(prices, index + 1, n, fee, 0, dp);
+            int y = maxProfit(prices, index + 1, n, fee, 1, dp);
+            return dp[index][buy] = Math.max(x, y);
+        }
     }
 }
