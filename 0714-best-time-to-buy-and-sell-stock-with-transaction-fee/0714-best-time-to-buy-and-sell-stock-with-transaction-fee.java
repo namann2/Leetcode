7a127628b24ec240f52cc90df39899bd71e68739
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-02 23:55:18 +0530

    Time: 1691 ms (5.42%), Space: 55.6 MB (28.64%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,38 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][]dp = new int[n+1][2];
-        // return maxProfit(prices, 0, n, fee, 0, dp); // 0 - buy, 1 - sell
-        
+        int[] next = new int[n + 1];
         for(int index = n-1; index >= 0; index--) {
+            int[] curr = new int[n + 1];
             for(int j = 0; j < 2; j++) {
                 if(j == 0) {
-                    dp[index][0] = Math.max(-prices[index] + dp[index + 1][1], dp[index + 1][0]);
+                    curr[0] = Math.max(-prices[index] + next[1], next[0]);
                 } else {
-                    dp[index][1] = Math.max(prices[index] - fee + dp[index + 1][0], dp[index + 1][1]);
+                    curr[1] = Math.max(prices[index] - fee + next[0], next[1]);
                 }
             }
+            next = curr;
         }
-        return dp[0][0];
-    }
-    
-    private int maxProfit(int[] prices, int index, int n, int fee, int buy, Integer[][] dp) {
-        // base case
-        if(index == n)
-            return 0;
-        
-        if(dp[index][buy] != null)
-            return dp[index][buy];
-        
-        // main logic
-        if(buy == 0) {
-            int x = -prices[index] + maxProfit(prices, index + 1, n, fee, 1, dp);
-            int y = maxProfit(prices, index + 1, n, fee, 0, dp);
-            return dp[index][buy] = Math.max(x, y);
-        } else {
-            int x = prices[index] - fee + maxProfit(prices, index + 1, n, fee, 0, dp);
-            int y = maxProfit(prices, index + 1, n, fee, 1, dp);
-            return dp[index][buy] = Math.max(x, y);
-        }
+        return next[0];
     }
 }
