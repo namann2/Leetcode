5543fe6331677a240f85b55984cd4eadcd2efbaf
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-06 22:46:43 +0530

    Time: 18 ms (38.69%), Space: 75.3 MB (9.13%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -1,22 +1,16 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Deque<ListNode> stack = new ArrayDeque<>();
-        ListNode curr = head;
-        // [5,2,13,3,8]
-        // 13 8
-        while(curr != null) {
-            while(!stack.isEmpty() && stack.peekLast().val < curr.val) stack.removeLast();
-            stack.addLast(curr);
-            curr = curr.next;
-        }
+        if(head == null) return null;
+        return reverse(head);
+    }
+    private ListNode reverse(ListNode head) {
+        if(head.next == null) return head;
+        ListNode get = reverse(head.next);
         
-        ListNode nHead = new ListNode(-1);
-        ListNode temp = nHead;
-        while(!stack.isEmpty()) {
-            curr = stack.removeFirst();
-            nHead.next = curr;
-            nHead = nHead.next;
+        if(head.val < get.val) {
+            return get;
         }
-        return temp.next;
+        head.next = get;
+        return head;
     }
 }
