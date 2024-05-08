65755a07732682646815d0db06bfb5b80801d7f6
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-08 09:43:51 +0530

    Time: 9 ms (57.14%), Space: 42.8 MB (94.40%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -1,21 +1,18 @@
 class Solution {
     public int countGoodStrings(int low, int high, int zero, int one) {
+        int mod = (int)(1e9 + 7);
         int[]dp = new int[high+1];
-        int ans = 0, mod = (int)1e9+7;
-        // dp state -> number of good strings of length i
-        dp[0] = 1; // empty string
-        for(int i=1;i<=high;i++) {
-            if(i-zero >= 0) 
-                dp[i] = dp[i-zero];
-            if(i-one >= 0)
-                dp[i] += dp[i-one];
+        dp[0] = 1; // number of good strings of length i
+        for(int i = 1; i<= high; i++) {
+            if(i - zero >= 0) dp[i] = dp[i - zero] % mod;
+            if(i - one >= 0) dp[i] += dp[i - one] % mod;
             dp[i] %= mod;
         }
         
-        for(int i=low;i<=high;i++) {
-            ans += dp[i];
-            ans %= mod;
-        }
+        int ans = 0;
+        for(int i = low; i <= high; i++)
+            ans = (ans % mod + dp[i] % mod) % mod;
+        
         return ans;
     }
 }
