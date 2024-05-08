42a9fd29925aad2e9cb6791254d0303e75b7f2c0
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-08 10:02:35 +0530

    Time: 18 ms (86.81%), Space: 44.5 MB (74.27%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,23 +1,55 @@
 class Solution {
     public int minimumDeleteSum(String A, String B) {
         int n = A.length(), m = B.length();
-        int[] next = new int[m+1];
-        for(int i=n-1;i>=0;i--) {
-            int[] curr = new int[m+1];
-            for(int j=m-1;j>=0;j--) {
-                if(A.charAt(i) == B.charAt(j)) 
-                    curr[j] = A.charAt(i) + next[j+1];
-                else
-                    curr[j] = Math.max(next[j], curr[j+1]);
+        int[][]dp = new int[n+1][m+1];
+        // for(int[] row : dp)
+        //     Arrays.fill(row, -1);
+        
+        // return LCS(A, B, n, m, 0, 0, dp);
+//         
+        
+        for(int j = m-1; j >= 0; j--)
+            dp[n][j] = dp[n][j+1] + B.charAt(j);
+        
+        for(int i = n-1; i >= 0; i--)
+            dp[i][m] = dp[i+1][m] + A.charAt(i);
+            
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                if(A.charAt(i) == B.charAt(j))
+                    dp[i][j] = dp[i + 1][j + 1];
+                else dp[i][j] = Math.min(A.charAt(i) + dp[i+1][j], B.charAt(j) + dp[i][j+1]);
             }
-            next = curr;
+        }
+        return dp[0][0];
+    }
+    
+    private int LCS(String A, String B, int n, int m, int i, int j, int[][]dp) {
+        // base case
+        if(i == n && j == m)
+            return 0;
+        if(i == n) {
+            int sum = 0;
+            while(j < m)
+                sum += B.charAt(j++);
+            return sum;
+        }
+        if(j == m) {
+            int sum = 0;
+            while(i < n)
+                sum += A.charAt(i++);
+            return sum;
         }
         
-        int lcs = next[0];
-        int s1 = 0, s2 = 0;
-        for(char ch : A.toCharArray()) s1 += ch;
-        for(char ch : B.toCharArray()) s2 += ch;
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        return s1 + s2 - 2*lcs;
+        // main logic
+        if(A.charAt(i) == B.charAt(j))
+            return dp[i][j] = LCS(A, B, n, m, i+1, j+1, dp);
+        
+        int x = A.charAt(i) + LCS(A, B, n, m, i+1, j, dp);
+        int y = B.charAt(j) + LCS(A, B, n, m, i, j+1, dp);
+        return dp[i][j] = Math.min(x, y);
     }
 }
