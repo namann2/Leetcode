0ceff00535eb0c802b36b568ab66d0364d53432c
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-12 02:33:22 +0530

    Time: 4 ms (24.78%), Space: 43.1 MB (5.86%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> answer = new ArrayList<>();
+        if(root == null) return answer;
+        
+        TreeMap<Integer, List<int[]>> map = new TreeMap<>(); // left to right
+        traverse(root, 0, 0, map);
+        
+        for(int key : map.keySet()) {
+            List<Integer> temp = new ArrayList<>();
+            for(int[] val : map.get(key)) {
+                temp.add(val[2]);
+            }
+            answer.add(temp);
+        }
+        return answer;
+    }
+    
+    private void traverse(TreeNode node, int hdis, int level, TreeMap<Integer, List<int[]>> map) {
+        // base case
+        if(node == null) return;
+        
+        // main logic
+        map.putIfAbsent(hdis, new ArrayList<>());
+        map.get(hdis).add(new int[]{level, hdis, node.val});
+        
+        traverse(node.left, hdis - 1, level + 1, map);
+        traverse(node.right, hdis + 1, level + 1, map);
+        
+        List<int[]> currList = map.get(hdis);
+        
+        Collections.sort(currList, (a, b) -> {
+            if(a[0] == b[0]) 
+                return a[1] - b[1];
+            else if(a[0] < b[0]) return -1;
+            return 1;
+        });
+        
+    }
+}
