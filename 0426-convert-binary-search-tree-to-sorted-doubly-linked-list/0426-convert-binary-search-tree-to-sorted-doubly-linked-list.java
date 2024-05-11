0b1e860fdf5ed060186817d0bfc90717c27ddb2e
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-12 03:06:19 +0530

    Time: 0 ms (100.00%), Space: 42 MB (38.10%) - LeetHub

--- 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,27 +1,66 @@
 class Solution {
     public Node treeToDoublyList(Node root) {
-        if(root == null) return null;
-        Node prev = null, curr = root, head = null;
-        Stack<Node> stack = new Stack<>();
+//         if(root == null) return null;
+//         Node prev = null, curr = root, head = null;
+//         Stack<Node> stack = new Stack<>();
         
-        while(!stack.isEmpty() || curr != null) {
-            while(curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
+//         while(!stack.isEmpty() || curr != null) {
+//             while(curr != null) {
+//                 stack.push(curr);
+//                 curr = curr.left;
+//             }
             
-            curr = stack.pop();
-            if(prev == null) {
-                prev = curr;
-                head = curr;
-            } else {
-                prev.right = curr;
-                curr.left = prev;
-                prev = curr;
-            }
-            curr = curr.right;
-        }
+//             curr = stack.pop();
+//             if(prev == null) {
+//                 prev = curr;
+//                 head = curr;
+//             } else {
+//                 prev.right = curr;
+//                 curr.left = prev;
+//                 prev = curr;
+//             }
+//             curr = curr.right;
+//         }
         
+//         prev.right = head;
+//         head.left = prev;
+//         return head;
+        
+        if(root == null) return null;
+        
+        Node prev = null, head = null, curr = root;
+        
+        while(curr != null) {
+            if(curr.left != null) {
+                Node ptr = curr.left;
+                while(ptr.right != null && ptr.right != curr) ptr = ptr.right;
+                if(ptr.right == null) {
+                    ptr.right = curr;
+                    curr = curr.left;
+                } else {
+                    ptr.right = null;
+                    if(prev == null) {
+                        prev = curr;
+                        head = curr;
+                    } else {
+                        prev.right = curr;
+                        curr.left = prev;
+                        prev = curr;
+                    }
+                    curr = curr.right;
+                }
+            } else {
+                if(prev == null) {
+                    prev = curr;
+                    head = curr;
+                } else {
+                    prev.right = curr;
+                    curr.left = prev;
+                    prev = curr;
+                }
+                curr = curr.right;
+            }
+        }
         prev.right = head;
         head.left = prev;
         return head;
