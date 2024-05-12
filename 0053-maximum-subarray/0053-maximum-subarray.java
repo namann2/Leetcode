a3cc720618b6653e8ca90a6de7423ceadcaf11a4
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-12 18:55:58 +0530

    Time: 1 ms (99.45%), Space: 56.7 MB (76.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,22 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE, local = 0;
+        // int max = Integer.MIN_VALUE, local = 0;
+        //  -2 1 -3 4 -1 2 1 -5 4
+        //l -2 1 -2 4 3  5 6 1 5
+        //m -2 1 1  4 4  5 6 6 6
+        // for(int num : nums) {
+        //     local += num; // subarray sum ending at current
+        //     if(num > local) local = num; // subarray starting from current
+        //     if(local > max) max = local;
+        // }
+        // return max;
+        
+        int localMax = 0, globalMax = Integer.MIN_VALUE;
         for(int num : nums) {
-            local += num;
-            if(num > local) local = num;
-            if(local > max) max = local;
+            localMax += num;
+            localMax = Math.max(localMax, num);
+            globalMax = Math.max(globalMax, localMax);
         }
-        return max;
+        return globalMax;
     }
 }
