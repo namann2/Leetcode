4d0debdf8d2a76c66d6b6584ddf42fb326435270
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-18 00:15:41 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (97.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,30 +1,35 @@
 class Solution {
+    
+    private static final char OP = '(';
+    private static final char CP = ')';
+    
     public List<String> generateParenthesis(int n) {
         List<String> answer = new ArrayList<>();
         generateParenthesis(n, n, new StringBuilder(), answer);
         return answer;
     }
+    
     private void generateParenthesis(int open, int close, StringBuilder temp, List<String> answer) {
         // base case
         if(open == 0 && close == 0) {
-            answer.add(new String(temp));
+            answer.add(temp.toString());
             return;
         }
-        // main logic
+        
         if(open == close) {
-            temp.append("(");
-            generateParenthesis(open-1, close, temp, answer);
-            temp.deleteCharAt(temp.length()-1);
+            temp.append(OP);
+            generateParenthesis(open - 1, close, temp, answer);
+            temp.deleteCharAt(temp.length() - 1);
         } else {
             if(open > 0) {
-                temp.append("(");
-                generateParenthesis(open-1, close, temp, answer);
-                temp.deleteCharAt(temp.length()-1);
+                temp.append(OP);
+                generateParenthesis(open - 1, close, temp, answer);
+                temp.deleteCharAt(temp.length() - 1);
             }
             if(close > 0) {
-                temp.append(")");
-                generateParenthesis(open, close-1, temp, answer);
-                temp.deleteCharAt(temp.length()-1);       
+                temp.append(CP);
+                generateParenthesis(open, close - 1, temp, answer);
+                temp.deleteCharAt(temp.length() - 1);
             }
         }
     }
