99cdcfc37b50626efa8670a05775d06759e6b923
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-17 23:14:41 +0530

    Time: 47 ms (62.48%), Space: 91.5 MB (85.96%) - LeetHub

--- 0716-max-stack/0716-max-stack.java
+++ 0716-max-stack/0716-max-stack.java
@@ -1,76 +1,86 @@
 class Node {
-    int data;
+    int key;
     Node prev, next;
-    Node(int data) {
-        this.data = data;
+    Node(int key) {
+        this.key = key;
     }
 }
 class MaxStack {
 
-    private Node dH, dT;
     private Map<Integer, List<Node>> map;
-    private PriorityQueue<Integer> pq;
-    private int size;
+    private Node dH, dT;
+    private PriorityQueue<Integer>maxHeap;
+    
     public MaxStack() {
+        map = new HashMap<>();
         dH = new Node(-1);
         dT = new Node(-1);
         dH.next = dT;
         dT.prev = dH;
-        map = new HashMap<>();
-        pq = new PriorityQueue<>((a, b) -> {
-            return b - a;
-        });
-        size = 0;
+        maxHeap = new PriorityQueue<>((a, b) -> b-a);
     }
-    
+
     public void push(int x) {
-        // DLL
         Node newNode = new Node(x);
-        newNode.next = dH.next;
-        dH.next.prev = newNode;
-        dH.next = newNode;
-        newNode.prev = dH;
-        // Map
+        
         map.putIfAbsent(x, new ArrayList<>());
         map.get(x).add(newNode);
-        // pq
-        pq.offer(x);
+        
+        dH.next.prev = newNode;
+        newNode.next = dH.next;
+        dH.next = newNode;
+        newNode.prev = dH;
+        
+        maxHeap.offer(x);
     }
     
     public int pop() {
-        Node nodeToRemove = dH.next;
-        int val = nodeToRemove.data;
-        // DLL
-        dH.next = dH.next.next;
-        dH.next.prev = dH;
-        // Map
-        List<Node> l = map.get(val);
-        l.remove(l.size()-1);
-        if(l.size() > 0)
-            map.put(val, l);
-        // pq
-        pq.remove(val);
-        return val;
+        Node node = dH.next;
+        dH.next = node.next;
+        node.next.prev = dH;
+        
+        List<Node> cnt = map.get(node.key);
+        cnt.remove(cnt.size()-1);
+        if(cnt.size() == 0)
+            map.remove(node.key);
+        else map.put(node.key, cnt);
+        
+        maxHeap.remove(node.key);
+        return node.key;
     }
     
     public int top() {
-        return dH.next.data;
+        return dH.next.key;
     }
     
     public int peekMax() {
-        return pq.peek();
+        return maxHeap.peek();
     }
     
     public int popMax() {
-        // pq
-        int x = pq.poll();
-        // map
-        List<Node> l = map.get(x);
-        Node node = l.get(l.size()-1);
-        l.remove(l.size()-1);
-        // DLL
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+        int x = maxHeap.poll();
+        
+        List<Node> allNodes = map.get(x);
+        Node lastNode = allNodes.get(allNodes.size() - 1);
+        allNodes.remove(allNodes.size()-1);
+        
+        if(allNodes.size() == 0)
+            map.remove(x);
+        else map.put(x, allNodes);
+        
+        lastNode.next.prev = lastNode.prev;
+        lastNode.prev.next = lastNode.next;
+        
         return x;
     }
 }
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
