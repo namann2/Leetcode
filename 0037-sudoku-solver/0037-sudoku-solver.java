80793e18f5d6af167e6cdd515c6ae1ffe2ade1dd
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-17 12:48:03 +0530

    Time: 5 ms (89.78%), Space: 41.4 MB (13.49%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -3,48 +3,46 @@ class Solution {
         if(board == null || board.length == 0) return;
         sudoku(board, 0, 0, 9, 9);
     }
-    private boolean sudoku(char[][] board, int row, int col, int rl, int cl) {
-        
-        // base condition
-        if(col == cl) {
-            row += 1;
-            col = 0;
+    
+    private boolean sudoku(char[][] board, int rc, int cc, int row, int col) {
+        // base case
+        if(cc == col) {
+            cc = 0;
+            rc++;
         }
         
-        if(row == rl) return true;
+        if(rc == row)
+            return true;
         
         // main logic
-        if(board[row][col] != '.') {
-            return sudoku(board, row, col+1, rl, cl);
+        if(board[rc][cc] != '.') {
+            return sudoku(board, rc, cc + 1, row, col);
         } else {
-            for(char i='1';i<='9';i++) {
-                boolean isValid = isValid(board, row, col, i);
+            for(char c = '1'; c <= '9'; c++) {
+                boolean isValid = isValidPlacement(board, rc, cc, c);
                 if(isValid) {
-                    board[row][col] = i;
-                    if(sudoku(board, row, col+1, rl, cl)) return true;
-                    board[row][col] = '.';
+                    board[rc][cc] = c;
+                    if(sudoku(board, rc, cc + 1, row, col)) return true;
+                    board[rc][cc] = '.';
                 }
             }
-            return false;
         }
+        return false;
     }
-    private boolean isValid(char[][] board, int row, int col, char value) {
-        // same row, same col
-        for(int i=0;i<9;i++) {
-            if(board[row][i] == value) return false;
-            if(board[i][col] == value) return false;
+    private boolean isValidPlacement(char[][] board, int rc, int cc, int val) {
+        for(int i = 0 ;i < 9; i++) {
+            if(board[rc][i] == val) return false;
+            if(board[i][cc] == val) return false;
         }
         
-        // same 3*3 grid
-        int blockRow = row / 3 * 3; // start row for this block
-        int blockCol = col / 3 * 3; // start col for this block
-        for(int r = 0;r < 3; r++) {
-            for(int c = 0;c < 3; c++) {
-                if(board[r + blockRow][c + blockCol] == value) return false;
+        int blockRow = rc / 3 * 3;
+        int blockCol = cc / 3 * 3;
+        
+        for(int r = 0; r < 3; r++) {
+            for(int c = 0; c < 3; c++) {
+                if(board[r + blockRow][c + blockCol] == val) return false;
             }
         }
-        
         return true;
     }
-
 }
