566257ce3f351873ab121bdefd05e720ed2bc33a
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-17 22:43:38 +0530

    Time: 6 ms (88.37%), Space: 44.8 MB (41.52%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,49 +1,56 @@
 class Solution {
     public int maxLength(List<String> A) {
+        int n = A.size();
         boolean[]used = new boolean[26];
-
-        int max = 0, n = A.size();
-        for(int i=0;i<n;i++) {
-            if(!hasUniqueCharacaters(A.get(i))) continue;
+        
+        int maxLength = 0;
+        for(int i = 0; i < n; i++) {
+            if(!hasUniqueCharacters(A.get(i))) continue;
             updateUsage(A.get(i), used, true);
-            max = Math.max(max, A.get(i).length() + findMax(A, i+1, n, used));
+            maxLength = Math.max(maxLength, A.get(i).length() + findMaxAhead(A, i+1, used));
             updateUsage(A.get(i), used, false);
         }
-        return max;
+        return maxLength;
     }
-    private int findMax(List<String> A, int index, int n, boolean[] used) {
-        // base case
-        if(index == n) return 0;
-        // main logic
-        int max = 0;
-        for(int i = index;i < n; i++) {
-            if(hasUniqueCharacaters(A.get(i)) && canUse(A.get(i), used)) {
+    
+    private int findMaxAhead(List<String> A, int index, boolean[] used) {
+        if(index == A.size())
+            return 0;
+        
+        int currLength = 0, n = A.size();
+        
+        for(int i = index; i < n; i++) {
+            if(hasUniqueCharacters(A.get(i)) && canUse(A.get(i), used)) {
                 updateUsage(A.get(i), used, true);
-                max = Math.max(max, A.get(i).length() + findMax(A, i+1, n, used)); 
+                currLength = Math.max(currLength, A.get(i).length() + findMaxAhead(A, i + 1, used));
                 updateUsage(A.get(i), used, false);
             }
         }
-        return max;
+        
+        return currLength;
     }
-    private boolean hasUniqueCharacaters(String s) {
+    
+    private void updateUsage(String s, boolean[] used, boolean with) {
+        int currLength = s.length();
+        for(int i = 0; i < currLength; i++) {
+            used[s.charAt(i) - 'a'] = with;
+        }
+    }
+    
+    private boolean hasUniqueCharacters(String s) {
         int[] count = new int[26];
         for(char ch : s.toCharArray()) {
             if(count[ch-'a'] > 0) return false;
             count[ch - 'a']++;
         }
         return true;
-    } // check if there are unique chars in string
-    private void updateUsage(String s, boolean[] used, boolean setTo) {
-        for(int i=0;i<s.length();i++) {
-            int ch = s.charAt(i) - 'a';
-            used[ch] = setTo;
-        }
     }
+    
     private boolean canUse(String s, boolean[] used) {
-        for(int i=0;i<s.length();i++) {
-            int ch = s.charAt(i) - 'a';
-            if(used[ch]) return false;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(used[s.charAt(i)-'a']) return false;
         }
         return true;
-    } // check if there are unique chars between strings
+    }
 }
