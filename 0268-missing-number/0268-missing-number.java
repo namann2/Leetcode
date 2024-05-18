7d07788ad412cbbdf2a5d3ec130bc7aba63387fd
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-18 13:44:28 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (36.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Different approaches to solve this : 
-        // xor and binary search approch is better
-        Arrays.sort(nums);
-        int start = 0, end = nums.length-1, ans = -1;
-        while(start <= end) {
-            int mid = (start + end) >> 1;
-            if(nums[mid] == mid) { // there is no missing uptil this point
-                start = mid + 1;
-            } else {
-                ans = mid;
-                end = mid - 1;
-            }
+        int n = nums.length;
+        int xor = 0;
+        for(int i = 1; i <= n; i++) {
+            xor ^= i ^ nums[i-1];
         }
-        return ans == -1 ? nums.length : ans;
+        return xor;
     }
 }
+/*
+    1 3 2 0 3 1
+    2 0
+    2
+*/
