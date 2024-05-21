3a2ff6956abe2ccf2e894aa493413fbb130407d2
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-22 01:30:49 +0530

    Time: 29 ms (82.99%), Space: 58.7 MB (59.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> q = new ArrayDeque<>();
-        int n = nums.length, idx = 0;
+        int n = nums.length;
+        int end = 0, begin = 0, idx = 0;
         int[] ans = new int[n-k+1];
-        // we maintain a monotonic queue
-        // we care about the max in a k range, but what if
-        // while moving the window ahead, the max element also vanishes in the prev window ?
-        // for this we need to store the max and second max and third....
-        // matter is : We need to store numbers smaller than the current max bcz
-        // they have the potential to become the max in next window.
-        int begin = 0, end = 0;
+        Deque<Integer> q = new ArrayDeque<>();
         while(end < n) {
-            while(!q.isEmpty() && nums[end] > nums[q.peekLast()]) q.removeLast();
-            q.addLast(end);
-            if(end-begin+1 == k) {
-                ans[idx++] = nums[q.peekFirst()];
-                if(!q.isEmpty() && begin == q.peekFirst()) q.removeFirst();
-                begin++;
+            while(!q.isEmpty() && q.peekLast() < nums[end])
+                q.removeLast();
+            q.addLast(nums[end]);
+            if(end - begin + 1 == k) {
+                ans[idx++] = q.peekFirst();
+                if(q.peekFirst() == nums[begin++]) q.removeFirst();
             }
             end++;
         }
         return ans;
     }
 }
-
