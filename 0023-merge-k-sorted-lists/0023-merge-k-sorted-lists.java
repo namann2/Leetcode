ff1ae50a2c710c96d3135f1388d36388188047fe
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-22 02:30:46 +0530

    Time: 2 ms (81.16%), Space: 44.7 MB (14.62%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,18 +1,20 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        // consider every list as an element in an array for a merge sort
         if(lists == null || lists.length == 0) return null;
-        return mergeSort(lists, 0, lists.length - 1); // n log k
+        int n = lists.length;
+        return merge(lists, 0, n-1);
     }
-    private ListNode mergeSort(ListNode[] lists, int leftStart, int rightEnd) {
-        if(leftStart == rightEnd) return lists[leftStart];
-        
-        int mid = leftStart + (rightEnd - leftStart) / 2;
-        
-        ListNode a = mergeSort(lists, leftStart, mid);
-        ListNode b = mergeSort(lists, mid + 1, rightEnd);
+    
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if(left == right) {
+            return lists[left];
+        }
+        int mid = left + (right - left) / 2;
+        ListNode a = merge(lists, left, mid);
+        ListNode b = merge(lists, mid+1, right);
         return mergeHalves(a, b);
     }
+    
     private ListNode mergeHalves(ListNode a, ListNode b) {
         if(a == null && b == null) return null;
         if(a == null || b == null) return a == null ? b : a;
