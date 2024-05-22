0808a36c3f498d53e95f3d20a1e4d286b12d5aff
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-22 23:06:36 +0530

    Time: 6 ms (79.43%), Space: 46.1 MB (31.76%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        int[]line_sweep = new int[(int)1e6+1];
-        for(int[] interval : intervals) {
-            line_sweep[interval[0]] += 1;
-            line_sweep[interval[1]] -= 1;
-        }
+        if(intervals == null || intervals.length == 0)
+            return true;
+        Arrays.sort(intervals, (i1, i2) -> {
+            if(i1[0] == i2[0])
+                return i1[1] - i2[1];
+            return i1[0] - i2[0];
+        });
         
-        if(line_sweep[0] > 1) return false;
-        
-        for(int i = 1; i < line_sweep.length; i++) {
-            line_sweep[i] += line_sweep[i-1];
-            if(line_sweep[i] > 1) return false;
+        int meetingRoom = 1, endTime = intervals[0][1];
+        int n = intervals.length;
+        for(int i = 1; i < n; i++) {
+            if(intervals[i][0] < endTime) {
+                return false;
+            } else endTime = Math.max(endTime, intervals[i][1]);
         }
-        return true;
+        return true ;
     }
 }
