3002fda611bbeecc905bc5fa4320c4be591f0b2a
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-22 23:25:37 +0530

    Time: 3 ms (99.91%), Space: 45.8 MB (74.40%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,33 +1,8 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // Approach 1 
-        // TC : O(nlogn), SC : O(n)
-        
-        /*
-        Arrays.sort(intervals, (i1, i2) -> {
-            if(i1[0] == i2[0]) return i1[1] - i2[1];
-            return i1[0] - i2[0];
-        });
-        
-        int meetingRoom = 0, n = intervals.length;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        minHeap.offer(intervals[0][1]);
-        for(int i = 0; i < n; i++) {
-            if(!minHeap.isEmpty() && intervals[i][0] < minHeap.peek()) {
-                meetingRoom++;
-                minHeap.offer(intervals[i][1]);
-            } else {
-                minHeap.remove();
-            }
-        }
-        return meetingRoom;
-        */
-        // Approach 2
         int n = intervals.length;
         int[] start = new int[n];
         int[] end = new int[n];
-        
         int idx = 0;
         for(int[] interval : intervals) {
             start[idx] = interval[0];
@@ -38,15 +13,11 @@ class Solution {
         Arrays.sort(start);
         Arrays.sort(end);
         
-        int meetingRoom = 0, startIndex = 0, endIndex = 0;
-        while(startIndex < n) {
-            if(start[startIndex] < end[endIndex]) {
+        int prev = 0, meetingRoom = 1;
+        for(int i = 1; i < n; i++) {
+            if(start[i] < end[prev]) {
                 meetingRoom++;
-                startIndex++;
-            } else {
-                startIndex++;
-                endIndex++;
-            }
+            } else prev++;
         }
         return meetingRoom;
     }
