0ea266027280feb3f355867bb5b9ee0a30c27c10
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-24 00:35:38 +0530

    Time: 23 ms (35.36%), Space: 63 MB (27.13%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,27 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) 
-            map.put(num, map.getOrDefault(num, 0)+1);
-        
-        int cnt = 0;
-        for(int num : map.keySet()) {
-            int freq = map.get(num);
-            if(freq == 1) return -1;
-            /* 
-                if freq is divisible by 2 but not by 3, we will pick 2 numbers 
-                and since, any even number that is not divisible by 3 and is 
-                divisible by 2, is of the form 2n + 2, for eg :  8, 14 etc..
-                so if we take 2 numbers from them, it becomes possible to 
-                pick 3 numbers.
-            */
-            if(freq % 2 == 0 && freq % 3 != 0) { 
-                cnt++;
-                freq-=2;
-            }
-            // if remain numbers can be picked up 3, else pick a pair.
-            cnt += freq / 3 + (freq % 3 > 0 ? 1 : 0);
+        int n = nums.length;
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-        return cnt;
+        
+        int operations = 0;
+        for(int num : count.keySet()) {
+            int freq = count.get(num);
+            if(freq == 1) return -1;
+            operations += freq / 3 + (freq % 3 > 0 ? 1 : 0);
+        }
+        return operations;
     }
 }
+
+// {2 : 4, 3 : 3, 4 : 2}
+// 2 + 1 + 1
