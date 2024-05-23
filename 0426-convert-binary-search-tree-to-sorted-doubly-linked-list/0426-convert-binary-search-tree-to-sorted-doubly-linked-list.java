1ae0b819ac3a3f35d8fb5f5c71907f015a4af049
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-23 23:12:19 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (36.73%) - LeetHub

--- 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,68 +1,30 @@
 class Solution {
     public Node treeToDoublyList(Node root) {
-//         if(root == null) return null;
-//         Node prev = null, curr = root, head = null;
-//         Stack<Node> stack = new Stack<>();
+        if(root == null) 
+            return null;
+        Deque<Node> stack = new ArrayDeque<>();
+        Node curr = root;
+        Node head = null, prev = null;
         
-//         while(!stack.isEmpty() || curr != null) {
-//             while(curr != null) {
-//                 stack.push(curr);
-//                 curr = curr.left;
-//             }
-            
-//             curr = stack.pop();
-//             if(prev == null) {
-//                 prev = curr;
-//                 head = curr;
-//             } else {
-//                 prev.right = curr;
-//                 curr.left = prev;
-//                 prev = curr;
-//             }
-//             curr = curr.right;
-//         }
-        
-//         prev.right = head;
-//         head.left = prev;
-//         return head;
-        
-        if(root == null) return null;
-        
-        Node prev = null, head = null, curr = root;
-        
-        while(curr != null) {
-            if(curr.left != null) {
-                Node ptr = curr.left;
-                while(ptr.right != null && ptr.right != curr) ptr = ptr.right;
-                if(ptr.right == null) {
-                    ptr.right = curr;
-                    curr = curr.left;
-                } else {
-                    ptr.right = null;
-                    if(prev == null) {
-                        prev = curr;
-                        head = curr;
-                    } else {
-                        prev.right = curr;
-                        curr.left = prev;
-                        prev = curr;
-                    }
-                    curr = curr.right;
-                }
-            } else {
-                if(prev == null) {
-                    prev = curr;
-                    head = curr;
-                } else {
-                    prev.right = curr;
-                    curr.left = prev;
-                    prev = curr;
-                }
-                curr = curr.right;
+        while(!stack.isEmpty() || curr != null) {
+            while(curr != null) {
+                stack.addLast(curr);
+                curr = curr.left;
             }
+            curr = stack.removeLast();
+            if(prev == null) {
+                head = curr;
+                prev = curr;
+            } else {
+                prev.right = curr;
+                curr.left = prev;
+                prev = curr;
+            }
+            curr = curr.right;
         }
         prev.right = head;
         head.left = prev;
+        
         return head;
     }
 }
