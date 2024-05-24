64c55a9b55f7b45f3c69b9d2191162d467adcade
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-25 00:22:30 +0530

    Time: 31 ms (64.60%), Space: 51.3 MB (66.65%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
-        Set<List<Integer>> ans = new HashSet<>();
-        Set<Integer> starts = new HashSet<>();
-        // required sum with i as starting point of the triplet, 
-        // to keep a check that the triplet we are formulating
-        // starts with index >= i
-        Map<Integer,Integer> requiredSumWithI = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            // check if unique start point
-            if(starts.add(nums[i])) {
-                for(int j = i + 1; j < n; j++) {
-                    // the required number
-                    int third = -(nums[i] + nums[j]);
-                    if(requiredSumWithI.containsKey(third) && requiredSumWithI.get(third) >= i) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
-                        // sorting a triplet 
-                        Collections.sort(triplet);
-                        ans.add(triplet);
-                    }
-                    // update the index of the number to check 
-                    // if this can contribute for a triplet ahead
-                    requiredSumWithI.put(nums[j], i);
-                }
+        Arrays.sort(nums);
+        int i = 0;
+        while(i < n) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                i++;
+                continue;
             }
+            int left = i + 1, right = n-1;
+            while(left < right) {
+                int csum = nums[i] + nums[left] + nums[right];
+                if(csum == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(right > left && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if(csum > 0) right--;
+                else left++;
+            }
+            i++;
         }
-        return new ArrayList<>(ans);
+        return result;
     }
 }
