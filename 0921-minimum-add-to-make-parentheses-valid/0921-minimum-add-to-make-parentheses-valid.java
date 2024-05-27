464a609a7c7ac15a0aa178f83746b8f3acf92a8d
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-28 01:20:08 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (83.55%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -2,12 +2,21 @@ class Solution {
     public int minAddToMakeValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         int n = s.length();
+        // for(int i = 0; i < n; i++) {
+        //     char ch = s.charAt(i);
+        //     if(ch == '(') stack.addLast(ch);
+        //     else if(ch == ')' && !stack.isEmpty() && stack.peekLast() == '(') stack.removeLast();
+        //     else stack.addLast(ch);
+        // }
+        // return stack.size();
+        
+        int open = 0, close = 0;
         for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(ch == '(') stack.addLast(ch);
-            else if(ch == ')' && !stack.isEmpty() && stack.peekLast() == '(') stack.removeLast();
-            else stack.addLast(ch);
+            if(ch == '(') open++;
+            else if(ch == ')' && open > 0) open--;
+            else close ++;
         }
-        return stack.size();
+        return open + close;
     }
 }
