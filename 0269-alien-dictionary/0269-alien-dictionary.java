2ab8b6ae3f89b182244803e4cd64de8a716db232
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-30 01:02:11 +0530

    Time: 3 ms (97.03%), Space: 41.7 MB (66.13%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,63 +1,79 @@
+enum COLOR {
+    WHITE, GREY, BLACK;    
+}
+
 class Solution {
+    
+    private static final String INVALID_EMPTY = "";
+    
     public String alienOrder(String[] words) {
+        if(words == null || words.length == 0)
+            return INVALID_EMPTY;
+        
+        // get count of all the characters in the word
+        boolean[] diffChars = new boolean[26];
+        for(String w : words)
+            for(char ch : w.toCharArray())
+                diffChars[ch-'a'] = true;
+        
         Map<Character, List<Character>> g = new HashMap<>();
-        Map<Character, Integer> indegree = new HashMap<>();
+        boolean isValid = checkDictionaryValid(g, words);
         
-        for(String word : words) {
-            int n = word.length();
-            for(int i = 0; i < n; i++) {
-                char ch = word.charAt(i);
-                indegree.put(ch, 0);
-            }
+        if(!isValid)
+            return INVALID_EMPTY;
+        
+        StringBuilder answer = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        COLOR[] visited = new COLOR[26];
+        Arrays.fill(visited, COLOR.WHITE);
+        
+        for(int i = 0; i < 26; i++) {
+            if(diffChars[i] && visited[i] == COLOR.WHITE)
+                if(dfs(g, (char)(i + 'a'), stack, visited, diffChars))
+                    return INVALID_EMPTY;
         }
         
-        boolean valid = constructGraph(g, words, indegree);
+        while(!stack.isEmpty())
+            answer.append(stack.removeLast());
         
-        if(!valid) return "";
-        
-        StringBuilder ans = new StringBuilder();
-        Deque<Character> q = new ArrayDeque<>();
-        for(char ch : indegree.keySet()) {
-            if(indegree.get(ch) == 0) {
-                q.addLast(ch);
-            }
-        }
-        
-        if(q.size() == 0) return "";
-        
-        while(!q.isEmpty()) {
-            char u = q.removeFirst();
-            ans.append(u);
-            if(!g.containsKey(u)) continue;
-            for(char v : g.get(u)) {
-                indegree.put(v, indegree.getOrDefault(v, 0) - 1);
-                if(indegree.get(v) == 0)
-                    q.addLast(v);
-            }
-        }
-        
-        if(ans.length() != indegree.size()) return "";
-        
-        return ans.toString();
+        return answer.toString();
     }
-    private boolean constructGraph(Map<Character, List<Character>> g, String[] words, Map<Character, Integer> indegree) {
+    
+    private boolean dfs(Map<Character, List<Character>> g, char u, Deque<Character> stack, COLOR[] visited, boolean[] diffChars) {
+        
+        visited[u-'a'] = COLOR.GREY;
+        
+        if(g.containsKey(u)) {
+            for(char v : g.get(u)) {
+                if(visited[v-'a'] == COLOR.GREY) 
+                    return true;
+                if(visited[v-'a'] == COLOR.WHITE)
+                    if(dfs(g, v, stack, visited, diffChars))
+                        return true;
+            }
+        }
+        
+        visited[u-'a'] = COLOR.BLACK;
+        stack.addLast(u);
+        return false;
+    }
+    
+    private boolean checkDictionaryValid(Map<Character, List<Character>> g, String[] words) {
         int n = words.length;
-        for(int i = 0; i < n - 1; i++) {
-            String w1 = words[i];
-            String w2 = words[i+1];
-            
-            if(w1.equals(w2)) continue;
+        
+        for(int i = 0; i < n-1; i++) {
+            String w1 = words[i], w2 = words[i+1];
             int minLength = Math.min(w1.length(), w2.length());
             
+            if(w1.equals(w2)) continue;
             if(w2.length() == minLength && w1.startsWith(w2)) return false;
             
             for(int j = 0; j < minLength; j++) {
-                char c1 = w1.charAt(j);
-                char c2 = w2.charAt(j);
-                if(c1 == c2) continue;
-                g.putIfAbsent(c1, new ArrayList<>());
-                g.get(c1).add(c2);
-                indegree.put(c2, indegree.getOrDefault(c2, 0) + 1);
+                char ch1 = w1.charAt(j), ch2 = w2.charAt(j);
+                if(ch1 == ch2) continue;
+                g.putIfAbsent(ch1, new ArrayList<>());
+                g.get(ch1).add(ch2);
                 break;
             }
         }
