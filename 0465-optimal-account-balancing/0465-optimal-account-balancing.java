b4e3500ca0f6463a5852001b1ff8cbbafa1cc29b
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-05-30 00:05:16 +0530

    Time: 6 ms (83.39%), Space: 40.8 MB (52.92%) - LeetHub

--- 0465-optimal-account-balancing/0465-optimal-account-balancing.java
+++ 0465-optimal-account-balancing/0465-optimal-account-balancing.java
@@ -1,41 +1,38 @@
 class Solution {
     public int minTransfers(int[][] transactions) {
         Map<Integer, Integer> trans = new HashMap<>();
+        
         for(int[] transaction : transactions) {
             int from = transaction[0], to = transaction[1], amount = transaction[2];
             trans.put(from, trans.getOrDefault(from, 0) - amount);
             trans.put(to, trans.getOrDefault(to, 0) + amount);
         }
         
-        List<Integer> remain = new ArrayList<>();
-        for(int transaction : trans.keySet())
-            if(trans.get(transaction) != 0)
-                remain.add(trans.get(transaction));
-        
-        int n = remain.size();
-        int[] A = new int[n];
-        for(int i = 0; i < n; i++)
-            A[i] = remain.get(i);
-        
-        return solve(A, 0, n);
+        int size = trans.size();
+        int[] needed = new int[size];
+        int idx = 0;
+        for(int key : trans.keySet()) {
+            needed[idx++] = trans.get(key);
+        }
+        return settleAmount(needed, 0, size);
     }
     
-    private int solve(int[] A, int index, int n) {
-        if(index == n) return 0;
+    private int settleAmount(int[] needed, int index, int n) {
+        if(index == n)
+            return 0;
         
-        if(A[index] == 0)
-            return solve(A, index + 1, n);
+        if(needed[index] == 0)
+            return settleAmount(needed, index + 1, n);
         
-        int min = Integer.MAX_VALUE;
-        // A[index] ko kahan kahan use krke settlement kiya ja sakta hai
+        int minTransactions = Integer.MAX_VALUE;
         for(int i = index + 1; i < n; i++) {
-            if(A[index] * A[i] < 0) {
-                A[i] += A[index];
-                int curr = 1 + solve(A, index + 1, n);
-                min = Math.min(min, curr);
-                A[i] -= A[index];
+            if(needed[index] * needed[i] < 0) {
+                needed[i] += needed[index];
+                minTransactions = Math.min(minTransactions, 1 + settleAmount(needed, index + 1, n));
+                needed[i] -= needed[index];
             }
         }
-        return min;
+        
+        return minTransactions;
     }
 }
