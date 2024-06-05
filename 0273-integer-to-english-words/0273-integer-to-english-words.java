bd8e017cbd7532e50769cb253065059adae920b2
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-05 14:51:39 +0530

    Time: 8 ms (55.12%), Space: 42 MB (65.98%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,29 +1,34 @@
 class Solution {
     
-    private static String[] LESS_THAN_TEN = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private static String[] LESS_THAN_TWENTY = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private static String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] LESS_THAN_TEN = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String[] LESS_THAN_TWNETY = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String HUNDRED = " Hundred ";
+    private static final String THOUSAND = " Thousand ";
+    private static final String MILLION = " Million ";
+    private static final String BILLION = " Billion ";
     
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
-        return wordsHelper(num);
+        return wordHelper(num);
     }
     
-    private String wordsHelper(int num) {
+    private String wordHelper(int num) {
         String result = new String();
-        if(num < 10) // 7
+        if(num < 10)
             result = LESS_THAN_TEN[num];
-        else if(num < 20) // 1
-            result = LESS_THAN_TWENTY[num - 10];
-        else if(num < 100) // 56
-            result = TENS[num / 10] + " " + wordsHelper(num % 10);
-        else if(num < 1000) // 678
-            result = wordsHelper(num / 100) + " Hundred " + wordsHelper(num % 100);
-        else if(num < 1000000) // this range will cover all number b/w 1001 - 999999
-            result = wordsHelper(num / 1000) + " Thousand " + wordsHelper(num % 1000);
-        else if(num < 1000000000) 
-            result = wordsHelper(num / 1000000) + " Million " + wordsHelper(num % 1000000);
-        else result = wordsHelper(num / 1000000000) + " Billion " + wordsHelper(num % 1000000000);
+        else if(num < 20)
+            result = LESS_THAN_TWNETY[num - 10];
+        else if(num < 100)
+            result = TENS[num / 10] + " " + LESS_THAN_TEN[num % 10];
+        else if(num < 1000)
+            result = LESS_THAN_TEN[num / 100] + HUNDRED + wordHelper(num % 100);
+        else if(num < 1000000)
+            result = wordHelper(num / 1000) + THOUSAND + wordHelper(num % 1000);
+        else if(num < 1000000000)
+            result = wordHelper(num / 1000000) + MILLION + wordHelper(num % 1000000);
+        else 
+            result = wordHelper(num / 1000000000) + BILLION + wordHelper(num % 1000000000);
         return result.trim();
     }
 }
