6621aaf21e285ac3ba6e7b0b95070a2f568654a9
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-13 22:17:01 +0530

    Time: 24 ms (76.24%), Space: 56 MB (53.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,37 +1,37 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         int n = nums.length;
-        mergeSort(nums, 0, n-1, new int[n]);
+        if(n == 1) return nums;
+        mergeSort(nums, new int[n], 0, n-1);
         return nums;
     }
-    private void mergeSort(int[] nums, int leftStart, int rightEnd, int[] temp) {
-        if(leftStart == rightEnd) 
+    
+    private void mergeSort(int[] nums, int[] temp, int leftStart, int rightEnd) {
+        // base case
+        if(leftStart >= rightEnd) 
             return;
-        
+        // main logic
         int mid = leftStart + (rightEnd - leftStart) / 2;
-        
-        mergeSort(nums, leftStart, mid, temp);
-        mergeSort(nums, mid + 1, rightEnd, temp);
-        mergeHalves(nums, leftStart, mid, rightEnd, temp);
+        mergeSort(nums, temp, leftStart, mid);
+        mergeSort(nums, temp, mid + 1, rightEnd);
+        mergeHalves(nums, temp, leftStart , rightEnd);
     }
-    private void mergeHalves(int[] nums, int leftStart, int leftEnd, int rightEnd, int[] temp) {
+    
+    private void mergeHalves(int[] nums, int[] temp, int leftStart, int rightEnd) {
+        int leftEnd = leftStart + (rightEnd - leftStart) / 2;
         int rightStart = leftEnd + 1;
-        int left = leftStart, right = rightStart, index = leftStart;
         
+        int left = leftStart, right = rightStart, index = leftStart;
         while(left <= leftEnd && right <= rightEnd) {
             if(nums[left] <= nums[right]) {
-                temp[index] = nums[left];
-                left++;
-            } else {
-                temp[index] = nums[right];
-                right++;
-            }
-            index++;
+                temp[index++] = nums[left++];
+            } else temp[index++] = nums[right++];
         }
         
         while(left <= leftEnd) temp[index++] = nums[left++];
         while(right <= rightEnd) temp[index++] = nums[right++];
-        for(int i=leftStart;i<=rightEnd;i++)
+        
+        for(int i = leftStart; i <= rightEnd; i++)
             nums[i] = temp[i];
     }
 }
