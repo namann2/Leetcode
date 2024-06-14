e320949da30dda3472e64cd4be8977c8492c2d6e
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-14 11:03:08 +0530

    Time: 6 ms (73.12%), Space: 44.3 MB (74.74%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
@@ -3,14 +3,12 @@ class Solution {
         UnionFind uf = new UnionFind();
         uf.makeSet(n);
         
-        
         Arrays.sort(logs, (l1, l2) -> {
             return l1[0] - l2[0];
         });
         
         for(int[] log : logs) {
-            uf.union(log[1], log[2]);
-            if(uf.getSize() == 1) return log[0];
+            if(uf.union(log) && uf.size == 1) return log[0];
         }
         
         return -1;
@@ -18,44 +16,49 @@ class Solution {
 }
 
 class Node {
-    int index, rank;
+    int data, rank;
     Node parent;
+    
+    public Node() {}
+    
+    public Node(int data, int rank, Node parent) {
+        this.data = data;
+        this.rank = rank;
+        this.parent = parent;
+    }
 }
 
 class UnionFind {
-    private HashMap<Integer, Node> map;
-    private int size;
+    Map<Integer, Node> map;
+    int size;
     UnionFind() {
-        this.map = new HashMap<>();
-        this.size = 0;
+        map = new HashMap<>();
+        size = 0;
     }
+    
     public void makeSet(int n) {
-        for(int i = 0; i < n ; i ++) {
-            Node newNode = new Node();
-            newNode.index = i;
-            newNode.rank = 0;
+        for(int i = 0; i < n; i++) {
+            Node newNode = new Node(i, 0, null);
             newNode.parent = newNode;
             map.put(i, newNode);
         }
-        this.size = n;
+        size = n;
     }
-    public int getSize() {
-        return this.size;
-    }
-    public int findSet(int a) {
-        return findSet(map.get(a)).index;
-    }
+    
     private Node findSet(Node node) {
         if(node == null || node.parent == node) return node;
-        Node newNode = findSet(node.parent);
-        node.parent = newNode;
-        return newNode;
+        Node newParent = findSet(node.parent);
+        node.parent = newParent;
+        return newParent;
     }
-    public boolean union(int a,int b) {
-        Node pA = findSet(map.get(a));
-        Node pB = findSet(map.get(b));
+    
+    public boolean union(int[] log) {
+        int ts = log[0], node1 = log[1], node2 = log[2];
+        Node pA = findSet(map.get(node1));
+        Node pB = findSet(map.get(node2));
         
         if(pA == pB) return false;
+        
         if(pA.rank >= pB.rank) {
             pB.parent = pA;
             pA.rank = pA.rank == pB.rank ? pA.rank : pA.rank + 1;
@@ -63,7 +66,7 @@ class UnionFind {
             pA.parent = pB;
             pB.rank++;
         }
-        size--;
+        this.size--;
         return true;
     }
 }
