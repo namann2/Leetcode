d247a5ee3bd0bc43b34ba31c15854041600d86d8
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-17 16:16:46 +0530

    Time: 75 ms (84.14%), Space: 76.3 MB (40.43%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -0,0 +1,45 @@
+class NumArray {
+
+    int n;
+    int[] nums, bit;
+    public NumArray(int[] nums) {
+        this.n = nums.length;
+        this.nums = nums;
+        this.bit = new int[n+1];
+        buildTree();
+    }
+    
+    private void buildTree() {
+        for(int i = 0; i < n; i++) {
+            add(i, nums[i]);
+        }    
+    }
+    
+    private void add(int index, int val) {
+        index ++;
+        while(index <= n) {
+            bit[index] += val;
+            index += (index & -index);
+        }
+    }
+    
+    private int getSum(int index) {
+        int sum = 0;
+        index ++;
+        while(index > 0) {
+            sum += bit[index];
+            index -= (index & -index);
+        }
+        return sum;
+    }
+    
+    public void update(int index, int val) {
+        int diff = -nums[index] + val;
+        nums[index] = val;
+        add(index, diff);
+    }
+    
+    public int sumRange(int left, int right) {
+        return getSum(right) - getSum(left - 1);
+    }
+}
