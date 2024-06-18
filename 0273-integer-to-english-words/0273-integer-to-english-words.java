4b91c15c0ed113f67a2da412a9602a947b7f4e7e
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-18 12:08:20 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (76.23%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,34 +1,37 @@
 class Solution {
     
     private static final String[] LESS_THAN_TEN = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private static final String[] LESS_THAN_TWNETY = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String HUNDRED = " Hundred ";
-    private static final String THOUSAND = " Thousand ";
-    private static final String MILLION = " Million ";
-    private static final String BILLION = " Billion ";
+        private static final String[] LESS_THAN_TWENTY = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        
+        private static final String HUNDRED = " Hundred ";
+        private static final String THOUSAND = " Thousand ";
+        private static final String MILLION = " Million ";
+        private static final String BILLION = " Billion ";
+        private static final String DEL = " ";
     
     public String numberToWords(int num) {
-        if(num == 0) return "Zero";
-        return wordHelper(num);
+        if(num == 0)
+            return "Zero";
+        return numberToWordsHelper(num);
     }
     
-    private String wordHelper(int num) {
-        String result = new String();
+    private String numberToWordsHelper(int num) {
+        StringBuilder result = new StringBuilder();
         if(num < 10)
-            result = LESS_THAN_TEN[num];
-        else if(num < 20)
-            result = LESS_THAN_TWNETY[num - 10];
-        else if(num < 100)
-            result = TENS[num / 10] + " " + LESS_THAN_TEN[num % 10];
-        else if(num < 1000)
-            result = LESS_THAN_TEN[num / 100] + HUNDRED + wordHelper(num % 100);
-        else if(num < 1000000)
-            result = wordHelper(num / 1000) + THOUSAND + wordHelper(num % 1000);
-        else if(num < 1000000000)
-            result = wordHelper(num / 1000000) + MILLION + wordHelper(num % 1000000);
-        else 
-            result = wordHelper(num / 1000000000) + BILLION + wordHelper(num % 1000000000);
-        return result.trim();
+            result.append(LESS_THAN_TEN[num]);
+        else if(num < 20) // 14
+            result.append(LESS_THAN_TWENTY[num - 10]);
+        else if(num < 100) // 67
+            result.append(TENS[num / 10]).append(DEL).append(numberToWordsHelper(num % 10));
+        else if(num < 1000) // 789
+            result.append(numberToWordsHelper(num / 100)).append(HUNDRED).append(numberToWordsHelper(num % 100));
+        else if(num < 1000000) // 1000 - 99999
+            result.append(numberToWordsHelper(num / 1000)).append(THOUSAND).append(numberToWordsHelper(num % 1000));
+        else if(num < 1000000000) //
+            result.append(numberToWordsHelper(num / 1000000)).append(MILLION).append(numberToWordsHelper(num % 1000000));
+        else
+            result.append(numberToWordsHelper(num / 1000000000)).append(BILLION).append(numberToWordsHelper(num % 1000000000));
+        return result.toString().trim();
     }
 }
