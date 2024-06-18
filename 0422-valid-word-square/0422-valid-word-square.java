9805eea37591cb78805fc743dd712a23c565a179
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-18 13:02:13 +0530

    Time: 5 ms (88.86%), Space: 43.1 MB (14.25%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.java
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean validWordSquare(List<String> words) {
-
         int n = words.size();
         for(int i = 0; i < n; i++) {
-            String rowWord = words.get(i);
-            String colWord = getColumnWord(words, i, n);
-            if(!rowWord.equals(colWord))
+            String currWord = words.get(i);
+            String colWord = getWord(words, n, i);
+            if(!currWord.equals(colWord))
                 return false;
         }
         return true;
     }
     
-    private String getColumnWord(List<String> words, int index, int n) {
+    private String getWord(List<String> words, int n, int index) {
         StringBuilder colWord = new StringBuilder();
         for(int i = 0; i < n; i++) {
             String currWord = words.get(i);
-            if(index < currWord.length())
+            if(index < currWord.length()) 
                 colWord.append(currWord.charAt(index));
-            else break;
         }
         return colWord.toString();
     }
