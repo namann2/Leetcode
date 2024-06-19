f1a4039c936c953069299235a514721d0db360ec
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-19 17:29:56 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (93.10%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -2,24 +2,20 @@ class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode smaller = new ListNode(-1);
         ListNode greaterEqual = new ListNode(-1);
-        ListNode sH = smaller, gH = greaterEqual;
+        ListNode sH = smaller, gH = greaterEqual, next = null;
         
         while(head != null) {
             if(head.val < x) {
                 smaller.next = head;
                 smaller = smaller.next;
-                ListNode next = head.next;
-                head.next = null;
-                head = next;
             } else {
                 greaterEqual.next = head;
                 greaterEqual = greaterEqual.next;
-                ListNode next = greaterEqual.next;
-                head.next = null;
-                head = next;
             }
+            head = head.next;
         }
         
+        greaterEqual.next = null;
         smaller.next = gH.next;
         return sH.next;
     }
