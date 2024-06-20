134a3e60c5cc2e9fb7960baf55b8877ca87a50e8
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-20 12:57:29 +0530

    Time: 2 ms (40.29%), Space: 45.2 MB (7.18%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,39 +1,41 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
-        List<int[]> intervalsList = new ArrayList<>();
-        for(int[] interval : intervals)
-            intervalsList.add(interval);
+        List<int[]> ans = new ArrayList<>();
         
-        intervalsList.add(newInterval);
+        // insert in begining and middle
+        int it = 0;
+        while(it < n) {
+            // insertion based on start time of the interval
+            if(newInterval[0] < intervals[it][0]) ans.add(newInterval);
+            ans.add(intervals[it++]);
+        }
         
-        Collections.sort(intervalsList, (i1, i2) -> {
-            return i1[0] - i2[0];
-        });
-        
-        return mergeIntervals(intervalsList);
+        // insert at end
+        if(ans.size() == n) ans.add(newInterval);
+        return mergeIntervals(ans);
     }
     
     private int[][] mergeIntervals(List<int[]> intervals) {
         int n = intervals.size();
         List<int[]> merged = new ArrayList<>();
-        merged.add(intervals.get(0));
         
+        merged.add(intervals.get(0));
         for(int i = 1; i < n; i++) {
             int[] interval = intervals.get(i);
             int lastIndex = merged.size() - 1;
-            int[] last = intervals.get(lastIndex);
-            // if no overlap in intervals last[0]...last[1]....intervals[0]...intervals[1]
-            if(interval[0] > last[1]) 
-                merged.add(interval);
+            int[] last = merged.get(lastIndex);
+            // if no overlap ..last[0]...last[1]...interval[0]..interval[1]
+            if(interval[0] > last[1]) merged.add(interval);
             else {
                 last[1] = Math.max(interval[1], last[1]);
                 merged.set(lastIndex, last);
             }
         }
+        
         n = merged.size();
         int[][] R = new int[n][2];
-        
+        int idx = 0;
         for(int i = 0; i < n; i++)
             R[i] = merged.get(i);
         
