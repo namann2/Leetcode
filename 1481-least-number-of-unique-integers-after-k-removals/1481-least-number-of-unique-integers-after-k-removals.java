5bf1dfeb7038165ae1b5d5ddaf63396eb1efbb09
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-20 17:02:06 +0530

    Time: 53 ms (50.00%), Space: 56.9 MB (66.69%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -6,23 +6,13 @@ class Solution {
         for(int a : A)
             cnt.put(a, cnt.getOrDefault(a, 0) + 1);
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return a[1] - b[1];    
-        });
-        
-        for(int num : cnt.keySet()) {
-            pq.offer(new int[]{num, cnt.get(num)});
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(cnt.values());
         
         while(pq.size() > 0 && k > 0) {
-            int[] currCnt = pq.poll();
-            if(currCnt[1] <= k) {
-                k -= currCnt[1];
-            } else {
-                currCnt[1] -= k;
-                k = 0;
-                pq.offer(currCnt);
-            }
+            int currCnt = pq.poll();
+            k -= currCnt;
+            if(k < 0)
+                return pq.size() + 1;
         }
         
         return pq.size();
