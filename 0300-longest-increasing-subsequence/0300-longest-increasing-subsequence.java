3a6299dad5f06f576cb6a644c6d89d9d8c24d334
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-20 16:13:36 +0530

    Time: 7 ms (83.79%), Space: 44.2 MB (47.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -6,9 +6,7 @@ class Solution {
             if(list.size() == 0 || num > list.get(list.size()-1)) list.add(num);
             else {
                 int index = binarySearch(list, num);
-                // TODO : handling
                 if(index < list.size()) list.set(index, num);
-                else list.add(num);
             }
         }
         return list.size();
@@ -16,16 +14,14 @@ class Solution {
     
     private int binarySearch(List<Integer> list, int num) {
         int n = list.size();
-        int start = 0, end = n-1, ans = n;
+        int start = 0, end = n-1;
+        // 1 3 5 6, 5
         while(start <= end) {
             int mid = start + (end - start) / 2;
-            // 1 3 4 5 6 7 
-            // 2
-            if(num <= list.get(mid)) {
-                ans = mid;
-                end = mid - 1;
-            } else start = mid + 1;
+            if(num > list.get(mid)) {
+                start = mid + 1;
+            } else end = mid - 1;
         }
-        return ans;
+        return start;
     }
 } 
