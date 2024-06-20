23006cd4cbbc279e5483b295d1fc324573d57792
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-20 18:03:41 +0530

    Time: 25 ms (28.06%), Space: 63.4 MB (6.81%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,20 +1,27 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        int operations = 0;
-        for(int num : count.keySet()) {
-            int freq = count.get(num);
+        int cnt = 0;
+        for(int freq : map.values()) {
             if(freq == 1) return -1;
-            operations += freq / 3 + (freq % 3 > 0 ? 1 : 0);
+            else if(freq % 3 == 0) {
+                cnt += freq / 3;
+            } else if(freq % 3 == 1) { // 7 = 7-2 = 5-2 = 3
+                // (count - 4) / 3 + 2 = count/3 - 4/3 + 2 = count/3 + 1
+                // is this generalised ?
+                // let's check : 10 => 3 + 1 = 4 (is it ? 10-2=8-2=6 => 2 + 2)
+                cnt += freq / 3 + 1;
+            } else if(freq % 3 == 2) { // 8 = 8-2 = 6  = 2 + 1
+                // (count - 2) / 3 + 1 = c/3 - 2/3 + 1 = c/3 + 1/3 = (c + 1)/3 
+                // is this generalised ?
+                // let's check : 14 = 14-2 = 12 / 3 = 4 + 1 = 5
+                cnt += (freq + 1) / 3;
+            }
         }
-        return operations;
+        return cnt;
     }
 }
-
-// {2 : 4, 3 : 3, 4 : 2}
-// 2 + 1 + 1
