e280fffe390a38b3a4a517f3058f3c07fe3d694f
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-20 16:36:29 +0530

    Time: 2 ms (81.95%), Space: 44.4 MB (44.10%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,23 +1,22 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0) return null;
-        int n = lists.length;
-        return merge(lists, 0, n-1);
+        return mergeSort(lists, 0, lists.length - 1);
     }
     
-    private ListNode merge(ListNode[] lists, int left, int right) {
-        if(left == right) {
-            return lists[left];
-        }
-        int mid = left + (right - left) / 2;
-        ListNode a = merge(lists, left, mid);
-        ListNode b = merge(lists, mid+1, right);
+    private ListNode mergeSort(ListNode[] lists, int leftStart, int rightEnd) {
+        if(leftStart >= rightEnd) 
+            return lists[leftStart];
+        
+        int mid = leftStart + (rightEnd - leftStart) / 2;
+        ListNode a = mergeSort(lists, leftStart, mid);
+        ListNode b = mergeSort(lists, mid + 1, rightEnd);
         return mergeHalves(a, b);
     }
     
     private ListNode mergeHalves(ListNode a, ListNode b) {
-        if(a == null && b == null) return null;
-        if(a == null || b == null) return a == null ? b : a;
+        if(a == null) return b;
+        if(b == null) return a;
         
         ListNode result = null;
         if(a.val <= b.val) {
