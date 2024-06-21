894efc7bd3c32312af3f3dd16f5058c7f388e090
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 23:38:02 +0530

    Time: 17 ms (78.49%), Space: 48.3 MB (24.86%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,36 +2,37 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
         Boolean[][]dp = new Boolean[n+1][m+1];
-        return isMatch(s, p, 0, 0, n, m, dp);
+        return wildCardMatch(s, p, 0, 0, n, m, dp);
     }
     
-    private boolean isMatch(String s, String p, int i, int j, int n, int m, Boolean[][]dp) {
+    private boolean wildCardMatch(String s, String p, int i, int j, int n, int m, Boolean[][] dp) {
         // base case
-        if(i == n) {
+        if(i == n && j == m) return true;
+        if(i == n) { 
             while(j < m && p.charAt(j) == '*') j++;
             return j == m;
         }
-        
         if(dp[i][j] != null)
             return dp[i][j];
         
         // main logic
         if(j < m && p.charAt(j) == '*') {
-            // can match with any sequence of characters
-            boolean zeroMatch = isMatch(s, p, i, j+1, n, m, dp);
+            // can match any sequence of characters
+            // zero
+            boolean zeroMatch = wildCardMatch(s, p, i, j+1, n, m, dp);
             if(zeroMatch) return dp[i][j] = true;
+            // one
             
-            boolean oneMatch = isMatch(s, p, i+1, j+1, n, m, dp);
+            boolean oneMatch = wildCardMatch(s, p, i+1, j+1, n, m, dp);
             if(oneMatch) return dp[i][j] = true;
+            // more
             
-            boolean moreMatch = isMatch(s, p, i+1, j, n, m, dp);
+            boolean moreMatch = wildCardMatch(s, p, i+1, j, n, m, dp);
             if(moreMatch) return dp[i][j] = true;
-        }
-        else {
-            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
-                if(isMatch(s, p, i+1, j+1, n, m, dp))
+        } else {
+            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'))
+                if(wildCardMatch(s, p, i+1, j+1, n, m, dp))
                     return dp[i][j] = true;
-            }     
         }
         return dp[i][j] = false;
     }
