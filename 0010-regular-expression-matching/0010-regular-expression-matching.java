98e33799507575420577720bdaeca5340a002464
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 22:58:14 +0530

    Time: 386 ms (22.22%), Space: 41.9 MB (53.94%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
-        Boolean[][]dp = new Boolean[n+1][m+1];
-        return isMatch(s, p, 0, 0, n, m, dp);
+        return regexMatch(s, p, 0, 0, n, m);
     }
     
-    private boolean isMatch(String s, String p, int i, int j, int n, int m, Boolean[][]dp) {
-        // base case
-        if(i == n && j == m) 
-            return true;
+    private boolean regexMatch(String s, String p, int i, int j, int n, int m) {
+        if(i == n && j == m) return true;
         
-        if(dp[i][j] != null)
-            return dp[i][j];
-        
-        // main logic
         if(j+1 < m && p.charAt(j+1) == '*') {
-            // zero match
-            boolean zeroMatch = isMatch(s, p, i, j+2, n, m, dp);
-            if(zeroMatch) return dp[i][j] = true;
-            // one or more
-            // if(i >= n || j >= m) return dp[i][j] = false;
-            
-            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'))
-                if(isMatch(s, p, i+1, j, n, m, dp))
-                    return dp[i][j] = true;
+            boolean zeroMatch = regexMatch(s, p, i, j+2, n, m);
+            if(zeroMatch) return true;
+            // one or more match
+            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) // consider ba with a* : in this case we need to return false
+                if(regexMatch(s, p, i+1, j, n, m))
+                    return true;
         } else {
-            // if(i >= n || j >= m) return dp[i][j] = false;
-            
-            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'))
-                if(isMatch(s, p, i+1, j+1, n, m, dp))
-                    return dp[i][j] = true;
+            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
+                if(regexMatch(s, p, i+1, j+1, n, m))
+                    return true;
+            }
         }
-        return dp[i][j] = false;
+        return false;
     }
 }
