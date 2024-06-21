280f42954e6b0a818ef907335072a0e6079da65f
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 18:30:23 +0530

    Time: 51 ms (19.64%), Space: 49.3 MB (35.60%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,93 +1,135 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        List<List<String>> answer = new ArrayList<>();
+        Map<String, Integer> emailsToIndex = new HashMap<>();
+        
         int n = accounts.size();
+        
         UnionFind uf = new UnionFind();
         uf.makeSet(n);
         
-        // construction of graph
-        HashMap<String, Integer> email2Index = new HashMap<>();
-        for(int i = 0; i <  n; i++) {
-            int size = accounts.get(i).size();
+        // step1
+        for(int i = 0; i < n; i++) {
             List<String> account = accounts.get(i);
-            for(int j = 1; j < size; j++) { // 0th index is the name of the person having an acct
+            int size = account.size();
+            for(int j = 1; j < size; j++) {
                 String email = account.get(j);
-                if(!email2Index.containsKey(email)) {
-                    email2Index.put(email, i);
+                if(!emailsToIndex.containsKey(email)) {
+                    emailsToIndex.put(email, i);
                 } else {
-                    uf.union( uf.findSet(email2Index.get(email)),  uf.findSet(i) );
+                    int a = uf.findSet(emailsToIndex.get(email));
+                    int b = uf.findSet(i);
+                    uf.union(a, b);
                 }
             }
         }
         
-        // grouping emails by index
-        HashMap<Integer, PriorityQueue<String>> validIndex2Email = new HashMap<>();
-        for(String email : email2Index.keySet()) {
-            int p = uf.findSet(email2Index.get(email)); // find the set of the current email
-            if(!validIndex2Email.containsKey(p))
-                validIndex2Email.put(p, new PriorityQueue<>());
-            validIndex2Email.get(p).offer(email);
+        // step2
+        Map<Integer, List<String>> mergedAccounts = new HashMap<>();
+        
+        for(String email : emailsToIndex.keySet()) {
+            int index = emailsToIndex.get(email);
+            int unifiedParent = uf.findSet(index);
+            mergedAccounts.putIfAbsent(unifiedParent, new ArrayList<>());
+            mergedAccounts.get(unifiedParent).add(email);
         }
         
-        List<List<String>> mergedDetails =  new ArrayList<>();
-        for(Integer id : validIndex2Email.keySet()) {
-            PriorityQueue<String> emails =  validIndex2Email.get(id);
-            List<String> temp = new ArrayList<>();
-            temp.add(accounts.get(id).get(0));
-            while(!emails.isEmpty()) temp.add(emails.poll());
-            mergedDetails.add(temp);
+        // step3
+        for(int unifiedParent : mergedAccounts.keySet()) {
+            String nameOfPerson = accounts.get(unifiedParent).get(0);
+            List<String> emailIds = mergedAccounts.get(unifiedParent);
+            Collections.sort(emailIds);
+            List<String> currList = new ArrayList<>();
+            currList.add(nameOfPerson);
+            currList.addAll(emailIds);
+            answer.add(currList);
         }
         
-        return  mergedDetails;
-    }
-}
-
-class UnionFind {
-    HashMap<Integer, Node> map;
-    UnionFind() {
-        this.map = new HashMap<>();
-    }
-    public int findSet(int val) {
-        if(!map.containsKey(val))
-            return -1;
-        return findSet(map.get(val)).index;
-    }
-    private Node findSet(Node node) {
-        if(node.parent == node)
-            return node;
-        Node newNode = findSet(node.parent);
-        node.parent = newNode;
-        return newNode;
-    }
-    public void makeSet(int n) {
-        for(int i=0;i<n;i++) {
-            Node newNode = new Node();
-            newNode.index = i;
-            newNode.rank = 0;
-            newNode.parent = newNode;
-            map.put(i, newNode);
-        }
-    }
-    public void union(int a, int b) {
-        Node pA = map.get(a);
-        Node pB = map.get(b);
-        
-        if(pA == pB)
-            return;
-        
-        if(pA.rank >= pB.rank) {
-            pB.parent = pA;
-            pA.rank = pA.rank == pB.rank ? pA.rank + 1 : pA.rank;
-        }
-        else {
-            pA.parent = pB;
-            pB.rank ++;
-        }
+        return answer;
     }
 }
 
 class Node {
-    int index;
-    int rank;
+    int data, rank;
     Node parent;
-    Node(){}
+    
+    public Node() {
+        
+    }
 }
+
+class UnionFind {
+    
+    private Map<Integer, Node> map;
+    
+    public UnionFind() {
+        map = new HashMap<>();    
+    }
+    
+    public void makeSet(int n) {
+        
+        for(int i = 0; i < n; i++) {
+            Node newNode = new Node();
+            newNode.data = i;
+            newNode.rank = 0;
+            newNode.parent = newNode;
+            map.put(i, newNode);
+        }
+        
+    }
+    
+    public int findSet(int a) {
+        return findSet(map.get(a)).data;
+    }
+    
+    private Node findSet(Node node) {
+        if(node == null || node.parent == node) return node;
+        Node newParent = findSet(node.parent);
+        node.parent = newParent;
+        return newParent;
+    }
+    
+    public boolean union(int a, int b) {
+        
+        Node pA = findSet(map.get(a));
+        Node pB = findSet(map.get(b));
+        
+        if(pA == pB) return false;
+        
+        if(pA.rank >= pB.rank) {
+            pB.parent = pA;
+            pA.rank = pA.rank > pB.rank ? pA.rank : pA.rank + 1;
+        } else {
+            pA.parent = pB;
+            pB.rank++;
+        }
+        
+        System.out.println("Unified : "+a +" :: "+b);
+        return true;
+    }
+}
+
+/*
+
+johnsmith@mail.com : [0, 1]
+john_newyork@mail.com : [0]
+john00@mail.com : [1]
+mary@mail.com : [2]
+johnnybravo@mail.com : [3]
+
+
+[0,1](0) : John
+[2] : Mary
+[3] : John
+
+
+John : [johnsmith@mail.com, john_newyork@mail.com, john00@mail.com]
+
+
+
+Optimisation : We can upgrade from having a String : List<Integer> to String : Integer and go ahead with merge or unify the 
+emails while constructing the graph itself.
+
+1. Construct the graph and unify()
+2. Merge
+*/
