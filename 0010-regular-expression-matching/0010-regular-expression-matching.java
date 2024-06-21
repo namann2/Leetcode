60d85d9621e43b3fd7ee25634c7c4d417b651928
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 23:03:03 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (80.58%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
-        return regexMatch(s, p, 0, 0, n, m);
+        Boolean[][] dp = new Boolean[n+1][m+1];
+        return regexMatch(s, p, 0, 0, n, m, dp);
     }
     
-    private boolean regexMatch(String s, String p, int i, int j, int n, int m) {
+    private boolean regexMatch(String s, String p, int i, int j, int n, int m, Boolean[][] dp) {
         if(i == n && j == m) return true;
+        if(i > n || j > m) return false;
+        
+        if(dp[i][j] != null) return dp[i][j];
         
         if(j+1 < m && p.charAt(j+1) == '*') {
-            boolean zeroMatch = regexMatch(s, p, i, j+2, n, m);
-            if(zeroMatch) return true;
+            boolean zeroMatch = regexMatch(s, p, i, j+2, n, m, dp);
+            if(zeroMatch) return dp[i][j] = true;
             // one or more match
             if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) // consider ba with a* : in this case we need to return false
-                if(regexMatch(s, p, i+1, j, n, m))
-                    return true;
+                if(regexMatch(s, p, i+1, j, n, m, dp))
+                    return dp[i][j] = true;
         } else {
             if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
-                if(regexMatch(s, p, i+1, j+1, n, m))
-                    return true;
+                if(regexMatch(s, p, i+1, j+1, n, m, dp))
+                    return dp[i][j] = true;
             }
         }
-        return false;
+        return dp[i][j] = false;
     }
 }
