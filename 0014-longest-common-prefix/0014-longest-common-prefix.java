5f418490321348f61f9bc342c765312352a33143
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 16:26:15 +0530

    Time: 4 ms (20.96%), Space: 41.6 MB (38.49%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,39 @@
-class Node {
-    Map<Character, Node> children;
-    boolean isEnd;
-    
-    Node() {
-        this.children = new HashMap<>();
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Trie trie = new Trie();
+        Node root = trie.getRoot();
+        
+        for(String word : strs) {
+            trie.insert(word);
+        }
+        
+        return trie.findPrefix();
     }
 }
 
-class Solution {
+class Node {
+    Map<Character, Node> children;
+    boolean isEnd;
+    public Node() {
+        this.children = new HashMap<>();
+        this.isEnd = false;
+    }
+}
+
+class Trie {
     
     private Node root;
-
-    public Solution() {
-        root = new Node();
+    
+    public Trie() {
+        this.root = new Node();
     }
+    
+    public Node getRoot() {
+        return this.root;    
+    }
+    
     public void insert(String word) {
-        Node curr = root;
+        Node curr = this.root;
         int n = word.length();
         for(int i = 0; i < n; i++) {
             char ch = word.charAt(i);
@@ -26,25 +44,16 @@ class Solution {
         curr.isEnd = true;
     }
     
-    public String longestCommonPrefixUtil() {
-        Node curr = root;
-        StringBuilder ans = new StringBuilder();
+    public String findPrefix() {
+        Node curr = this.root;
+        StringBuilder prefix = new StringBuilder();
         while(curr != null && curr.children.size() == 1) {
-            if(curr.isEnd) break;
+            if(curr.isEnd) return prefix.toString();
             for(char ch : curr.children.keySet()) {
-                ans.append(ch);
+                prefix.append(ch);
                 curr = curr.children.get(ch);
             }
         }
-        return ans.toString();
-    }
-    
-    public String longestCommonPrefix(String[] strs) {
-
-        for(String s : strs) {
-            insert(s);
-        }
-        
-        return longestCommonPrefixUtil();
+        return prefix.toString();
     }
 }
