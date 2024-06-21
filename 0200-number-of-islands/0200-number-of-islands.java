0b4c7981abdf941b7ec7cc07eba9498f2e7c19fc
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 22:29:16 +0530

    Time: 3 ms (85.53%), Space: 49.4 MB (54.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int islands = 0;
-        boolean[][]visited = new boolean[n][m];
+        int island = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(grid, i, j, n, m, visited);
-                    islands++;
+                if(grid[i][j] == '1') {
+                    dfs(grid, i, j, n, m);
+                    island++;
                 }
             }
         }
-        return islands;
+        return island;
     }
-    private void dfs(char[][] grid, int i, int j, int row, int col, boolean[][]visited) {
-        
-        boolean isCurrentCellSafe = isSafe(grid, i, j, row, col, visited);
+    
+    private void dfs(char[][] grid, int i, int j, int n, int m) {
+        boolean isCurrentCellSafe = isSafe(grid, i, j, n, m);
         if(isCurrentCellSafe) {
-            visited[i][j] = true;
-            dfs(grid, i-1, j, row, col, visited);
-            dfs(grid, i+1, j, row, col, visited);
-            dfs(grid, i, j-1, row, col, visited);
-            dfs(grid, i, j+1, row, col, visited);
+            grid[i][j] = '0';
+            dfs(grid, i-1, j, n, m);
+            dfs(grid, i+1, j, n, m);
+            dfs(grid, i, j-1, n, m);
+            dfs(grid, i, j+1, n, m);
         }
     }
-    private boolean isSafe(char[][] grid, int i, int j, int row, int col, boolean[][]visited) {
-        if(i >= 0 && i < row && j >= 0 && j < col && grid[i][j] == '1' && !visited[i][j]) return true;
+    
+    private boolean isSafe(char[][] grid, int i, int j, int n, int m) {
+        if(i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1')
+            return true;
         return false;
     }
 }
