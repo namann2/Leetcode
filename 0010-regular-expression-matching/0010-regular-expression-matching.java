8a45d08e6df5a2ea0b5e008a4a852228d2b2d6df
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 23:16:27 +0530

    Time: 1 ms (100.00%), Space: 41.9 MB (53.94%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,29 +1,32 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
-        Boolean[][] dp = new Boolean[n+1][m+1];
+        Boolean[][]dp = new Boolean[n+1][m+1];
         return regexMatch(s, p, 0, 0, n, m, dp);
     }
     
-    private boolean regexMatch(String s, String p, int i, int j, int n, int m, Boolean[][] dp) {
+    private boolean regexMatch(String s, String p, int i, int j, int n, int m, Boolean[][]dp) {
+        // base case
         if(i == n && j == m) return true;
-        if(i > n || j > m) return false;
-        
-        if(dp[i][j] != null) return dp[i][j];
-        
+        if(dp[i][j] != null)
+            return dp[i][j];
+        // main logic
         if(j+1 < m && p.charAt(j+1) == '*') {
+            // zero or more matched
             boolean zeroMatch = regexMatch(s, p, i, j+2, n, m, dp);
-            if(zeroMatch) return dp[i][j] = true;
-            // one or more match
-            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) // consider ba with a* : in this case we need to return false
+            if(zeroMatch) return dp[i][j]=true;
+            
+            // one or more matched
+            if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
                 if(regexMatch(s, p, i+1, j, n, m, dp))
-                    return dp[i][j] = true;
+                    return dp[i][j]=true;
+            }
         } else {
             if(i < n && j < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
                 if(regexMatch(s, p, i+1, j+1, n, m, dp))
-                    return dp[i][j] = true;
+                    return dp[i][j]=true;
             }
         }
-        return dp[i][j] = false;
+        return dp[i][j]=false;
     }
 }
