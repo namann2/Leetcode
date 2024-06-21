d11e6c6130e7d5f184c53ae4ae3ea95f5c5590b7
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-21 21:30:41 +0530

    Time: 37 ms (71.98%), Space: 49.7 MB (71.00%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,65 +1,42 @@
 class Solution {
     public int sumSubarrayMins(int[] A) {
         int n = A.length;
+        
         int[] L = new int[n];
         int[] R = new int[n];
-       
-        findNextSmallerOnLeft(A, L, n);
-        findNextSmallerOnRight(A, R, n);
+        
+        // find range in which A[i] is minimum
+        findMinOnLeft(A, L, n);
+        findMinOnRight(A, R, n);
         
         long ans = 0;
-        int mod = (int)(1e9 + 7);
-        
-        // System.out.println(Arrays.toString(L));
-        // System.out.println(Arrays.toString(R));
+        int mod = (int) (1e9 + 7);
         
         for(int i = 0; i < n; i++) {
-            long l = L[i] % mod;
-            long r = R[i] % mod;
-            ans += (l * r * A[i]) % mod;
-            ans %= mod;
+            long left = L[i] % mod;
+            long right = R[i] % mod;
+            long contri = (A[i] * left * right) % mod;
+            ans = (ans % mod + contri % mod) % mod;
         }
-        return (int)(ans % mod);
+        return (int)ans % mod;
+        
     }
     
-    private void findNextSmallerOnLeft(int[] A, int[] L, int n) {
+    private void findMinOnLeft(int[] A, int[] L, int n) {
         Deque<Integer> stack = new ArrayDeque<>();
         for(int i = 0; i < n; i++) {
             while(!stack.isEmpty() && A[stack.peekLast()] >= A[i]) stack.removeLast();
-            L[i] = stack.isEmpty() ? i + 1 : i - stack.peekLast();
+            L[i] = stack.size() == 0 ? i + 1 : i + 1 - (stack.peekLast() + 1);
             stack.addLast(i);
         }
     }
     
-    private void findNextSmallerOnRight(int[] A, int[] R, int n) {
+    private void findMinOnRight(int[] A, int[] R, int n) {
         Deque<Integer> stack = new ArrayDeque<>();
         for(int i = n-1; i >= 0; i--) {
             while(!stack.isEmpty() && A[stack.peekLast()] > A[i]) stack.removeLast();
-            R[i] = stack.isEmpty() ? n - i : stack.peekLast() - i;
+            R[i] = stack.size() == 0 ? n - i : stack.peekLast() - i; // second case : n - i - (stack.peekLast() - i)
             stack.addLast(i);
         }
     }
 }
-
-/*
-
-    3 1 2 4
-    
-    3   3 1     3 1 2   3 1 2 4
-    1   1 2     1 2 4
-    2   2 4
-    4
-    
-    10. 4           2       1
-    
-    
-    
-    3 1 2 4
-    0 1 2 3
-    
-   -1-1 1 3
-    1 4 4 4
-    
-    2 
-    
-*/
