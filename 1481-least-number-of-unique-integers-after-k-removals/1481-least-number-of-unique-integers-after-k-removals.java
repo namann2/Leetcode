f4a1fae9b74f79ab8803e5e5e03c7155d25aad43
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-23 12:17:27 +0530

    Time: 52 ms (52.85%), Space: 56.3 MB (85.54%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,23 +1,25 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] A, int k) {
-        int n = A.length;
-        Map<Integer, Integer> cnt = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int num : A) {
+            count.put(num, count.getOrDefault(num, 0)+1);
+        }
         
-        for(int a : A)
-            cnt.put(a, cnt.getOrDefault(a, 0) + 1);
+        for(int num : count.keySet()) {
+            pq.offer(count.get(num));
+        }
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(cnt.values());
-        
-        while(pq.size() > 0 && k > 0) {
-            int currCnt = pq.poll();
-            k -= currCnt;
-            if(k < 0)
-                return pq.size() + 1;
+        while(!pq.isEmpty() && k > 0) {
+            int freq = pq.poll();
+            if(freq <= k) k -= freq;
+            else {
+                freq -= k;
+                if(freq > 0) pq.offer(freq);
+                break;
+            }
         }
         
         return pq.size();
     }
 }
-// 4 - 1, 3 - 3, 1 - 2, 2 -  1
-// 4-1, 2-1, 1-2, 3-3
-// k = 3
