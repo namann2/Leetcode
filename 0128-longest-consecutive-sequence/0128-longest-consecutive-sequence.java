05c0473a3b61db2a180df4ac129e9d8218c10d5a
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-23 17:23:07 +0530

    Time: 41 ms (43.14%), Space: 61.8 MB (60.80%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,30 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
-        if(n == 0 || n == 1) return n;
-        
-        Set<Integer> numbers = new HashSet<>();
-        for(int num : nums) numbers.add(num);
-        
-        int answer = 1;
-        
+        Map<Integer, Boolean> map = new HashMap<>();
+        // first iteration
         for(int num : nums) {
-            // why ? bcz then num will become the part of a sequence
-            if(numbers.contains(num-1))  
-                continue; 
-            int local = 1;
-            while(numbers.contains((num++) + 1)) local++;
-            answer = Math.max(answer, local);
+            map.put(num, true);
         }
-        return answer;
+        
+        // second iteration
+        for(int num : map.keySet()) {
+            if(map.containsKey(num - 1))
+                map.put(num, false);
+        }
+        
+        // actual length
+        int maxLength = 0;
+        for(int num : map.keySet()) {
+            if(map.get(num) == false) continue;
+            int length = 0;
+            while(map.containsKey(num)) {
+                num++;
+                length++;
+            }
+            maxLength = Math.max(maxLength, length);
+        }
+        
+        return maxLength;
     }
 }
