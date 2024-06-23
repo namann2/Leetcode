4083f4bdb256478bdcc975127048cdbf0d5a1770
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-23 11:05:50 +0530

    Time: 3 ms (88.22%), Space: 44.9 MB (29.53%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,21 +1,25 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int max = 0, n = nums.length;
+        int max = 0;
         for(int num : nums)
             max = Math.max(max, num);
         
-        int[] values = new int[max+1];
-        for(int num : nums) 
-            values[num] += num;
+        int[] count = new int[max + 1];
+        for(int num : nums)
+            count[num]++;
         
-        
-        int[]dp = new int[max+1];
-        dp[0] = values[0];
-        dp[1] = values[1];
-        for(int i = 2; i < max+1; i++) {
-            dp[i] = Math.max(values[i] + dp[i-2], dp[i-1]);
+        // 2 2 3 3 3 4
+        // 0 1 2 3 4 - index
+        // 0 0 2 3 1 
+        int[] dp = new int[max+1];
+        // dp state - max uptil this index
+        dp[0] = 0;
+        dp[1] = Math.max(1 * count[1], count[0]);
+        for(int i = 2; i < max + 1; i++) {
+            int choose = count[i] * i + dp[i-2];
+            int dont = dp[i-1];
+            dp[i] = Math.max(choose, dont);
         }
-        
         return dp[max];
     }
 }
