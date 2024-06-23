764d520c87dc285af017d9c3470e192f17aeb514
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-23 13:52:01 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (62.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,11 @@
 public class Solution {
     // you need treat n as an unsigned value
+    // Java's primitive integer types (byte, short, int, and long) are all signed.
     public int reverseBits(int n) {
         int result = 0;
         for(int i = 31; i >= 0; i--) {
             int bit = (n >> i) & 1;
-            if(bit == 0) continue;
-            result |= (1 << (31-i));
+            result |= (bit << (31-i));
         }
         return result;
     }
