9df529a47fb6fe2fffe5da33dbc9217157a68187
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-24 01:18:22 +0530

    Time: 24 ms (82.24%), Space: 93.9 MB (9.26%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,7 +1,8 @@
 class RandomizedSet {
 
     private Map<Integer, Integer> map;
-    private ArrayList<Integer> locations;
+    private List<Integer> locations;
+    
     public RandomizedSet() {
         map = new HashMap<>();
         locations = new ArrayList<>();
@@ -9,8 +10,7 @@ class RandomizedSet {
     
     public boolean insert(int val) {
         boolean contains = map.containsKey(val);
-        if(contains)
-            return false;
+        if(contains) return false;
         
         map.put(val, locations.size());
         locations.add(val);
@@ -19,25 +19,36 @@ class RandomizedSet {
     
     public boolean remove(int val) {
         boolean contains = map.containsKey(val);
-        if(!contains)
+        if(!contains) {
             return false;
-        
-        int index = map.get(val);
-        
-        int lastIndex = locations.size()-1;
-        
-        if(index != lastIndex) {
-            locations.set(index, locations.get(lastIndex));
-            map.put(locations.get(index), index);
         }
-        map.remove(val);
-        locations.remove(lastIndex);
+        
+        int valIndex = map.get(val);
+        int lastIndex = locations.size() - 1;
+        if(valIndex != lastIndex) {
+            map.remove(val);
+            int lastElement = locations.get(lastIndex);
+            locations.set(valIndex, lastElement);
+            map.put(lastElement, valIndex);
+            locations.remove(lastIndex);
+        } else {
+            locations.remove(valIndex);
+            map.remove(val);
+        }
         return true;
     }
     
     public int getRandom() {
         int min = 0, max = locations.size();
-        int randomIndex = (int)(Math.random() * (max - min) + min);
+        int randomIndex = (int)(Math.random() * (max - min) + min );
         return locations.get(randomIndex);
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
