4e06a44078b2067964cd0591f921671e0f4877a9
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-24 13:14:22 +0530

    Time: 23 ms (83.35%), Space: 58 MB (41.66%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,36 +1,37 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        if(k == 50000) return 1;
         int n = nums.length;
+        if(k == 50000) return 1;
         k = n - k;
         return quickSelect(nums, 0, n-1, k);
     }
     
     private int quickSelect(int[] nums, int left, int right, int k) {
+        if(left > right) return -1;
         int pi = partition(nums, left, right);
-        if(pi == k) 
-            return nums[k];
-        else if(k > pi)
-            return quickSelect(nums, pi + 1, right, k);
-        else return quickSelect(nums, left, pi - 1, k);
+        if(pi == k) return nums[k];
+        else if(k < pi) return quickSelect(nums, left, pi - 1, k);
+        else return quickSelect(nums, pi + 1, right, k);
     }
     
     private int partition(int[] nums, int left, int right) {
-        int pivot = right;
-        int i = left, j = left;
-        while(i <= right) {
-            if(nums[i] <= nums[pivot]) {
-                swap(nums, i, j);
-                i++;
-                j++;
-            } else i++;
+        int pivot = nums[right];
+        int it = left, pos = left;
+        while(it <= right) {
+            if(nums[it] <= pivot) {
+                swap(nums, it, pos);
+                pos++;
+                it++;
+            } else {
+                it++;
+            }
         }
-        return j-1;
+        return pos - 1;
     }
     
-    private void swap(int[] nums, int A, int B) {
-        int tmp = nums[A];
-        nums[A] = nums[B];
-        nums[B] = tmp;
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
