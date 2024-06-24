fad5c5bdb1595f0a4e2149cec42a8961404551fa
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-24 12:37:04 +0530

    Time: 14 ms (56.68%), Space: 48.8 MB (24.26%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,52 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : nums)
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        
-        int n = map.size();
-        int[] unique = new int[n];
-        int idx = 0;
-        for(int num : map.keySet())
-            unique[idx++] = num;
-        
-        quickSelect(unique, map, 0, n-1, n-k);
-        
-        int[] ans = new int[k];
-        int l = k-1;
-        while(l >= 0) {
-            ans[l--] = unique[n-1];
-            n--;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p, q)->{
+            return p[0]-q[0]; // freq
+        });
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
+        
+        for(int num : count.keySet()) {
+            pq.offer(new int[]{count.get(num), num});
+            if(pq.size() > k)
+                pq.remove();
+        }
+        
+        int n = pq.size(), idx = 0;
+        int[] ans = new int[n];
+        while(!pq.isEmpty())
+            ans[idx++] = pq.poll()[1];
         
         return ans;
-    }
-    
-    private int quickSelect(int[] unique, Map<Integer, Integer> map, int left, int right, int k) {
-        int pi = partition(unique, map, left, right);
-        if(pi == k) return pi;
-        else if(k > pi) return quickSelect(unique, map, pi + 1, right, k);
-        return quickSelect(unique, map, left, pi - 1, k);
-    }
-    
-    private int partition(int[] unique, Map<Integer, Integer> map, int left, int right) {
-        int pivot = right;
-        int i = left, j = left;
-        
-        while(i <= right) {
-            if(map.get(unique[i]) <= map.get(unique[pivot])) {
-                swap(unique, i, j);
-                i++;
-                j++;
-            } else i++;
-        }
-        return j-1;
-    }
-    
-    private void swap(int[] A, int i, int j) {
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
     }
 }
