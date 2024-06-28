e21ba034a1741defe2df8b6acd8c88619e85ca34
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-28 13:37:11 +0530

    Time: 72 ms (90.75%), Space: 76.1 MB (44.10%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,43 +1,52 @@
 class NumArray {
+    int[] nums;
     int n;
-    int []nums, bit;
+    private FenwickTree fenwick;
+    
     public NumArray(int[] nums) {
-        n = nums.length;
         this.nums = nums;
-        this.bit = new int[n+1];
-        buildTree();
-    }
-    
-    private void buildTree() {
+        this.n = nums.length;
+        fenwick = new FenwickTree(n);
+        
         for(int i = 0; i < n; i++)
-            add(i, nums[i]);
-    }
-    
-    private void add(int index, int val) {
-        index++;
-        while(index <= n) {
-            bit[index] += val;
-            index += (index & -index);
-        }
-    }
-    
-    private int getSum(int index) {
-        int sum = 0;
-        index++;
-        while(index > 0) {
-            sum += bit[index];
-            index -= (index & -index);
-        }
-        return sum;
+            fenwick.update(i, nums[i]);
     }
     
     public void update(int index, int val) {
         int diff = -nums[index] + val;
         nums[index] = val;
-        add(index, diff);
+        fenwick.update(index, diff);
     }
     
     public int sumRange(int left, int right) {
-        return getSum(right) - getSum(left-1);
+        return fenwick.sumRange(right) - fenwick.sumRange(left - 1);
+    }
+}
+
+class FenwickTree {
+    int n;
+    int[] bit;
+    
+    public FenwickTree(int n) {
+        this.n = n + 1;
+        this.bit = new int[this.n];
+    }
+    
+    public void update(int index, int val) {
+        index++;
+        while(index < n) {
+            bit[index] += val;
+            index += (index & -index);
+        }
+    }
+    
+    public int sumRange(int index) {
+        index++;
+        int sum = 0;
+        while(index > 0) {
+            sum += bit[index];
+            index -= (index & -index);
+        }
+        return sum;
     }
 }
