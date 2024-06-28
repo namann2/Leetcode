570b62db2cc0e0d68415b0871b9a81d647c50646
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-06-28 14:39:25 +0530

    Time: 93 ms (60.15%), Space: 77.9 MB (14.35%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,52 +1,79 @@
 class NumArray {
-    int[] nums;
-    int n;
-    private FenwickTree fenwick;
     
+    int n;
+    int[] nums;
+    SegmentTree obj;
     public NumArray(int[] nums) {
-        this.nums = nums;
         this.n = nums.length;
-        fenwick = new FenwickTree(n);
-        
-        for(int i = 0; i < n; i++)
-            fenwick.update(i, nums[i]);
+        this.nums = nums;
+        obj = new SegmentTree(n);
+        obj.buildTree(nums, 0, n-1, 0); // nums, left, right, tree_index
     }
     
     public void update(int index, int val) {
-        int diff = -nums[index] + val;
         nums[index] = val;
-        fenwick.update(index, diff);
+        obj.update(0, n-1, index, val, 0); // left, right, to_update_index, with_val, tree_index
     }
     
     public int sumRange(int left, int right) {
-        return fenwick.sumRange(right) - fenwick.sumRange(left - 1);
+        return obj.getSum(0, n-1, left, right, 0); // complete_range[0, n-1], required_range[left, right]
     }
 }
 
-class FenwickTree {
+class SegmentTree {
     int n;
-    int[] bit;
+    int[] tree;
     
-    public FenwickTree(int n) {
-        this.n = n + 1;
-        this.bit = new int[this.n];
+    public SegmentTree(int n) {
+        this.n = 4 * n;
+        tree = new int[this.n];
     }
     
-    public void update(int index, int val) {
-        index++;
-        while(index < n) {
-            bit[index] += val;
-            index += (index & -index);
+    public void buildTree(int[] nums, int left, int right, int tree_index) {
+        if(left == right) {
+            tree[tree_index] = nums[left];
+            return;
         }
+        
+        int mid = left + (right - left) / 2;
+        
+        buildTree(nums, left, mid, 2 * tree_index + 1);
+        buildTree(nums, mid + 1, right, 2 * tree_index + 2);
+        
+        tree[tree_index] = tree[2 * tree_index + 1] + tree[2 * tree_index + 2];
     }
     
-    public int sumRange(int index) {
-        index++;
-        int sum = 0;
-        while(index > 0) {
-            sum += bit[index];
-            index -= (index & -index);
+    public void update(int left, int right, int index, int val, int tree_index) {
+        if(left == right) {
+            tree[tree_index] = val;
+            return;
         }
-        return sum;
+        
+        int mid = left + (right - left) / 2;
+        if(index <= mid)
+            update(left, mid, index, val, 2 * tree_index + 1);
+        else update(mid + 1, right, index, val, 2 * tree_index + 2);
+        
+        tree[tree_index] = tree[2 * tree_index + 1] + tree[2 * tree_index + 2];
+    }
+    
+    public int getSum(int L, int R, int left, int right, int tree_index) {
+        // three conditions
+        // complete overlap..L...left...right..R
+        if(L >= left && R <= right)
+            return tree[tree_index];
+        // no overlap
+        if(left > R || right < L)
+            return 0;
+        // complete overlap
+        if(L >= left && R <= right)
+            return tree[tree_index];
+        // partial overlap
+        int mid = L + (R - L) / 2;
+        
+        int l = getSum(L, mid, left, right, 2 * tree_index + 1);
+        int r = getSum(mid + 1, R, left, right, 2 * tree_index + 2);
+        
+        return l + r;
     }
 }
