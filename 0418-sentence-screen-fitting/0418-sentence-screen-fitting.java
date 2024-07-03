6ea408eb3abfc71fc89c1591d301d256fb1bcd29
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-03 13:16:15 +0530

    Time: 2053 ms (14.91%), Space: 41 MB (41.02%) - LeetHub

--- 0418-sentence-screen-fitting/0418-sentence-screen-fitting.java
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.java
@@ -1,21 +1,20 @@
 class Solution {
     public int wordsTyping(String[] sentence, int rows, int cols) {
-        int cnt = 0, n = sentence.length, length = 0;
+        int cnt = 0, n = sentence.length, colsUsed = 0;
+        // colsUsed signifies the number of cols we have already used in current row
         while(rows > 0) {
             for(int i = 0; i < n; i++) {
                 String curr = sentence[i];
                 int currLength = curr.length();
                 // if curr word can not be placed because of cols limit
-                if(currLength > cols) return cnt;
+                if(currLength > cols) return 0;
                 
-                if(length + currLength <= cols) { // can we add curr word to current row ?
-                    // length += currLength + 1;
+                if(colsUsed + currLength <= cols) { // can we add curr word to current row ?
+                    colsUsed += currLength + 1;
                 } else { // add curr word to new row
                     rows--;
-                    length = 0;
-                    // length = currLength + 1;
+                    colsUsed = currLength + 1;
                 }
-                length += currLength + 1;
                 if(rows == 0) return cnt;
             }
             cnt++;
