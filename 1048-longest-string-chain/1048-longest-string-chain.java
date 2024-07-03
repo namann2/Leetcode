e76ce1c11bdec537b7e821f6bbab33b2b7e22d5c
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-03 16:48:44 +0530

    Time: 50 ms (58.45%), Space: 43.9 MB (87.81%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,34 +1,45 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        // TC : O(nlogn) + O(n * wl * wl)
-        // SC : O(n) + O(n)
         int n = words.length;
-        if(n == 1) return 1;
-        
+        // sort the words by length
         Arrays.sort(words, (w1, w2) -> {
             return w1.length() - w2.length();
         });
         
-        Map<String,Integer> map = new HashMap<>();
-        
-        int[]dp = new int[n];
-        dp[0] = 1;
-        map.put(words[0], 0);
-        int maxLength = 1;
-        
+        // this seems like an application of LIS
+        int[] dp = new int[n + 1];
+        int maxLength = 0;
         for(int i = 0; i < n; i++) {
-            int wl = words[i].length();
             dp[i] = 1;
-            for(int j=0;j<wl;j++) {
-                StringBuilder tmp = new StringBuilder(words[i]);
-                String t = tmp.deleteCharAt(j).toString();
-                if(map.containsKey(t) && dp[map.get(t)] + 1 > dp[i]) {
-                    dp[i] = dp[map.get(t)] + 1;
+            for(int j = 0; j < i; j++) {
+                if(isPredecessor(words[j], words[i]) && 1 + dp[j] > dp[i]) {
+                    dp[i] = 1 + dp[j];
                 }
             }
-            map.put(words[i], i);
+            
             maxLength = Math.max(maxLength, dp[i]);
         }
         return maxLength;
+    }
+    
+    private boolean isPredecessor(String A, String B) {
+        // check if word A is predecessor of word B
+        int differ = 0;
+        int n = A.length(), m = B.length(), i = 0, j = 0;
+        if(m - n != 1) return false;
+        while(i < n && j < m) {
+            char a = A.charAt(i);
+            char b = B.charAt(j);
+            if(a == b) {
+                i ++;
+                j ++;
+            } else {
+                j++;
+                differ ++;
+            }
+        }
+        return differ <= 1; // less than one because
+        // there are two options : ab, abc and ab, dab
+        // ab, abc has differ 0 but we know that their length differ by 1
     }
 }
