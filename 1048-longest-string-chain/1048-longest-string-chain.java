f3b72290a67490c972f2cceae267cb55c99670a4
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-04 00:51:54 +0530

    Time: 32 ms (90.56%), Space: 45 MB (25.95%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -7,39 +7,25 @@ class Solution {
         });
         
         // this seems like an application of LIS
-        int[] dp = new int[n + 1];
-        int maxLength = 0;
-        for(int i = 0; i < n; i++) {
-            dp[i] = 1;
-            for(int j = 0; j < i; j++) {
-                if(isPredecessor(words[j], words[i]) && 1 + dp[j] > dp[i]) {
-                    dp[i] = 1 + dp[j];
+        Map<String, Integer> wordLIS = new HashMap<>();
+        wordLIS.put(words[0], 1);
+        
+        int maxLength = 1;
+        
+        for(int i = 1; i < n; i++) {
+            String currWord = words[i];
+            int wordLength = currWord.length();
+            int currLIS = 1;
+            for(int j = 0; j < wordLength; j++) {
+                String newStr = new StringBuilder(currWord).deleteCharAt(j).toString();
+                if(wordLIS.containsKey(newStr)) {
+                    currLIS = Math.max(currLIS, 1 + wordLIS.get(newStr));
                 }
             }
-            
-            maxLength = Math.max(maxLength, dp[i]);
+            wordLIS.put(currWord, currLIS);
+            maxLength = Math.max(maxLength, currLIS);
         }
+        
         return maxLength;
-    }
-    
-    private boolean isPredecessor(String A, String B) {
-        // check if word A is predecessor of word B
-        int differ = 0;
-        int n = A.length(), m = B.length(), i = 0, j = 0;
-        if(m - n != 1) return false;
-        while(i < n && j < m) {
-            char a = A.charAt(i);
-            char b = B.charAt(j);
-            if(a == b) {
-                i ++;
-                j ++;
-            } else {
-                j++;
-                differ ++;
-            }
-        }
-        return differ <= 1; // less than one because
-        // there are two options : ab, abc and ab, dab
-        // ab, abc has differ 0 but we know that their length differ by 1
     }
 }
