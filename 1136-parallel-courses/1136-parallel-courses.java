6073ad378b8ef6718df51d3325bbaf3d4aac5391
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-05 17:23:49 +0530

    Time: 15 ms (25.28%), Space: 45.8 MB (43.12%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.java
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -1,19 +1,15 @@
 class Solution {
     public int minimumSemesters(int n, int[][] relations) {
         Map<Integer, List<Integer>> g = new HashMap<>();
+        int[] indegree = new int[n];
         for(int[] relation : relations) {
-            g.putIfAbsent(relation[0], new ArrayList<>());
-            g.get(relation[0]).add(relation[1]);
-        }
-        
-        // this can be done with above step
-        int[] indegree = new int[n+1];
-        for(int[] relation : relations) {
-            indegree[relation[1]]++;
+            g.putIfAbsent(relation[0]-1, new ArrayList<>());
+            g.get(relation[0]-1).add(relation[1]-1);
+            indegree[relation[1]-1]++;
         }
         
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i < n+1; i++) {
+        for(int i = 0; i < n; i++) {
             if(indegree[i] == 0)
                 q.offer(i);
         }
@@ -34,7 +30,7 @@ class Solution {
             semester++;
         }
         
-        for(int i = 1; i < n+1; i++)
+        for(int i = 0; i < n; i++)
             if(indegree[i] != 0) return -1;
         
         return semester - 1;
