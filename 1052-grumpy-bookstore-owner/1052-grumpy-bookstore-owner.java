9586c3d36bf1f3040d0d68ec6a4c9927d9edc699
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-25 02:13:43 +0530

    Time: 4 ms (35.10%), Space: 47.4 MB (46.10%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -3,14 +3,18 @@ class Solution {
         // greedy
         int n = customers.length;
         int happy = 0, convert = 0, extra = 0;
+        
         for(int i = 0; i < n; i++) {
-            
+            // if grumpy[i] == 0 -> all customers are happy customers
             if(grumpy[i] == 0) happy += customers[i];
+            // else the customers are extra customers that can be potentially converted to happy customers
             else extra += customers[i];
             
+            // keep on updating the extra customers based on the minutes window
             if(i >= minutes && grumpy[i-minutes] == 1) {
                 extra -= customers[i-minutes];
             }
+            // get max customers that can be converted
             convert = Math.max(convert, extra);
         }
         return happy + convert;
