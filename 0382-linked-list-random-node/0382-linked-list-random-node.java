6945cca6b9bad65021b2876c68cd79e1ce3c8185
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-07-25 17:57:06 +0530

    Time: 12 ms (25.46%), Space: 45.9 MB (55.14%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -0,0 +1,22 @@
+class Solution {
+    // O(1)
+    ListNode linkedList;
+    public Solution(ListNode head) {
+        linkedList = head;
+    }
+    // O(n)
+    public int getRandom() {
+        int i = 0; // count of numbers seen till now
+        ListNode curr = linkedList;
+        int ans = -1;
+        while(curr != null) {
+            ++i; // we have seen one number more
+            int randomIndex = (int)(Math.random() * i);
+            if(randomIndex == i-1) { // assume these numbers are stored in an array, 1st number corresponds to 0th index
+                ans = curr.val;
+            }
+            curr = curr.next;
+        }
+        return ans;
+    }
+}
