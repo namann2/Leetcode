3956d768701704695ee12131741a3324e5820e81
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-10-20 21:23:56 +0530

    Time: 4 ms (45.37%), Space: 57.3 MB (22.78%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,13 +1,9 @@
 class Solution {
     public int minNumberOperations(int[] target) {
-        // dp[i] = min number of operations
-        // required to convert target[i]
-        
         int n = target.length;
-        int[] dp = new int[n];
+        int[]dp = new int[n];
         dp[0] = target[0];
-
-        for(int i = 1; i < n; i++) {
+        for(int i=1; i < n; i++) {
             if(target[i] > target[i-1])
                 dp[i] = dp[i-1] + target[i] - target[i-1];
             else dp[i] = dp[i-1];
