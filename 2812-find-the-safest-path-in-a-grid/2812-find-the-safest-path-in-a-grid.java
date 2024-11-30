30ac2fd275e55a4eb9ca4381e59a3a20183d882c
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-12-01 02:08:40 +0530

    Time: 186 ms (45.42%), Space: 91.9 MB (5.11%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,63 +1,24 @@
 class Solution {
     public int maximumSafenessFactor(List<List<Integer>> grid) {
-        int rows = grid.size(), cols = rows;
+        /*
+            1. Find out the distance from each cell to the nearest 1
+            2. Binary Search from start = 0, end = n + m
+                2.1. Do a DFS from 0,0 and update the range
+        */
         
-        if(grid.get(0).get(0) == 1 || grid.get(rows-1).get(cols-1) == 1) {
+        int n = grid.size();
+        if(grid.get(0).get(0) == 1 || grid.get(n-1).get(n-1) == 1) 
             return 0;
-        }
         
-        // step1 : calculate the nearest distance from thief for each cell
-        int[][] distance = new int[rows][cols];
-        for(int[] row : distance) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
+        int[][] distanceOfNearestThief = new int[n][n];
         
-        // find the distance of nearest 1 for each cell in the grid
-        findNearestThiefForEachCellInGrid(grid, rows, cols, distance);
+        // find distance to nearest thief
+        findDistanceOfNearestThief(grid, n, distanceOfNearestThief);
         
-        // find the manhatan distance
-        return maximumSafenessFactor(distance, rows, cols);
-    }
-    
-    // helper functions
-    private void findNearestThiefForEachCellInGrid(List<List<Integer>> grid, int rows, int cols, int[][] distance) {
-        Queue<int[]> q = new LinkedList<>();
-        for(int i = 0; i < rows; i++) {
-            List<Integer> currRow = grid.get(i);
-            for(int j = 0; j < cols; j++) {
-                if(grid.get(i).get(j) == 1) {
-                    q.offer(new int[]{i, j, 0});
-                    distance[i][j] = 0;
-                }
-            }
-        }
-        
-        // do a bfs from the cells in queue
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        
-        while(!q.isEmpty()) {
-            int[] currCell = q.poll();
-            for(int i = 0; i < 4; i++) {
-                int newX = currCell[0] + dx[i];
-                int newY = currCell[1] + dy[i];
-                int newD = currCell[2] + 1;
-                
-                if(newX >= 0 && newX < rows && newY >= 0 && newY < cols && distance[newX][newY] > newD) {
-                    distance[newX][newY] = newD;
-                    q.offer(new int[]{newX, newY, newD});
-                }
-            }
-        }
-    }
-    
-    private int maximumSafenessFactor(int[][] distance, int rows, int cols) {
-        // the range of manhatan distance in the grid
-        // aim : whether we are able to get a path with this manhatan distance in the grid
-        int start = 0, end = rows + cols, ans = -1;
+        int start = 0, end = n + n, ans = 0; 
         while(start <= end) {
             int mid = start + (end - start) / 2;
-            if(doesPathExistWithGivenDistance(distance, 0, 0, rows, cols, mid, new boolean[rows][cols])) {
+            if(canReachEndCell(grid, 0, 0, distanceOfNearestThief, mid, n, new boolean[n][n])) {
                 ans = mid;
                 start = mid + 1;
             } else end = mid - 1;
@@ -65,29 +26,68 @@ class Solution {
         return ans;
     }
     
-    private boolean doesPathExistWithGivenDistance(int[][] distance, int i, int j, int rows, int cols, int minDistance, boolean[][] visited) {
+    // helper functions
+    
+    private void findDistanceOfNearestThief(List<List<Integer>> grid, int n, int[][] distanceOfNearestThief) {
         
-        boolean isCurrentCellSafe = isSafe(distance, i, j, rows, cols, minDistance, visited);
+        for(int[] row : distanceOfNearestThief) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
         
+        Queue<int[]> q = new LinkedList<>();
+        
+        for(int i = 0; i < n; i++) {
+            List<Integer> cols = grid.get(i);
+            for(int j = 0; j < n; j++) {
+                if(grid.get(i).get(j) == 1) {
+                    q.offer(new int[]{i, j, 0});
+                    distanceOfNearestThief[i][j] = 0;
+                }
+            }
+        }
+        
+        // bfs on the grid
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        
+        while(!q.isEmpty()) {
+            int[] currCell = q.poll();
+            int currX = currCell[0], currY = currCell[1], currDis = currCell[2];
+            for(int i = 0; i < 4; i++) {
+                int newX = currX + dx[i],
+                newY = currY + dy[i],
+                newDis = currDis + 1;
+                
+                if(newX >= 0 && newX < n && newY >= 0 && newY < n && newDis < distanceOfNearestThief[newX][newY]) {
+                    distanceOfNearestThief[newX][newY] = newDis;
+                    q.offer(new int[]{newX, newY, newDis});
+                }
+            }
+        }
+    }
+    
+    private boolean canReachEndCell(List<List<Integer>> grid, int i, int j, int[][] distanceOfNearestThief, int threshold, int n, boolean[][] visited) {
+        boolean isCurrentCellSafe = isSafe(grid, i, j, distanceOfNearestThief, threshold, n, visited);
         if(isCurrentCellSafe) {
             visited[i][j] = true;
+            if(i == n-1 && j == n-1) return true;
             
-            if(i == rows - 1 && j == cols - 1) return true;
-            
-            boolean left = doesPathExistWithGivenDistance(distance, i, j-1, rows, cols, minDistance, visited);
+            boolean left = canReachEndCell(grid, i, j-1, distanceOfNearestThief, threshold, n, visited);
             if(left) return true;
-            boolean right = doesPathExistWithGivenDistance(distance, i, j+1, rows, cols, minDistance, visited);
+            boolean right = canReachEndCell(grid, i, j+1, distanceOfNearestThief, threshold, n, visited);
             if(right) return true;
-            boolean up = doesPathExistWithGivenDistance(distance, i-1, j, rows, cols, minDistance, visited);
+            boolean up = canReachEndCell(grid, i-1, j, distanceOfNearestThief, threshold, n, visited);
             if(up) return true;
-            boolean down = doesPathExistWithGivenDistance(distance, i+1, j, rows, cols, minDistance, visited);
+            boolean down = canReachEndCell(grid, i+1, j, distanceOfNearestThief, threshold, n, visited);
             if(down) return true;
         }
         return false;
     }
     
-    private boolean isSafe(int[][] distance, int i, int j, int rows, int cols, int minDistance, boolean[][] visited) {
-        if(i >= 0 && i < rows && j >= 0 && j < cols && distance[i][j] >= minDistance && !visited[i][j]) return true;
+    private boolean isSafe(List<List<Integer>> grid, int i, int j, int[][] distanceOfNearestThief, int threshold, int n, boolean[][] visited) {
+        if(i >= 0 && i < n && j >= 0 && j < n && !visited[i][j] && distanceOfNearestThief[i][j] >= threshold) {
+            return true;
+        }
         return false;
     }
 }
