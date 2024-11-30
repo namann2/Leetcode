e7bebda9c69f939f21f7d1738ee34ce994b991d8
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-12-01 02:24:50 +0530

    Time: 41 ms (58.82%), Space: 61.7 MB (53.57%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
@@ -1,34 +1,32 @@
 class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
-        // sort the arrays to arrange buses and passangers as per their earliest times ( departure for bus and arrival for passangers)
         Arrays.sort(buses);
         Arrays.sort(passengers);
         
-        // Note : we want to reach at max time at which we can board the bus
         Set<Integer> arrivalTimes = new HashSet<>();
-        int busesLength = buses.length;
-        int passengerLength = passengers.length;
+        for(int arrivalTime : passengers) arrivalTimes.add(arrivalTime);
+        
+        int busesLength = buses.length, passangerLength = passengers.length;
         int prev = 0, currCapacity = 0;
         for(int i = 0; i < busesLength; i++) {
             int runner = prev;
             currCapacity = capacity;
-            while(runner < passengerLength && passengers[runner] <= buses[i] && currCapacity > 0) {
-                arrivalTimes.add(passengers[runner]);
-                currCapacity--;
+            // check which passengers gets on the bus
+            while(runner < passangerLength && passengers[runner] <= buses[i] && currCapacity > 0) {
+                currCapacity--; // this passanger onboarded the bus[i]
                 runner++;
             }
             prev = runner;
         }
-        // AIM : find the latest time for us to board the bus
-        // if the capacity of last bus is filled then, we need to come before the time at which the last person arrived
-        --prev; // last passanger to board the bus
-        if(prev >= 0 && (currCapacity == 0 || buses[busesLength - 1] == passengers[prev])) {
-            // we need to reach before this person
-            int lastArrivalTime = passengers[prev];
-            while(!arrivalTimes.add(lastArrivalTime)) --lastArrivalTime;
-            return lastArrivalTime;
+        --prev;
+        if(prev >= 0 && (currCapacity == 0 || passengers[prev] == buses[busesLength-1])) {
+            int runner = prev, time = passengers[prev];
+            while(runner >= 0 && arrivalTimes.contains(time)) {
+                runner--;
+                time--;
+            }
+            return time;
         }
-        // or else, there is still capacity left for us to board
         return buses[busesLength-1];
     }
 }
