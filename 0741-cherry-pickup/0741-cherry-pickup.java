ae91baf419473ca261125cae2c18ddf085fc766e
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-12-02 18:27:04 +0530

    Time: 122 ms (14.91%), Space: 107.4 MB (20.00%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,47 +1,47 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
-        if(grid[0][0] == -1 || grid[rows-1][cols-1] == -1)
+        int n = grid.length;
+        if(grid[0][0] == -1 || grid[n-1][n-1] == -1) 
             return 0;
-        Integer[][][][]dp = new Integer[rows][rows][cols][cols];
-        return Math.max(0, cherryPick(grid, 0, 0, 0, 0, rows, cols, dp));
+        Integer dp[][][][] = new Integer[n][n][n][n];
+        int numberOfCherries = cherryPick(grid, dp, 0, 0, 0, 0, n, n);
+        return Math.max(numberOfCherries, 0);
     }
     
-    private int cherryPick(int[][] grid, int r1, int c1, int r2, int c2, int rows, int cols, Integer[][][][] dp) {
+    // helper functions
+    
+    private int cherryPick(int[][] grid, Integer[][][][] dp, int r1, int c1, int r2, int c2, int rows, int cols) {
         // base case
-        // if we went out of the grid or hit a thorn, discourage this path by returning Integer.MIN_VALUE
         if(r1 >= rows || r2 >= rows || c1 >= cols || c2 >= cols || grid[r1][c1] == -1 || grid[r2][c2] == -1) {
             return Integer.MIN_VALUE;
         }
         
-        if(dp[r1][r2][c1][c2] != null)
-            return dp[r1][r2][c1][c2];
-        
-        // if person 1 reached the bottom right, return what's in there (could be 1 or 0)
+        if(dp[r1][c1][r2][c2] != null)
+            return dp[r1][c1][r2][c2];
+            
         if(r1 == rows - 1 && c1 == cols - 1) {
             return grid[r1][c1];
         }
         
-        // if person 2 reached the bottom right, return what's in there (could be 1 or 0)
         if(r2 == rows - 1 && c2 == cols - 1) {
             return grid[r2][c2];
         }
         
         // main logic
         int cherries = 0;
-        // if both person are on same cell, get the value of it once
         if(r1 == r2 && c1 == c2) {
             cherries += grid[r1][c1];
-        } else {
-            cherries += grid[r1][c1] + grid[r2][c2];
+        } 
+        else {
+            cherries = grid[r1][c1] + grid[r2][c2];
         }
         
-        int r1r2 = cherryPick(grid, r1 + 1, c1, r2 + 1, c2, rows, cols, dp);
-        int c1c2 = cherryPick(grid, r1, c1 + 1, r2, c2 + 1, rows, cols, dp);
-        int r1c2 = cherryPick(grid, r1 + 1, c1, r2, c2 + 1, rows, cols, dp);
-        int c1r2 = cherryPick(grid, r1, c1 + 1, r2 + 1, c2, rows, cols, dp);
+        int r1c2 = cherryPick(grid, dp, r1 + 1, c1, r2, c2 + 1, rows, cols);
+        int r1r2 = cherryPick(grid, dp, r1 + 1, c1, r2 + 1, c2, rows, cols);
+        int c1c2 = cherryPick(grid, dp, r1, c1 + 1, r2, c2 + 1, rows, cols);
+        int c1r2 = cherryPick(grid, dp, r1, c1 + 1, r2 + 1, c2, rows, cols);
         
-        cherries += Math.max(Math.max(r1r2, c1c2), Math.max(r1c2, c1r2));
-        return dp[r1][r2][c1][c2] = cherries;
+        int curr = Math.max(Math.max(r1c2, r1r2), Math.max(c1c2, c1r2));
+        return dp[r1][c1][r2][c2] = cherries + curr;
     }
 }
