4a081ea7db879f69c0bcfb7e9def32d17f32a168
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-12-08 00:05:51 +0530

    Update 0055-jump-game.java

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,3 +1,20 @@
+/*
+
+Much simpler and better solution : 
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest = 0;
+        for(int i = 0; i <= farthest && i < n; i++) {
+            if(i + nums[i] > farthest) {
+                farthest = i + nums[i];
+            }
+        }
+        return farthest >= n-1;
+    }
+}
+*/
+
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
