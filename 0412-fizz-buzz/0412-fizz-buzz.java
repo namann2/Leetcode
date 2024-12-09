32403bc9db5b5f9e9635e9ff586e8c4aabc1314a
Naman Gupta <59756482+namann2@users.noreply.github.com>
2024-12-09 10:32:08 +0530

    Time: 1 ms (99.54%), Space: 45.6 MB (7.83%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,13 +2,19 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         final String Fizz = "Fizz", Buzz = "Buzz", FizzBuzz = "FizzBuzz";
         List<String> answer = new ArrayList<>();
+        int fizz = 0, buzz = 0;
         for(int i = 1; i <= n; i++) {
-            if(i%3 == 0 && i%5 == 0) {
+            fizz++;
+            buzz++;
+            if(fizz == 3 && buzz == 5) {
                 answer.add(FizzBuzz);
-            } else if(i % 5 == 0) {
-                answer.add(Buzz);
-            } else if(i % 3 == 0) {
+                fizz = buzz = 0;
+            } else if(fizz == 3) {
                 answer.add(Fizz);
+                fizz = 0;
+            } else if(buzz == 5) {
+                answer.add(Buzz);
+                buzz = 0;
             } else answer.add(String.valueOf(i));
         }
         return answer;
