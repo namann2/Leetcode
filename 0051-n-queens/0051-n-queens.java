bc4af77f4c0018af31559d428405df48d1a61ab3
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-03-11 01:38:25 +0530

    Time: 2 ms (87.46%), Space: 44.8 MB (87.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,59 +1,52 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> answer = new ArrayList<>();
-        
-        char[][] board = new char[n][n];
-        for(char[] row : board)
+        char[][] grid = new char[n][n];
+        for(char[] row : grid) {
             Arrays.fill(row, '.');
-        
-        
+        }
+
         boolean[] rdiag = new boolean[2*n-1];
         boolean[] ndiag = new boolean[2*n-1];
         boolean[] cols = new boolean[n];
-        
-        nQueens(board, 0, rdiag, ndiag, cols, n, answer);
+
+        nQueens(grid, 0, n, answer, rdiag, ndiag, cols);
         return answer;
     }
-    
-    private void nQueens(char[][] board, int row, boolean[] rdiag, boolean[] ndiag, boolean[] cols, int n, List<List<String>> answer) {
-        if(row == n) {
-            List<String> t = new ArrayList<>();
-            for(int i = 0; i < n; i++) {
-                StringBuilder temp = new StringBuilder();
-                for(int j = 0; j < n; j++) {
-                    temp.append(board[i][j]);
-                }
-                t.add(temp.toString());
+
+    private void nQueens(char[][] grid, int r, int n, List<List<String>> answer, boolean[] rdiag, boolean[] ndiag, boolean[] cols) {
+        // base case
+        if(r == n) {
+            List<String> temp = new ArrayList<>();
+            for(char[] row : grid) {
+                temp.add(String.valueOf(row));
             }
-            answer.add(t);
+            answer.add(temp);
             return;
         }
-        
+        // main logic
         for(int c = 0; c < n; c++) {
-            boolean isCurrentCellSafe = isSafe(board, row, c, rdiag, ndiag, cols, n);
-            
+            boolean isCurrentCellSafe = isSafe(grid, r, c, n, rdiag, ndiag, cols);
             if(isCurrentCellSafe) {
-                rdiag[row - c + n - 1] = true;
-                ndiag[row + c] = true;
+                grid[r][c] = 'Q';
+                ndiag[r - c + n - 1] = true;
+                rdiag[r + c] = true;
                 cols[c] = true;
-                board[row][c] = 'Q';
-                
-                nQueens(board, row + 1, rdiag, ndiag, cols, n, answer);
-                
-                board[row][c] = '.';
-                rdiag[row - c + n - 1] = false;
-                ndiag[row + c] = false;
+
+                nQueens(grid, r + 1, n, answer, rdiag, ndiag, cols);
+
+                grid[r][c] = '.';
+                ndiag[r - c + n - 1] = false;
+                rdiag[r + c] = false;
                 cols[c] = false;
             }
         }
     }
-    
-    private boolean isSafe(char[][] board, int row, int col, boolean[] rdiag, boolean[] ndiag, boolean[] cols, int n) {
-        if( cols[col] == false &&
-            ndiag[row+col] == false &&
-            rdiag[row-col+n-1] == false) {
-            return true;
-        }
-        return false;
+
+    private boolean isSafe(char[][] grid, int r, int c, int n, boolean[] rdiag, boolean[] ndiag, boolean[] cols) {
+        return grid[r][c] == '.' && 
+                !ndiag[r - c + n - 1] && 
+                    !rdiag[r + c] && 
+                        !cols[c];
     }
 }
