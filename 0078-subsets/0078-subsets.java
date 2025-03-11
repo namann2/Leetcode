e2a92b091f22bc00016301161d5d98a6e66f6c32
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-03-11 23:01:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (46.61%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        subsets(nums, 0, new ArrayList<>(), answer);
-        return answer;
+        List<List<Integer>> result = new ArrayList<>();
+        subsets(nums, 0, nums.length, new ArrayList<>(), result);
+        return result;
     }
-    private void subsets(int[] nums, int index, List<Integer> temp, List<List<Integer>> answer) {
+
+    private void subsets(int[] nums, int index, int n, List<Integer> temp, List<List<Integer>> result) {
         // base case
-        if(index == nums.length) {
-            answer.add(new ArrayList<>(temp));
+        if(index == n) {
+            result.add(new ArrayList<>(temp));
             return;
         }
-        
+
         // main logic
-        temp.add(nums[index]);
-        subsets(nums, index+1, temp, answer);
-        temp.remove(temp.size()-1);
+        subsets(nums, index + 1, n, temp, result);
         
-        subsets(nums, index+1, temp, answer);
+        temp.add(nums[index]);
+        subsets(nums, index + 1, n, temp, result);
+        temp.remove(temp.size()-1);
     }
 }
