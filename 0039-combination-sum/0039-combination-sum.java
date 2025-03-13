40b77ea1e468714486911fa752c6e8c6cee45ef6
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-03-13 22:37:59 +0530

    Time: 2 ms (83.32%), Space: 44.9 MB (21.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> answer = new ArrayList<>();
-        combination(candidates, 0, candidates.length, new ArrayList<>(), target, answer);
-        return answer;
+        List<List<Integer>> result = new ArrayList<>();
+        int n = candidates.length;
+        cs(candidates, 0, n, target, new ArrayList<>(), result);
+        return result;
     }
-    private void combination(int[] candidates, int index, int n, List<Integer> temp, int target, List<List<Integer>> answer) {
+
+    private void cs(int[] candidates, int index, int n, int target, List<Integer> temp, List<List<Integer>> result) {
         // base case
-        if(target == 0) {
-            answer.add(new ArrayList<>(temp));
+        if(target < 0 || index >= n) {
             return;
         }
-        
-        if(target < 0 || index >= n)
+        if(target == 0) {
+            result.add(new ArrayList<>(temp));
             return;
-        
+        }
         // main logic
-        temp.add(candidates[index]);
-        combination(candidates, index, n, temp, target - candidates[index], answer);
-        temp.remove(temp.size()-1);
-        
-        combination(candidates, index+1, n, temp, target, answer);
+        for(int i = index; i < n; i++) {
+            temp.add(candidates[i]);
+            cs(candidates, i, n, target - candidates[i], temp, result);
+            temp.remove(temp.size()-1);
+        }
     }
 }
