adfe9ebbc0656a00ef7f0c442a8cc9d6adbecd71
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-03-29 13:34:19 +0530

    Time: 2294 ms (5.01%), Space: 55.7 MB (27.46%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -4,56 +4,61 @@ enum COLOR {
 
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
+        List<Integer> answer = new ArrayList<>();
         Map<Integer, List<Integer>> g = new HashMap<>();
-        boolean[] isSafeNodeReachable = new boolean[n];
+        int n = graph.length;
+        boolean[] isEventualSafeNode = new boolean[n];
+
         // construct graph
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             g.put(i, new ArrayList<>());
-            for(int j : graph[i]) {
+            for (int j : graph[i]) {
                 g.get(i).add(j);
             }
-            if(g.get(i).size() == 0) {
-                isSafeNodeReachable[i] = true;
-            }
+            if (g.get(i).size() == 0)
+                isEventualSafeNode[i] = true;
         }
 
-
-        for(int i = 0; i < n; i++) {
-            if(isSafeNodeReachable[i]) continue;
+        // step1 : dfs from each node, explore neigh node1, neigh node2.... neigh nodeN.
+        // step2 : for the selected node, check if all paths lead to an eventual safe node
+        for (int i = 0; i < n; i++) {
+            boolean isSafe = true;
             COLOR[] state = new COLOR[n];
             Arrays.fill(state, COLOR.WHITE);
-            boolean isSafe = true;
-            for(int j : g.get(i)) {
-                if(state[j] == COLOR.WHITE) {
-                    isSafe &= dfs(g, j, isSafeNodeReachable, state);
-                    if(!isSafe) break;
+
+            for (int neigh : g.get(i)) {
+                isSafe &= dfs(g, neigh, state, isEventualSafeNode);
+                if (!isSafe)
+                    break;
+            }
+            isEventualSafeNode[i] = isSafe;
+            if(isEventualSafeNode[i]) {
+                for (int neigh : g.get(i)) {
+                    isEventualSafeNode[neigh] = true;
                 }
             }
-            isSafeNodeReachable[i] = isSafe;
+            if(isEventualSafeNode[i]) answer.add(i);
         }
-        
-        // TODO : formulate answer from isSafeNodeReachable
-        List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            if(isSafeNodeReachable[i]) answer.add(i);
-        }
+
         return answer;
     }
 
-    private boolean dfs(Map<Integer, List<Integer>> g, int u, boolean[] isSafeNodeReachable, COLOR[] state) {
+    private boolean dfs(Map<Integer, List<Integer>> g, int u, COLOR[] state, boolean[] isEventualSafeNode) {
+        // base case
+        // if 'u' is the terminal node
+        if(isEventualSafeNode[u]) return true;
         state[u] = COLOR.GREY;
 
         boolean isSafe = true;
         for(int v : g.get(u)) {
-            if(isSafeNodeReachable[v]) continue;
-            if(state[v] == COLOR.GREY) return false;
+            if(isEventualSafeNode[v]) continue;
+            if(state[v] == COLOR.GREY) return false; // loop / cycle detected
             if(state[v] == COLOR.WHITE) {
-                isSafe &= dfs(g, v, isSafeNodeReachable, state);
+                isSafe &= dfs(g, v, state, isEventualSafeNode);
                 if(!isSafe) return false;
             }
         }
         state[u] = COLOR.BLACK;
-        return isSafe;
+        return true;
     }
 }
