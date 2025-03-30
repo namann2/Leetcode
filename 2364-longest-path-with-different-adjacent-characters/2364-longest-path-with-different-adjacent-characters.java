31f85f38c9a19fb02ab408746c1b49d41935d2fd
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-03-30 08:57:02 +0530

    Time: 160 ms (23.24%), Space: 97.8 MB (67.31%) - LeetHub

--- 2364-longest-path-with-different-adjacent-characters/2364-longest-path-with-different-adjacent-characters.java
+++ 2364-longest-path-with-different-adjacent-characters/2364-longest-path-with-different-adjacent-characters.java
@@ -1,35 +1,32 @@
 class Solution {
-    int longest = 1;
     public int longestPath(int[] parent, String s) {
         int n = parent.length;
         Map<Integer, List<Integer>> g = new HashMap<>();
-        int root = 0;
         for(int i = 0; i < n; i++) {
             g.putIfAbsent(parent[i], new ArrayList<>());
             g.get(parent[i]).add(i);
         }
-        dfs(g, root, s);
-        return longest;
+        return getLongestPath(g, 0, s)[1];
     }
 
-    private int dfs(Map<Integer, List<Integer>> g, int root, String s) {
-        // base case
-        if(!g.containsKey(root)) { // curr node is a leaf node
-            return 1;
-        }
-        // main logic
-        int firstMax = 0, secondMax = 0;
-        for(int child : g.get(root)) {
-            int childPathLength = dfs(g, child, s);
-            if(s.charAt(root) == s.charAt(child)) continue;
-            if(childPathLength > firstMax) {
-                secondMax = firstMax;
-                firstMax = childPathLength;
-            } else if(childPathLength > secondMax) {
-                secondMax = childPathLength;
+    private int[] getLongestPath(Map<Integer, List<Integer>> g, int root, String s) {
+        if(g.containsKey(root)) {
+            int firstMax = 0, secondMax = 0, longest = 0;
+            for(int child : g.get(root)) {
+                int[] childLongestPath = getLongestPath(g, child, s);
+                longest = Math.max(longest, childLongestPath[1]);
+                // consider all options with current node
+                if(s.charAt(root) == s.charAt(child)) continue;
+                if(firstMax < childLongestPath[0]) {
+                    secondMax = firstMax;
+                    firstMax = childLongestPath[0];
+                } else if(secondMax < childLongestPath[0]) {
+                    secondMax = childLongestPath[0];
+                }
             }
             longest = Math.max(longest, 1 + firstMax + secondMax);
+            return new int[]{1 + firstMax, longest};
         }
-        return 1 + firstMax;
+        return new int[]{1, 1}; // leaf node
     }
 }
