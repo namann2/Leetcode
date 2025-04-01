0dbcf1fbfcd138647a18c423fea4f0a02a1acd24
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-01 12:11:47 +0530

    Time: 1 ms (99.3%), Space: 42.5 MB (37.87%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,56 +1,59 @@
 class Solution {
+
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, List<Pair<String, Double>>> g = new HashMap<>();
         constructGraph(g, equations, values);
-        
+
         int qsize = queries.size();
-        double[] ans = new double[qsize];
-        Arrays.fill(ans, -1d);
-        
+        double[] answer = new double[qsize];
+        Arrays.fill(answer, -1d);
+
         for(int i = 0; i < qsize; i++) {
             List<String> query = queries.get(i);
-            String u = query.get(0);
-            String v = query.get(1);
+            String u = query.get(0), v = query.get(1);
             if(g.containsKey(u))
-                ans[i] = dfs(g, u, v, new HashSet());
+                answer[i] = dfs(g, u, v, new HashSet<>());
         }
-        return ans;
+        return answer;
     }
+
     private void constructGraph(Map<String, List<Pair<String, Double>>> g, List<List<String>> equations, double[] values) {
         int n = values.length;
         for(int i = 0; i < n; i++) {
             List<String> equation = equations.get(i);
-            String A = equation.get(0), B = equation.get(1);
-            
-            g.putIfAbsent(A, new ArrayList<>());
-            g.putIfAbsent(B, new ArrayList<>());
-            
-            Pair p1 = new Pair(B, values[i]);
-            Pair p2 = new Pair(A, 1 / values[i]);
-            
-            g.get(A).add(p1);
-            g.get(B).add(p2);
+            String u = equation.get(0), v = equation.get(1);
+            updateGraph(g, u, v, values[i]);
         }
     }
-    
-    private double dfs(Map<String, List<Pair<String, Double>>> g, String u, String v, HashSet<String> visited) {
+
+    private void updateGraph(Map<String, List<Pair<String, Double>>> g, String u, String v, double value) {
+        g.putIfAbsent(u, new ArrayList<>());
+        g.get(u).add(new Pair(v, value));
+        g.putIfAbsent(v, new ArrayList<>());
+        g.get(v).add(new Pair(u, 1 / value));
+    }
+
+    private double dfs(Map<String, List<Pair<String, Double>>> g, String source, String destination, Set<String> visited) {
         
-        visited.add(u);
-        
-        if(u.equals(v))
+        visited.add(source);
+
+        if(source.equals(destination)) 
             return 1d;
-        
-        if(!g.containsKey(u))
-            return -1d;
-        
-        for(Pair<String, Double> p : g.get(u)) {
-            if(!visited.contains(p.getKey())) {
-                double value = dfs(g, p.getKey(), v, visited);
-                if(value != -1d)
-                    return value * p.getValue();
+
+        if(g.containsKey(source)) {
+            for(Pair<String, Double> neigh : g.get(source)) {
+                String currNode = neigh.getKey();
+                Double value = neigh.getValue();
+                if(!visited.contains(currNode)) {
+                    double returnValue = dfs(g, currNode, destination, visited);
+                    if(returnValue != -1d) {
+                        updateGraph(g, source, currNode, value * returnValue);
+                        return value * returnValue;
+                    }
+                }
             }
         }
-        
+
         return -1d;
     }
 }
