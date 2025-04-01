1cecc4e6b4120dd178f4ed0d344f95b0710c2c6c
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-01 12:14:18 +0530

    Update 399-evaluate-division.java

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -14,74 +14,61 @@
 
 class Solution {
 
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {    
-        Map<String, List<Pair>> g = constructGraph(equations, values);
-        int n = queries.size();
-        double[]answer = new double[n];
-        Arrays.fill(answer, -1.0);
-        
-        for(int i=0;i<n;i++) {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Pair<String, Double>>> g = new HashMap<>();
+        constructGraph(g, equations, values);
+
+        int qsize = queries.size();
+        double[] answer = new double[qsize];
+        Arrays.fill(answer, -1d);
+
+        for(int i = 0; i < qsize; i++) {
             List<String> query = queries.get(i);
-            String source = query.get(0);
-            String destination = query.get(1);
-            if(g.containsKey(source))
-                answer[i] = dfs(g, source, destination, new HashSet());
+            String u = query.get(0), v = query.get(1);
+            if(g.containsKey(u))
+                answer[i] = dfs(g, u, v, new HashSet<>());
         }
         return answer;
     }
-    
-    private Map<String, List<Pair>> constructGraph(List<List<String>> equations, double[] values) {
-        Map<String, List<Pair>> g = new HashMap<>();
+
+    private void constructGraph(Map<String, List<Pair<String, Double>>> g, List<List<String>> equations, double[] values) {
         int n = values.length;
-        for(int i=0;i<n;i++) {
+        for(int i = 0; i < n; i++) {
             List<String> equation = equations.get(i);
-            String source = equation.get(0);
-            String destination = equation.get(1);
-            
-            g.putIfAbsent(source, new ArrayList<>());
-            g.putIfAbsent(destination, new ArrayList<>());
-            
-            Pair currPair1 = new Pair(destination, values[i]);
-            Pair currPair2 = new Pair(source, 1/values[i]);
-            
-            g.get(source).add(currPair1);
-            g.get(destination).add(currPair2);
+            String u = equation.get(0), v = equation.get(1);
+            updateGraph(g, u, v, values[i]);
         }
-        return g;
     }
-    
-    private double dfs(Map<String, List<Pair>> g, String source, String destination, Set<String> visited) {
-        if(source.equals(destination))
-            return 1.0;
+
+    private void updateGraph(Map<String, List<Pair<String, Double>>> g, String u, String v, double value) {
+        g.putIfAbsent(u, new ArrayList<>());
+        g.get(u).add(new Pair(v, value));
+        g.putIfAbsent(v, new ArrayList<>());
+        g.get(v).add(new Pair(u, 1 / value));
+    }
+
+    private double dfs(Map<String, List<Pair<String, Double>>> g, String source, String destination, Set<String> visited) {
+        
+        visited.add(source);
+
+        if(source.equals(destination)) 
+            return 1d;
 
         if(g.containsKey(source)) {
-            visited.add(source);
-            for(Pair pair : g.get(source)) {
-                String node = pair.getNodeRepresentation();
-                double value = pair.getNodeValue();
-                if(visited.contains(node)) continue;
-                double ans = dfs(g, node, destination, visited);
-                if(ans != -1.0) 
-                    return ans * value;
+            for(Pair<String, Double> neigh : g.get(source)) {
+                String currNode = neigh.getKey();
+                Double value = neigh.getValue();
+                if(!visited.contains(currNode)) {
+                    double returnValue = dfs(g, currNode, destination, visited);
+                    if(returnValue != -1d) {
+                        // update all the intermediate findings
+                        updateGraph(g, source, currNode, value * returnValue);
+                        return value * returnValue;
+                    }
+                }
             }
         }
-        return -1.0;
-    }
-}
 
-class Pair {
-    private final String node;
-    private final double val;
-    Pair(String node, double val) {
-        this.node = node;
-        this.val = val;
-    }
-    
-    public String getNodeRepresentation() {
-        return this.node;
-    }
-    
-    public double getNodeValue() {
-        return this.val;
+        return -1d;
     }
 }
