8f718a6481b0aa90a9d50bb93430e87ae9c24780
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-01 15:34:25 +0530

    Time: 0 ms (100%), Space: 40.8 MB (89.54%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,19 +4,19 @@ class Solution {
         combination(1, 10, k, n, new ArrayList<>(), answer);
         return answer;
     }
-    
+
     private void combination(int start, int n, int k, int target, List<Integer> temp, List<List<Integer>> answer) {
         if(target == 0 && k == 0) {
             answer.add(new ArrayList<>(temp));
             return;
         }
-        
-        if(k < 0 || target < 0 || start >= n) return;
-        
+
+        if(start >= n || k < 0 || target < 0) return;
+
         temp.add(start);
         combination(start + 1, n, k - 1, target - start, temp, answer);
         temp.remove(temp.size()-1);
-        
+
         combination(start + 1, n, k, target, temp, answer);
     }
 }
