fde8aaf244b1aa5694a0213bf11eb6e5335406fc
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-02 21:21:45 +0530

    Time: 8 ms (45.35%), Space: 44.7 MB (50%) - LeetHub

--- 0679-24-game/0679-24-game.java
+++ 0679-24-game/0679-24-game.java
@@ -1,49 +1,51 @@
 class Solution {
+
+    private static final double EPS = 0.0000001d, REQUIRED = 24d;
+
     public boolean judgePoint24(int[] cards) {
+        int n = cards.length;
         List<Double> list = new ArrayList<>();
-        for(int card : cards)
-            list.add(1d * card);
-        
-        return judge(list, list.size());
+        for(int i = 0; i < n; i++)
+            list.add(1d * cards[i]);
+        return settleGame(list);
     }
-    
-    private boolean judge(List<Double> cards, int n) {
+
+    private boolean settleGame(List<Double> list) {
+        int n = list.size();
         // base case
-        if(cards.size() == 1) {
-            return Math.abs(cards.get(0) - 24) <= 0.000000001;
-        } 
-        
-        // n * (n^2 * )
+        if(n == 1) {
+            return Math.abs(list.get(0) - REQUIRED) <= EPS;
+        }
+
+        // main logic
         for(int i = 0; i < n; i++) {
+            double first = list.get(i);
             for(int j = 0; j < n; j++) {
                 if(i == j) continue;
-                // get all options from i and j
-                List<Double> options = getOptions(cards.get(i), cards.get(j));
-                List<Double> nextIteration = new ArrayList<>();
-                for(int k = 0; k < n; k ++) {
-                    if(k != i && k != j) {
-                        nextIteration.add(cards.get(k));
-                    }
+                double second = list.get(j);
+                List<Double> newList = new ArrayList<>();
+                for(int k = 0; k < n; k++) {
+                    if(k == i || k == j) continue;
+                    newList.add(list.get(k));
                 }
-                
-                for(double num : options) {
-                    nextIteration.add(num);
-                    if(judge(nextIteration, nextIteration.size())) return true;
-                    nextIteration.remove(nextIteration.size()-1);
+                List<Double> options = getOptions(first, second);
+                for(double option : options) {
+                    newList.add(option);
+                    if(settleGame(newList))
+                        return true;
+                    newList.remove(newList.size()-1);
                 }
             }
         }
         return false;
     }
-    
-    private List<Double> getOptions(double a, double b) {
-        List<Double> options = new ArrayList<>();
-        options.add(a + b);
-        options.add(a - b);
-        options.add(b - a);
-        options.add(a * b);
-        options.add(a / b);
-        options.add(b / a);
-        return options;
+
+    private List<Double> getOptions(double first, double second) {
+        return List.of(
+            first + second,
+            first - second,
+            first * second,
+            first / second
+        );
     }
 }
