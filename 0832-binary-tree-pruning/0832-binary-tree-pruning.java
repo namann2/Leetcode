2ae2c38a02d480f2f41b6eff2589413af7cc98a7
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-04 21:37:07 +0530

    Time: 0 ms (100%), Space: 42 MB (5.08%) - LeetHub

--- 0832-binary-tree-pruning/0832-binary-tree-pruning.java
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.java
@@ -1,20 +1,11 @@
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
         if(root == null) return null;
-        boolean isRoot = prune(root);
-        return !isRoot ? null : root;
-    }
+        
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
 
-    private boolean prune(TreeNode root) {
-        if(root == null) return false;
-        if(root.left == null && root.right == null) return root.val == 1;
-
-        boolean left = prune(root.left);
-        boolean right = prune(root.right);
-
-        if(!left) root.left = null;
-        if(!right) root.right = null;
-
-        return left || right || root.val == 1;
+        if(root.val == 1 || (root.left != null || root.right != null)) return root;
+        return null;
     }
 }
