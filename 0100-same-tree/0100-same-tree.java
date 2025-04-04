f75d2aff350784c73375f6c5e1745a7f82f99682
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-04 21:14:47 +0530

    Time: 0 ms (100%), Space: 41.1 MB (68.8%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // TC : O(N + M), SC : O(H1 + H2)
-        // In worst case, N = M and H1 = H2 i.e. TC : O(N), SC : O(H)
-        if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
+        if(p == null) return q == null;
+        if(q == null) return p == null;
         
         if(p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
