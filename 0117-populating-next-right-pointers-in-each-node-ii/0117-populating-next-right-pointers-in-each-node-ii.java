a7e8974878f4952fe29315c0b695aae98e980dc6
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-07 15:33:41 +0530

    Time: 1 ms (67.76%), Space: 44.6 MB (27.62%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     public Node connect(Node root) {
-        if(root == null || isLeaf(root)) return root;
-        
-        if(root.right != null) root.right.next = findNext(root.next);
-        if(root.left != null) root.left.next = root.right != null ? root.right : findNext(root.next);
-        
-        connect(root.right);
-        connect(root.left);
-        
-        return root;
-    }
-    
-    private Node findNext(Node root) {
         if(root == null) return null;
-        if(root.left != null) return root.left;
-        if(root.right != null) return root.right;
-        return findNext(root.next);
-    }
-    private boolean isLeaf(Node root) {
-        return root.left == null && root.right == null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            Node next = null;
+            for(int i = 0; i < size; i++) {
+                Node curr = q.poll();
+                if(next != null) {
+                    curr.next = next;
+                }
+                next = curr;
+                if(curr.right != null) q.offer(curr.right);
+                if(curr.left != null) q.offer(curr.left);
+            }
+        }
+        return root;
     }
 }
