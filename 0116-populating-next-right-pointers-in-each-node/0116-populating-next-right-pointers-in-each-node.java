34f12f2d8d37c3459fb5b1d415b88868fa7610e4
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-04-07 15:18:49 +0530

    Time: 0 ms (100%), Space: 44.8 MB (23.17%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,15 +1,10 @@
 class Solution {
     public Node connect(Node root) {
-        if(root == null || isLeaf(root)) return root;
-        
-        if(root.next != null) root.right.next = root.next.left;
-        root.left.next = root.right;
-        
-        connect(root.right);
+        if(root == null) return null;
+        if(root.next != null && root.right != null) root.right.next = root.next.left;
+        if(root.left != null) root.left.next = root.right;
         connect(root.left);
+        connect(root.right);
         return root;
-    }
-    private boolean isLeaf(Node root) {
-        return root.left == null && root.right == null;
     }
 }
