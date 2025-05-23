d46a63c0b6aa8d45a8fd19e6def0ed5eac0a809d
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-23 17:43:18 +0530

    Time: 9 ms (22.95%), Space: 45.5 MB (49.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,35 @@
-enum COLOR {
-    WHITE, GREY, BLACK;
-}
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // if two courses depend on each other then, it is not possible ->> cycle detection in directed graph
-
-        // construct graph
+        // b -> a
+        // check for cycles
+        // since there is an order to maintain - topological sort is a good choice
+        // if any indegree is not zero - not possible
         Map<Integer, List<Integer>> g = new HashMap<>();
+        int[] indegree = new int[numCourses];
         for(int[] prerequisite : prerequisites) {
-            g.putIfAbsent(prerequisite[0], new ArrayList<>());
-            g.get(prerequisite[0]).add(prerequisite[1]);
+            g.putIfAbsent(prerequisite[1], new ArrayList<>());
+            g.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
 
-        COLOR[] state = new COLOR[numCourses];
-        Arrays.fill(state, COLOR.WHITE);
-
-        // we are not sure if the given graph is connected
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < numCourses; i++) {
-            if(state[i] == COLOR.WHITE) {
-                if(dfs(g, i, state)) return false;
+            if(indegree[i] == 0) q.offer(i);
+        }
+        
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            if(!g.containsKey(curr)) continue;
+            for(int neigh : g.get(curr)) {
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) 
+                    q.offer(neigh);
             }
         }
-        return true;
-    }
 
-    private boolean dfs(Map<Integer, List<Integer>> g, int u, COLOR[] state) {
-        state[u] = COLOR.GREY;
-        if(g.containsKey(u)) {
-            for(int v : g.get(u)) {
-                if(state[v] == COLOR.GREY) return true;
-                if(state[v] == COLOR.WHITE) {
-                    if(dfs(g, v, state)) return true;
-                }
-            } 
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] != 0) return false;
         }
-        state[u] = COLOR.BLACK;
-        return false;
+        return true;
     }
 }
