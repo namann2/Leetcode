6aae02e90dd05b1f2290acaded67dd22a09ff060
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-23 09:33:14 +0530

    Time: 14 ms (53.93%), Space: 47.5 MB (87.84%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,56 +1,46 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        /*
-            1. Single Source Shortest Path
-            Options: 
-                1. Djikstra
-                2. Bellman Ford
-            Question : 
-                Can there be any neg edge ? No, as per the given constraints
-                the edge weight can not be negative hence, we can use either of the above
-                algorithm but which one is optimal for our use-case ?
-                
-                Djisktra is more optimal for positive edge weights
+    public int networkDelayTime(int[][] times, int n, int source) {
+        /* 
+            This is a direct application of Djikstra Algorithm.
+            Given the edge weights are positive, bellman ford would be an overkill
         */
-        
-        // construct graph
+
         Map<Integer, List<int[]>> g = new HashMap<>();
-        for(int[] time : times) {
-            int src = time[0] - 1, dest = time[1] - 1, weight = time[2];
-            g.putIfAbsent(src, new ArrayList<>());
-            g.get(src).add(new int[]{dest, weight});
+        int length = times.length;
+        for(int i = 0; i < length; i++) {
+            g.putIfAbsent(times[i][0], new ArrayList<>());
+            g.get(times[i][0]).add(new int[]{times[i][1], times[i][2]});
         }
 
-        PriorityQueue<int[]> q = new PriorityQueue<>((p1, p2) -> {
-            return p1[1] - p2[1];
-        });
-        q.offer(new int[]{k-1, 0});
-
-        int INF = (int)(1e9+7);
-        int[] timeTaken = new int[n];
+        int INF = (int)1e9+7;
+        int[] timeTaken = new int[n+1];
         Arrays.fill(timeTaken, INF);
-        timeTaken[k-1] = 0;
+        timeTaken[source] = 0;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p, q) -> {
+            return p[1] - q[1];
+        });
+        // node, timeTakenToReach
+        pq.offer(new int[]{source, 0});
 
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            int u = curr[0], dist = curr[1];
-            if(!g.containsKey(u)) continue;
-            for(int[] neigh : g.get(u)) {
-                int v = neigh[0], d = neigh[1];
-                if(timeTaken[v] > timeTaken[u] + d) {
-                    timeTaken[v] = timeTaken[u] + d;
-                    q.offer(new int[]{v, timeTaken[v]});
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int currNode = curr[0], currTime = curr[1];
+            if(!g.containsKey(currNode)) continue;
+            for(int[] neigh : g.get(currNode)) {
+                int neighNode = neigh[0], neighTime = neigh[1];
+                if(timeTaken[neighNode] > currTime + neighTime) {
+                    timeTaken[neighNode] = currTime + neighTime;
+                    pq.offer(new int[]{neighNode, timeTaken[neighNode]});
                 }
             }
         }
 
-        System.out.println(Arrays.toString(timeTaken));
-        int maxTime = 0; // dist(src, src)
-        for(int i = 0; i < n; i++) {
-            if(timeTaken[i] == INF) return -1; // we are unable to reach a node
-            if(i != k-1)
-                maxTime = Math.max(maxTime, timeTaken[i]);
+        int min = -1;
+        for(int i = 1; i < n+1; i++) {
+            if(timeTaken[i] == INF) return -1;
+            min = Math.max(min, timeTaken[i]);
         }
-        return maxTime;
+        return min;
     }
 }
