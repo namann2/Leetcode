fc7293a7a9ba5593445c035608232f211028292d
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-23 17:59:38 +0530

    Time: 7 ms (35.21%), Space: 45.7 MB (87.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,39 @@
 class Solution {
-    public int[] findOrder(int n, int[][] prerequisites) {
-        List<List<Integer>> g = new ArrayList<>();
-        for(int i = 0; i < n; i++)
-            g.add(new ArrayList<>());
-            
-        for(int[] p : prerequisites) {
-            g.get(p[1]).add(p[0]);
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> g = new HashMap<>();
+        int[] indegree = new int[numCourses];
+
+        for(int[] prerequisite : prerequisites) {
+            g.putIfAbsent(prerequisite[1], new ArrayList<>());
+            // [a,b] means b -> a 
+            g.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
-        
-        int[] indegree = new int[n];
-        for(int i = 0; i < n; i++) {
-            for(int v : g.get(i))
-                indegree[v]++;
+
+        int[] answer = new int[numCourses];
+        int idx = 0;
+        Queue<Integer> q = new LinkedList<>();
+        int completedCourses = 0;
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.offer(i);
+                completedCourses++;
+                answer[idx++] = i;
+            }
         }
-        
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i = 0; i < n; i++)
-            if(indegree[i] == 0) 
-                q.addLast(i);
-        
-        int[] order = new int[n];
-        int index = 0;
+
         while(!q.isEmpty()) {
-            int u = q.removeFirst();
-            order[index++] = u;
-            for(int v : g.get(u)) {
-                indegree[v] -= 1;
-                if(indegree[v] == 0) {
-                    q.addLast(v);
+            int currCourse = q.poll();
+            if(!g.containsKey(currCourse)) continue;
+            for(int v : g.get(currCourse)) {
+                if(--indegree[v] == 0) {
+                    q.offer(v);
+                    completedCourses++;
+                    answer[idx++] = v;
                 }
             }
         }
-        
-        for(int i = 0; i < n; i++)
-            if(indegree[i] != 0) return new int[]{};
-        
-        return order;
+
+        return completedCourses == numCourses ? answer : new int[]{};
     }
 }
