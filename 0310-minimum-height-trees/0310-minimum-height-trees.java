9360b1c01d868f3099ba4fef46fb65cbbfb6eaf3
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-24 14:40:24 +0530

    Time: 25 ms (65.83%), Space: 57.8 MB (18.5%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,10 +1,9 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if(n == 1) return List.of(0);
-        int[] degree = new int[n];
         Map<Integer, List<Integer>> g = new HashMap<>();
-        List<Integer> answer = new ArrayList<>();
-        for (int[] edge : edges) {
+        int[] degree = new int[n];
+        for(int[] edge : edges) {
             g.putIfAbsent(edge[0], new ArrayList<>());
             g.putIfAbsent(edge[1], new ArrayList<>());
             g.get(edge[0]).add(edge[1]);
@@ -14,28 +13,28 @@ class Solution {
         }
 
         Queue<Integer> leaves = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (degree[i] == 1)
+        int nodesRemaining = n;
+        for(int i = 0; i < n; i++) {
+            if(degree[i] == 1) {
                 leaves.offer(i);
+            }
         }
 
-        while (n > 2) {
-            int size = leaves.size();
-            n -= size;
-            // remove all leafves at one level
-            for (int i = 0; i < size; i++) {
-                int currLeaf = leaves.poll();
-                if (g.containsKey(currLeaf)) {
-                    for (int connectedNode : g.get(currLeaf)) {
-                        if (--degree[connectedNode] == 1) {
-                            leaves.offer(connectedNode);
-                        }
+        while(nodesRemaining > 2) {
+            int leafNodeCount = leaves.size();
+            nodesRemaining -= leafNodeCount;
+            for(int i = 0; i < leafNodeCount; i++) {
+                int leafNode = leaves.poll();
+                for(int neigh : g.get(leafNode)) {
+                    if(--degree[neigh] == 1) {
+                        leaves.offer(neigh);
                     }
                 }
             }
         }
 
-        while (!leaves.isEmpty()) {
+        List<Integer> answer = new ArrayList<>();
+        while(!leaves.isEmpty()) {
             answer.add(leaves.poll());
         }
         return answer;
