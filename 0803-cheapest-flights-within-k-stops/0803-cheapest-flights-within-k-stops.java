6205a1ac6a6a0b2745a79d267dbdbbae3aabdbb7
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-25 12:39:10 +0530

    Time: 8 ms (34.77%), Space: 44.5 MB (57.32%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -2,31 +2,33 @@ class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer, List<int[]>> g = new HashMap<>();
         for(int[] flight : flights) {
-            int srcn = flight[0], dest = flight[1], cost = flight[2];
-            g.putIfAbsent(srcn, new ArrayList<>());
-            g.get(srcn).add(new int[]{dest, cost});
+            g.putIfAbsent(flight[0], new ArrayList<>());
+            g.get(flight[0]).add(new int[]{flight[1], flight[2]});
         }
 
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{src, 0, 0});
-        int[] distance = new int[n];
-        int INF = (int)(1e9+7);
-        Arrays.fill(distance, INF);
-        distance[src] = 0;
 
+        int INF = Integer.MAX_VALUE;
+        int[] priceToReach = new int[n];
+        Arrays.fill(priceToReach, INF);
+        priceToReach[src] = 0;
+
+        q.offer(new int[]{src, 0, 0});
         while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            int city = curr[0], ck = curr[1], cdis = curr[2];
-            if(g.containsKey(city)) {
-                for(int[] neigh : g.get(city)) {
-                    int dest = neigh[0], cost = neigh[1];
-                    if(distance[dest] > cdis + cost && ck <= k) {
-                        distance[dest] = cdis + cost;
-                        q.offer(new int[]{dest, ck + 1, distance[dest]});
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int currNode = curr[0], currK = curr[1], currPrice = curr[2];
+                if(!g.containsKey(currNode)) continue;
+                for(int[] neighNode : g.get(currNode)) {
+                    int neigh = neighNode[0], incurCost = neighNode[1];
+                    if(currK <= k && priceToReach[neigh] > currPrice + incurCost) {
+                        priceToReach[neigh] = currPrice + incurCost;
+                        q.offer(new int[]{neigh, currK + 1, priceToReach[neigh]});
                     }
                 }
             }
         }
-        return distance[dst] == INF ? -1 : distance[dst];
+        return priceToReach[dst] == INF ? -1 : priceToReach[dst];
     }
 }
