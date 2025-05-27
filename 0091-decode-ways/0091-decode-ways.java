c3fd54f42642c169a45547b09ff6caab942cb416
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-05-27 17:47:03 +0530

    Time: 1 ms (84.55%), Space: 41.8 MB (64.96%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,47 +1,29 @@
 class Solution {
     public int numDecodings(String s) {
+        // dp[i] = num of ways in which we can decode string ending at ith index
         int n = s.length();
         int[]dp = new int[n+1];
-        /*
-            dp state = number of ways to decode a string ending at ith index
-            TC : O(n)
-            SC : O(n)
-            Since we are only dependent on previous two values, we can 
-            save space.
-            
-            prev1 = single digit
-            prev2 = double digit
-            
-            ----- ---- ------- 
-            prev2 prev1 current
-        */
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        for(int i=2;i<n+1;i++) {
-            int single_digit = s.charAt(i-1) - '0';
-            if(single_digit >= 1 && single_digit <= 9) 
-                dp[i] = dp[i-1];
-            int double_digit = Integer.valueOf(s.substring(i-2, i));
-            if(double_digit > 9 && double_digit <= 26)
-                dp[i] += dp[i-2];
+        dp[0] = 1; // empty string
+        for(int i = 1; i < n+1; i++) {
+            int ch = s.charAt(i-1) - '0';
+            if(ch > 0 && ch < 10) dp[i] = dp[i-1];
+            Integer double_digit = i-2 >= 0 ? Integer.valueOf(s.substring(i-2, i)) : 0;
+            if(double_digit > 9 && double_digit < 27) dp[i] += dp[i-2];
         }
         return dp[n];
-        
-        /*
-        int prev2 = 1, prev1 = s.charAt(0) == '0' ? 0 : 1;
-        for(int i=2;i<n+1;i++) {
-            int current = 0;
-            int single_digit = s.charAt(i-1) - '0';
-            if(single_digit >= 1 && single_digit <= 9) 
-                current = prev1;
-            int double_digit = Integer.valueOf(s.substring(i-2, i));
-            if(double_digit > 9 && double_digit <= 26)
-                current += prev2;
-            
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return prev1;
-        */
     }
 }
+
+
+
+/* 
+
+prev = 0
+
+prev = 1 | 2 | 3 | 4
+
+
+curr = 0 OR non-zero
+
+
+*/
