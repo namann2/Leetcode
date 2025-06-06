408e7d961dacc2d3b2d2152b037ba7fe205d88bf
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-06 23:47:40 +0530

    Time: 247 ms (5.09%), Space: 100.6 MB (5.03%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,24 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length, k = 2;
-        int[][][]dp = new int[n+1][2][k+1];
-        // TC : O(n * k)
-        // SC : O(nk)
-        for(int index = n - 1; index >= 0; index --) {
-            for(int canBuy = 0; canBuy < 2; canBuy ++) {
-                for(int trans = 1; trans <= k; trans ++) {
-                    if(canBuy == 1) {
-                        int buy = -prices[index] + dp[index+1][0][trans];
-                        int notBuy = dp[index+1][1][trans];
-                        dp[index][canBuy][trans] = Math.max(buy, notBuy);
-                    } else {
-                        int sell = prices[index] + dp[index+1][1][trans-1];
-                        int notSell = dp[index+1][0][trans];
-                        dp[index][canBuy][trans] = Math.max(sell, notSell);
-                    }
-                }
-            }
+        Integer[][][]dp = new Integer[n+1][k+1][2];
+        // 0 -> can buy = true
+        // 1 -> can buy = false
+        return maxProfit(prices, 0, n, k, 0, dp);
+    }
+
+    // helper function
+
+    private int maxProfit(int[] prices, int index, int n, int k, int canBuy, Integer[][][]dp) {
+        // base case
+        if(k == 0) return 0;
+        if(index == n) return 0;
+        if(dp[index][k][canBuy] != null)
+            return dp[index][k][canBuy];
+        // main logic
+        if(canBuy == 0) {
+            int buy = -prices[index] + maxProfit(prices, index + 1, n, k, 1, dp);
+            int notBuy = maxProfit(prices, index + 1, n, k, 0, dp);
+            return dp[index][k][canBuy] = Math.max(buy, notBuy);
+        } else {
+            int sell = prices[index] + maxProfit(prices, index + 1, n, k-1, 0, dp);
+            int notSell = maxProfit(prices, index + 1, n, k, 1, dp);
+            return dp[index][k][canBuy] = Math.max(sell, notSell);
         }
-        return dp[0][1][2];
     }
 }
