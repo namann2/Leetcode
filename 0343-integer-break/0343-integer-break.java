ba4034a0f379a5570fb8c06f6733d7c5eb884651
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-06 09:30:27 +0530

    Time: 1 ms (35.91%), Space: 40.6 MB (41.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,9 +2,10 @@ class Solution {
     public int integerBreak(int n) {
         int[] dp = new int[n+1];
         dp[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            for(int k = 1; k <= i-1; k++) {
-                dp[i] = Math.max(dp[i], k * Math.max((i-k), dp[i-k]));
+        for(int i = 2; i < n+1; i++) {
+            dp[i] = Integer.MIN_VALUE;
+            for(int k = 1; k < i; k++) {
+                dp[i] = Math.max(dp[i], k * Math.max(i-k, dp[i-k]));
             }
         }
         return dp[n];
