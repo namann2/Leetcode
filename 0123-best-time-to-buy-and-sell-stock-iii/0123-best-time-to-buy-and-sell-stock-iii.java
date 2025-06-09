bb970dab445b6d276da768f1cff970c396aec9ba
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-09 15:09:11 +0530

    Time: 73 ms (30.57%), Space: 65.5 MB (29.81%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,34 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length, k = 2;
-        Integer[][][]dp = new Integer[n+1][k+1][2];
-        /* 2 options
-            1. buy
-            2. sell
-            each option has two choices : can and can not &
-            to begin a transaction - we need to buy
-        */
-        int option = 1; // true means -> I have the option to buy
-        return maxProfit(prices, 0, n, k, 1, dp);
-    }
+        int[][][]dp = new int[n+1][k+1][2];
 
-    // helper functions
-
-    private int maxProfit(int[] prices, int index, int n, int k, int canBuy, Integer[][][] dp) {
-        // base case
-        if(k == 0) return 0;
-        if(index == n) return 0;
-        if(dp[index][k][canBuy] != null) 
-            return dp[index][k][canBuy];
-        // main logic
-        if(canBuy == 1) {
-            int buy = -prices[index] + maxProfit(prices, index + 1, n, k, 0, dp);
-            int dontBuy = maxProfit(prices, index + 1, n, k, 1, dp);
-            return dp[index][k][canBuy] = Math.max(buy, dontBuy);
-        } else {
-            int sell = prices[index] + maxProfit(prices, index + 1, n, k-1, 1, dp);
-            int dontSell = maxProfit(prices, index + 1, n, k, 0, dp);
-            return dp[index][k][canBuy] = Math.max(sell, dontSell);
+        for(int day = n-1; day >= 0; day--) {
+            for(int transaction = 1; transaction <= k; transaction++) {
+                for(int option = 0; option <= 1; option++) {
+                    if(option == 1) {
+                        int buy = -prices[day] +  dp[day+1][transaction][0];
+                        int dontBuy = dp[day+1][transaction][1];
+                        dp[day][transaction][option] = Math.max(buy, dontBuy);
+                    } else {
+                        int sell = prices[day] + dp[day+1][transaction-1][1];
+                        int dontSell = dp[day+1][transaction][0];
+                        dp[day][transaction][option] = Math.max(sell, dontSell);
+                    }
+                }
+            }
         }
+        return dp[0][k][1];
     }
 }
