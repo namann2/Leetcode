44cf01e807c39e9682437d4e4c3ac4279f6d5d33
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-14 01:07:28 +0530

    Time: 43 ms (62.88%), Space: 45.2 MB (68.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -4,24 +4,27 @@ class Solution {
         if((sum & 1) == 1) return false;
         int n = nums.length, T = sum / 2;
         // bottom up
-        boolean[][]dp = new boolean[n][T + 1];
         // step1 : base case
-        for(int index = 0; index < n; index++) dp[index][0] = true;
+        boolean[] prev = new boolean[T+1];
+        
+        prev[0] = true;
         for(int target = 0; target < T + 1; target++) {
-            if(nums[0] == target) dp[0][target] = true;
+            if(nums[0] == target) prev[target] = true;
         }
 
         // step2 : iteration
         for(int index = 1; index < n; index ++) {
+            boolean[] curr = new boolean[T+1];
             for(int target = 0; target < T + 1; target++) {
-                boolean skip = dp[index-1][target];
+                boolean skip = prev[target];
                 boolean take = false;
                 if(nums[index] <= target) {
-                    take = dp[index-1][target - nums[index]];
+                    take = prev[target - nums[index]];
                 } 
-                dp[index][target] = take || skip;
+                curr[target] = take || skip;
             }
+            prev = curr;
         }
-        return dp[n-1][T];
+        return prev[T];
     }
 }
