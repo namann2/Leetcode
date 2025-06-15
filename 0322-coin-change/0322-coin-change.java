bb958f64aabb529ef81ec2a94a795df9acdb4d05
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-15 15:49:00 +0530

    Time: 20 ms (39.38%), Space: 45 MB (37.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,47 +1,37 @@
 class Solution {
-    public int coinChange(int[] coins, int target) {
+    public int coinChange(int[] coins, int A) {
         int n = coins.length;
-        int[][]dp = new int[n+1][target+1];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        int min_coins = coinChange(coins, 0, n, target, dp);
-        return min_coins == Integer.MAX_VALUE ? -1 : min_coins;
+        // dp state = min number of coins required to make Ath amount from coins till ith index
+        int[][]dp = new int[n][A+1];
+        for(int[] row : dp) Arrays.fill(row, Integer.MAX_VALUE);
+
+        int[]prev = new int[A+1];
+        Arrays.fill(prev, Integer.MAX_VALUE);
+
+        // step 1 : base case
+        prev[0] = 0;
         
-        // bottom up
-//         for(int i = 0; i < n; i++) {
-//             if(target % coins[i] == 0) dp[i][target] = target / coins[i];
-//             else dp[i][target] = Integer.MAX_VALUE;
-//         }
-        
-//         for(int i = n - 1; i >= 0; i--) {
-//             for(int j = target; j >= 0; j--) {
-//                 int skip = dp[i+1][j]; // coinChange(coins, index+1, n, target, dp);
-//                 int take = dp[i][j - coins[i]]; // coinChange(coins, index, n, target - coins[index], dp);
-//                 if(take != Integer.MAX_VALUE)
-//                     take++;
-//                 dp[i][j] = Math.min(skip, take);
-//             }
-//         }
-        
-//         return dp[0][target] == Integer.MAX_VALUE ? -1 : dp[0][target];
-    }
-    private int coinChange(int[] coins, int index, int n, int target, int[][]dp) {
-        // base case
-        if(target == 0) return 0;
-        if(index == n) 
-            return Integer.MAX_VALUE;
-        if(target < 0)
-            return Integer.MAX_VALUE;
-        
-        if(dp[index][target] != -1)
-            return dp[index][target];
-        
-        // main logic
-        int skip = coinChange(coins, index+1, n, target, dp);
-        int take = coinChange(coins, index, n, target - coins[index], dp);
-        if(take != Integer.MAX_VALUE)
-            take++;
-        
-        return dp[index][target] = Math.min(skip, take);
+        for(int amount = 0; amount < A + 1; amount++) {
+            if(amount % coins[0] == 0) prev[amount] = amount / coins[0];
+        }
+
+        // step 2 : iteration 
+        for(int index = 1; index < n; index ++) {
+            int[] curr = new int[A + 1];
+            for(int amount = 0; amount < A + 1; amount++) {
+                int skip = prev[amount];
+                int take = Integer.MAX_VALUE;
+                if(coins[index] <= amount) {
+                    int numberOfCoinsUsed = curr[amount - coins[index]];
+                    if(numberOfCoinsUsed != Integer.MAX_VALUE)
+                        take = numberOfCoinsUsed + 1;
+                } 
+                curr[amount] = Math.min(take, skip);
+            }
+            prev = curr;
+        }
+
+        // step 3 : return statement
+        return prev[A] == Integer.MAX_VALUE ? -1 : prev[A];
     }
 }
