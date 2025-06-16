bef52449c4bc9d175c6b4c9717372a47376b9c35
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-16 05:43:33 +0530

    Time: 221 ms (5.01%), Space: 100.2 MB (5.11%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,42 +1,49 @@
 class Solution {
     public int calculate(String s) {
+        int n = s.length();
         Stack<Integer> stack = new Stack<>();
-        int n = s.length(), result = 0;
-        char sign = '+';
-        for(int i = 0; i < n ;i++) {
-            char ch = s.charAt(i);
-            if(Character.isDigit(ch)) {
-                int num = 0;
-                while(i<n && Character.isDigit(s.charAt(i))) {
-                    num = num * 10 + s.charAt(i) - '0';
+        int result = 0, sign = 1;
+        for(int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+            // get number and push to stack after updating with sign
+            if(Character.isDigit(curr)) {
+                int number = 0;
+                while(i < n && Character.isDigit(s.charAt(i))) {
+                    number = number * 10 + (s.charAt(i) - '0');
                     i++;
                 }
                 i--;
-                updateStack(stack, sign, num);
-                sign = '+';
-            } else if(ch == '(') {
-                int j = i;
-                int open = 0;
-                while(j < n) {
-                    if(s.charAt(j) == '(') open++;
-                    else if(s.charAt(j) == ')') open--;
-                    if(open == 0) break;
-                    j++;
+                updateStack(stack, number, sign);
+                sign = 1;
+            } else if(curr == '+') {
+                sign = 1;
+            } else if(curr == '-') {
+                sign = -1;
+            } else if(curr == '(') {
+                // find the corresponding closing paranthesis
+                int cursor = i, open = 0, close = 0;
+                while(cursor < n) {
+                    if(s.charAt(cursor) == '(') open++;
+                    else if(s.charAt(cursor) == ')') close++;
+                    if(open == close) break;
+                    cursor++;
                 }
-                int num = calculate(s.substring(i+1, j));
-                updateStack(stack, sign, num);
-                sign = '+';
-                i = j;
-            } else if(ch == ' ') continue;
-            else sign = ch;
+                int number = calculate(s.substring(i+1, cursor));
+                updateStack(stack, number, sign);
+                sign = 1;
+                i = cursor;
+            }
         }
-        
-        int res = 0;
-        while(!stack.isEmpty()) result += stack.pop();
+
+        while(!stack.isEmpty()) {
+            result += stack.pop();
+        }
         return result;
     }
-    private void updateStack(Stack<Integer> st, char sign, int num) {
-        if(sign == '+') st.push(num);
-        else st.push(-num);
+
+    // helper function
+    private void updateStack(Stack<Integer> stack, int number, int sign) {
+        if(sign == 1) stack.push(number);
+        else stack.push(-number);
     }
 }
