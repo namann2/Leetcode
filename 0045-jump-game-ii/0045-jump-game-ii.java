9f6989b80479171a7b66fffe09c39fbfff4024f7
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-19 00:34:46 +0530

    Time: 38 ms (15.91%), Space: 46.2 MB (5.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int minJump = 0, farthest = 0, currEnd = 0; // currEnd - max length/jump of current ladder/element
-        for(int i = 0; i < n; i++) {
-            // if we have reached the target
-            if(currEnd >= n-1) return minJump;
-            // keep a note of the farthest ladder
-            if(i+nums[i] > farthest) {
-                farthest = nums[i] + i;
+        int[] dp = new int[n];
+        for(int i = n-2; i >= 0; i--) {
+            int jumps = nums[i];
+            dp[i] = Integer.MAX_VALUE;
+            for(int jump = 1; jump <= jumps; jump++) {
+                if(i + jump < n && dp[i+jump] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], dp[i+jump]);
             }
-            // if we have exhausted our current ladder, we need to make a jump
-            if(i == currEnd) {
-                minJump++;
-                currEnd = farthest;
-            }
+            if(dp[i] != Integer.MAX_VALUE) dp[i]++;
         }
-        return minJump;
+        return dp[0];
     }
 }
