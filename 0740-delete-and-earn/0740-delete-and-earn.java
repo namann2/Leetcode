e2319bd991d9cf862555f04dc6d5dfe33afadb47
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-18 21:59:09 +0530

    Time: 3 ms (78.04%), Space: 44.5 MB (74.83%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,25 +1,18 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
+        int n = nums.length;
         int max = 0;
-        for(int num : nums)
-            max = Math.max(max, num);
-        
-        int[] count = new int[max + 1];
-        for(int num : nums)
-            count[num]++;
-        
-        // 2 2 3 3 3 4
-        // 0 1 2 3 4 - index
-        // 0 0 2 3 1 
-        int[] dp = new int[max+1];
-        // dp state - max uptil this index
-        dp[0] = 0;
-        dp[1] = Math.max(1 * count[1], count[0]);
-        for(int i = 2; i < max + 1; i++) {
-            int choose = count[i] * i + dp[i-2];
-            int dont = dp[i-1];
-            dp[i] = Math.max(choose, dont);
+        for(int num : nums) max = Math.max(max, num);
+
+        int[] numberCount = new int[max + 1];
+        for(int num : nums) numberCount[num]++;
+
+        int[]dp = new int[max + 1];
+        for(int index = max; index >= 0; index--) {
+            int pick = (numberCount[index] * index) + (index + 2 < max + 1 ? dp[index + 2] : 0);
+            int skip = index + 1 < max + 1 ? dp[index + 1] : 0;
+            dp[index] = Math.max(pick, skip);
         }
-        return dp[max];
+        return dp[0];
     }
 }
