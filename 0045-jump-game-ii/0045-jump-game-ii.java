b034e4b5741dfcf56d051bfbf14d728081b372d3
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-06-24 00:41:30 +0530

    Time: 44 ms (12.74%), Space: 45.6 MB (25.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
+        // dp state = min number of steps to reach last index from ith index
         for(int i = n-2; i >= 0; i--) {
-            int jumps = nums[i];
             dp[i] = Integer.MAX_VALUE;
-            for(int jump = 1; jump <= jumps; jump++) {
-                if(i + jump < n && dp[i+jump] != Integer.MAX_VALUE)
-                    dp[i] = Math.min(dp[i], dp[i+jump]);
+            for(int jump = 1; jump <= nums[i]; jump++) {
+                if(i + jump < n && dp[i+jump] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i+jump]);
             }
-            if(dp[i] != Integer.MAX_VALUE) dp[i]++;
+            if(dp[i] != Integer.MAX_VALUE) dp[i] ++;
         }
         return dp[0];
     }
