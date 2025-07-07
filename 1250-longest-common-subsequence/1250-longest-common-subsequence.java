d70b00b6b14f53b3e0144a8abbd9aa04e8ef12f0
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-07-08 01:28:56 +0530

    Time: 19 ms (87.27%), Space: 50.3 MB (89%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,24 +1,20 @@
 class Solution {
-    public int longestCommonSubsequence(String w1, String w2) {
-        int w1Length = w1.length(), w2Length = w2.length();
-        Integer[][]dp = new Integer[w1Length][w2Length];
-        return lcs(w1, w2, w1Length - 1, w2Length - 1, dp);
-    }
-
-    // helper function
-    private int lcs(String w1, String w2, int index1, int index2, Integer[][] dp) {
-        // base case
-        if(index1 == 0 && index2 == 0) return w1.charAt(0) == w2.charAt(0) ? 1 : 0;
-        if(index1 < 0 || index2 < 0) return 0;
-        if(dp[index1][index2] != null) 
-            return dp[index1][index2];
-        // main logic
-        if(w1.charAt(index1) == w2.charAt(index2)) {
-            return dp[index1][index2] = 1 + lcs(w1, w2, index1 - 1, index2 - 1, dp);
-        } else {
-            int op1 = lcs(w1, w2, index1 - 1, index2, dp);
-            int op2 = lcs(w1, w2, index1, index2 - 1, dp);
-            return dp[index1][index2] = Math.max(op1, op2);
+    public int longestCommonSubsequence(String word1, String word2) {
+        int w1Length = word1.length(), w2Length = word2.length();
+        // bottom up solution
+        int[][] dp = new int[w1Length + 1][w2Length + 1];
+        int[] prev = new int[w2Length + 1];
+        for(int index1 = 1; index1 < w1Length + 1; index1++) {
+            int[] curr = new int[w2Length + 1];
+            for(int index2 = 1; index2 < w2Length + 1; index2++) {
+                if(word1.charAt(index1 - 1) == word2.charAt(index2 - 1)) {
+                    curr[index2] = 1 + prev[index2 - 1];
+                } else {
+                    curr[index2] = Math.max(prev[index2], curr[index2 - 1]);
+                }
+            }
+            prev = curr;
         }
+        return prev[w2Length];
     }
 }
