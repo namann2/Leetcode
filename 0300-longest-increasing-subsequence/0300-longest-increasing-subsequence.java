a43fe092b88486ff22d94057db0673533cf798a6
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-07-08 20:29:21 +0530

    Time: 7 ms (79.65%), Space: 44.2 MB (65.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,26 +2,27 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         List<Integer> list = new ArrayList<>();
-        for(int num : nums) {
-            if(list.size() == 0 || num > list.get(list.size()-1)) list.add(num);
+        for(int i = 0; i < n; i++) {
+            if(list.size() == 0 || nums[i] > list.get(list.size()-1)) list.add(nums[i]);
             else {
-                int index = binarySearch(list, num);
-                if(index < list.size()) list.set(index, num);
+                int index = findInsertionIndex(list, list.size(), nums[i]);
+                // TODO : update the logic based on value of index
+                list.set(index, nums[i]);
             }
         }
         return list.size();
     }
-    
-    private int binarySearch(List<Integer> list, int num) {
-        int n = list.size();
-        int start = 0, end = n-1;
-        // 1 3 5 6, 5
+
+    // helper function
+    private int findInsertionIndex(List<Integer> list, int n, int ele) {
+        int start = 0, end = n-1, ans = -1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
-            if(num > list.get(mid)) {
-                start = mid + 1;
-            } else end = mid - 1;
+            if(list.get(mid) >= ele) {
+                ans = mid;
+                end = mid - 1;
+            } else start = mid + 1;
         }
-        return start;
+        return ans;
     }
-} 
+}
