871c01aa2ae04aa47a3db39481db1a2a776825fe
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-07-10 01:25:48 +0530

    Time: 21 ms (62.01%), Space: 44.7 MB (89.11%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,55 +1,33 @@
 class Solution {
-    public int minimumDeleteSum(String A, String B) {
-        int n = A.length(), m = B.length();
-        int[][]dp = new int[n+1][m+1];
-        // for(int[] row : dp)
-        //     Arrays.fill(row, -1);
-        
-        // return LCS(A, B, n, m, 0, 0, dp);
-//         
-        
-        for(int j = m-1; j >= 0; j--)
-            dp[n][j] = dp[n][j+1] + B.charAt(j);
-        
-        for(int i = n-1; i >= 0; i--)
-            dp[i][m] = dp[i+1][m] + A.charAt(i);
-            
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = m - 1; j >= 0; j--) {
-                if(A.charAt(i) == B.charAt(j))
-                    dp[i][j] = dp[i + 1][j + 1];
-                else dp[i][j] = Math.min(A.charAt(i) + dp[i+1][j], B.charAt(j) + dp[i][j+1]);
+    public int minimumDeleteSum(String s1, String s2) {
+        int s1Length = s1.length(), s2Length = s2.length();
+
+        // bottom up solution
+        int[] prev = new int[s2Length + 1];
+
+        for(int index2 = 1; index2 < s2Length + 1; index2++) {
+            prev[index2] = s2.charAt(index2 - 1) + prev[index2 - 1];
+        }
+
+        // dp[index1][0] needs to be converted to curr[0], hence, update
+        // the logic to where curr is being initialized
+        // for(int index1 = 1; index1 < s1Length + 1; index1++) {
+        //     dp[index1][0] = s1.charAt(index1 - 1) + dp[index1 - 1][0];
+        // }
+
+        // iteration
+        for(int index1 = 1; index1 < s1Length + 1; index1 ++) {
+            int[] curr = new int[s2Length + 1];
+            curr[0] = s1.charAt(index1 - 1) + prev[0];
+            for(int index2 = 1; index2 < s2Length + 1; index2 ++) {
+                if(s1.charAt(index1 - 1) == s2.charAt(index2 - 1)) {
+                    curr[index2] = prev[index2 - 1];
+                } else {
+                    curr[index2] = Math.min(s1.charAt(index1 - 1) + prev[index2], s2.charAt(index2 - 1) + curr[index2 - 1]);
+                }
             }
+            prev = curr;
         }
-        return dp[0][0];
-    }
-    
-    private int LCS(String A, String B, int n, int m, int i, int j, int[][]dp) {
-        // base case
-        if(i == n && j == m)
-            return 0;
-        if(i == n) {
-            int sum = 0;
-            while(j < m)
-                sum += B.charAt(j++);
-            return sum;
-        }
-        if(j == m) {
-            int sum = 0;
-            while(i < n)
-                sum += A.charAt(i++);
-            return sum;
-        }
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        // main logic
-        if(A.charAt(i) == B.charAt(j))
-            return dp[i][j] = LCS(A, B, n, m, i+1, j+1, dp);
-        
-        int x = A.charAt(i) + LCS(A, B, n, m, i+1, j, dp);
-        int y = B.charAt(j) + LCS(A, B, n, m, i, j+1, dp);
-        return dp[i][j] = Math.min(x, y);
+        return prev[s2Length];
     }
 }
