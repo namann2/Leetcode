7f080421fe09cb837d3e0de4a7a771eafd1a2146
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-09-08 19:35:41 +0530

    Time: 4 ms (99.57%), Space: 47.8 MB (49.63%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,16 +1,18 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int n = nums.length;
-        int end = 0, begin = 0, product = 1, cnt = 0;
+        int end = 0, begin = 0, product = 1;
+        int count = 0;
         while(end < n) {
             product *= nums[end];
-            while(product >= k && begin < end) {
+            while(begin < end && product >= k) {
                 product /= nums[begin++];
             }
-            if(product < k) 
-                cnt += end - begin + 1;
+            if(product < k) {
+                count += end - begin + 1;
+            }
             end++;
         }
-        return cnt;
+        return count;
     }
 }
