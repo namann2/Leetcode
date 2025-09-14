3a7302f08ca002d478823f0f693b8721a0607cf3
Naman Gupta <59756482+namann2@users.noreply.github.com>
2025-09-14 19:52:34 +0530

    Time: 34 ms (32.7%), Space: 65 MB (5.42%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
+        Deque<Integer> q = new LinkedList<>();
         int end = 0, begin = 0, idx = 0;
-        int[] ans = new int[n-k+1];
-        Deque<Integer> q = new ArrayDeque<>();
+        int[] answer = new int[n-k+1];
         while(end < n) {
-            while(!q.isEmpty() && q.peekLast() < nums[end])
-                q.removeLast();
+            // keep monotonic increasing queue
+            while(!q.isEmpty() && nums[end] > q.peekLast()) q.removeLast();
             q.addLast(nums[end]);
-            if(end - begin + 1 == k) {
-                ans[idx++] = q.peekFirst();
-                if(q.peekFirst() == nums[begin++]) q.removeFirst();
+            // keep maintaining the size of window as k
+            while(!q.isEmpty() && begin < end && end-begin+1 > k) {
+                if(nums[begin] == q.peekFirst()) q.removeFirst();
+                begin++;
+            }
+            // check if size of window is k and update the current maximum
+            if(end-begin+1 == k) {
+                answer[idx++] = q.isEmpty() ? 0 : q.peekFirst(); 
             }
             end++;
         }
-        return ans;
+        return answer;
     }
 }
